2a8fdb13274705ae40a6a9eeccad57426b1498be
Jesper Pedersen <blackie@blackie.dk>
2003-02-18 09:48:11 +0000

    updated arrow drawing code to protect against null pointer exceptions
    svn path=/trunk/kdeextragear-2/kimdaba/; revision=207655

--- linedraw.cpp
+++ linedraw.cpp
@@ -16,23 +16,24 @@ void LineDraw::draw( QPainter& painter, QMouseEvent* event )
     painter.setPen( QPen( Qt::black, 3 ) );
     painter.drawLine( _startPos, _lastPos );
 
-    double x0 = _startPos.x();
-    double y0 = _startPos.y();
-    double x1 = _lastPos.x();
-    double y1 = _lastPos.y();
-    double a = (y1-y0)/(x1-x0);
-    double angle =  atan(a) * 360.0/(2*3.14);
-    if ( x1 < x0 )
-        angle = 180+angle;
-    // angle is now the angle of the line.
+    double dx = _lastPos.x() - _startPos.x();
+    double dy = _lastPos.y() - _startPos.y();
 
-    angle = angle + 180 - 15;
-    painter.translate( x1, y1 );
-    painter.rotate( angle );
-    painter.drawLine( QPoint(0,0), QPoint( 30,0 ) );
+    if ( dx != 0 || dy != 0 ) {
+        if( dy < 0 ) dx = -dx;
+        double angle = acos(dx/sqrt( dx*dx+dy*dy ))*180./M_PI;
+        if( dy < 0 ) angle += 180.;
 
-    painter.rotate( 30 );
-    painter.drawLine( QPoint(0,0), QPoint( 30,0 ) );
+        // angle is now the angle of the line.
+
+        angle = angle + 180 - 15;
+        painter.translate( _lastPos.x(), _lastPos.y() );
+        painter.rotate( angle );
+        painter.drawLine( QPoint(0,0), QPoint( 30,0 ) );
+
+        painter.rotate( 30 );
+        painter.drawLine( QPoint(0,0), QPoint( 30,0 ) );
+    }
 
     painter.restore();
 }

a08102abffb37bf04cb13b2c48309bd8bd193ad5
Jesper Pedersen <blackie@blackie.dk>
2003-09-14 08:51:49 +0000

    a slightly ups ;-)
    svn path=/trunk/kdeextragear-2/kimdaba/; revision=250923

---
+++ datefolder.cpp
@@ -0,0 +1,80 @@
+#include "datefolder.h"
+#include <klocale.h>
+#include <kglobal.h>
+#include <kiconloader.h>
+#include <qlayout.h>
+#include <kdatepicker.h>
+#include <qlabel.h>
+#include "contentfolder.h"
+DateFolder::DateFolder( const ImageSearchInfo& info, Browser* parent )
+    :Folder( info, parent )
+{
+    setText( 0, i18n("Date") );
+    setText( 1, QString::fromLatin1( "-" ) );
+        KIconLoader loader;
+    setPixmap( 0, KGlobal::iconLoader()->loadIcon( QString::fromLatin1( "korganizer" ), KIcon::Desktop, 22 ) );
+
+}
+
+FolderAction* DateFolder::action( bool /* ctrlDown */ )
+{
+    DateSearchDialog dialog( _browser );
+    if ( dialog.exec() == QDialog::Accepted ) {
+        ImageSearchInfo info( _info );
+        info.setStartDate( ImageDate( dialog.fromDate() ) );
+        info.setEndDate( ImageDate( dialog.toDate() ) );
+        return new ContentFolderAction( QString::null, QString::null, info, _browser );
+    }
+    else
+        return 0;
+}
+
+DateSearchDialog::DateSearchDialog( QWidget* parent, const char* name )
+    :KDialogBase( KDialogBase::Plain, i18n("Date search"), KDialogBase::Ok | KDialogBase::Cancel, KDialogBase::Ok, parent, name ), _toChanged( false )
+{
+    QWidget* top = plainPage();
+    QHBoxLayout* lay1 = new QHBoxLayout( top, 6 );
+
+    QVBoxLayout* lay2 = new QVBoxLayout( lay1, 6 );
+    QLabel* label = new QLabel( QString::fromLatin1( "<qt><font size=\"+3\">%1:</font></qt>")
+                                .arg(i18n("From") ), top );
+    lay2->addWidget( label );
+    _from = new KDatePicker( top );
+
+    lay2->addWidget( _from );
+
+    lay1->addSpacing( 100 );
+
+    QVBoxLayout* lay3 = new QVBoxLayout( lay1, 6 );
+    label = new QLabel( QString::fromLatin1( "<qt><font size=\"+3\">%1:</font></qt>" )
+                        .arg( i18n("To") ), top );
+    lay3->addWidget( label );
+    _to = new KDatePicker( top );
+    lay3->addWidget( _to );
+
+    _from->setDate( QDate( QDate::currentDate().year(), 1, 1 ) );
+    _to->setDate( QDate( QDate::currentDate().year()+1, 1, 1 ) );
+    connect( _from, SIGNAL( dateChanged( QDate ) ), this, SLOT( fromDateChanged( QDate ) ) );
+    connect( _to, SIGNAL( dateChanged( QDate ) ), this, SLOT( toDateChanged() ) );
+}
+
+QDate DateSearchDialog::fromDate() const
+{
+    return _from->date();
+}
+
+QDate DateSearchDialog::toDate() const
+{
+    return _to->date();
+}
+
+void DateSearchDialog::fromDateChanged( QDate date )
+{
+    if ( !_toChanged )
+        _to->setDate( date );
+}
+
+void DateSearchDialog::toDateChanged()
+{
+    _toChanged = true;
+}
---
+++ datefolder.h
@@ -0,0 +1,70 @@
+/*
+ *  Copyright (c) 2003 Jesper K. Pedersen <blackie@kde.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License version 2 as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA 02111-1307, USA.
+ **/
+#ifndef DATEFOLDER_H
+#define DATEFOLDER_H
+
+#include "folder.h"
+#include <kdialogbase.h>
+#include <qdatetime.h>
+class KDatePicker;
+
+class DateFolder :public Folder {
+public:
+    DateFolder( const ImageSearchInfo& info, Browser* parent );
+    virtual FolderAction* action( bool ctrlDown = false );
+
+};
+
+class DateSearchDialog :public KDialogBase
+{
+    Q_OBJECT
+public:
+    DateSearchDialog( QWidget* parent, const char* name = 0 );
+    QDate fromDate() const;
+    QDate toDate() const;
+
+protected slots:
+    void fromDateChanged( QDate );
+    void toDateChanged();
+
+private:
+    KDatePicker* _from;
+    KDatePicker* _to;
+    bool _toChanged;
+};
+
+/*
+  class ContentFolderAction :public FolderAction {
+
+public:
+    ContentFolderAction( const QString& optionGroup, const QString& value,
+                         const ImageSearchInfo& info, Browser* parent );
+    virtual void action();
+    virtual bool showsImages() { return false; }
+    virtual bool allowSort() const;
+    virtual QString title() const;
+
+private:
+    QString _optionGroup;
+    QString _value;
+};
+*/
+
+
+#endif /* DATEFOLDER_H */
+

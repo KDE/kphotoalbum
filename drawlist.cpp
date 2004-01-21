c335db013650be26b45819ba4f7a2a2b5b77979d
Jesper Pedersen <blackie@blackie.dk>
2004-01-21 14:12:46 +0000

    updated copyright information
    svn path=/trunk/kdeextragear-2/kimdaba/; revision=281543

--- drawlist.cpp
+++ drawlist.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2003 Jesper K. Pedersen <blackie@kde.org>
+ *  Copyright (c) 2003-2004 Jesper K. Pedersen <blackie@kde.org>
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public

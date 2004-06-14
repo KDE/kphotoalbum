9de8286b0233de9084b289c13b414dd236503858
Jesper Pedersen <blackie@blackie.dk>
2004-06-14 18:41:18 +0000

    ups forgot these with the EXIF info patch
    svn path=/trunk/kdeextragear-2/kimdaba/; revision=320658

---
+++ readinfodialog.cpp
@@ -0,0 +1,76 @@
+#include "readinfodialog.h"
+#include <klocale.h>
+#include <qlabel.h>
+#include <qcheckbox.h>
+#include <qradiobutton.h>
+#include <qlayout.h>
+#include <qbuttongroup.h>
+#include <qfile.h>
+#include <kmessagebox.h>
+#include "imagedb.h"
+#include "imageinfo.h"
+#include "util.h"
+
+ReadInfoDialog::ReadInfoDialog( QWidget* parent, const char* name )
+    :KDialogBase( Plain, i18n("Read File Info"), Cancel|User1, User1, parent, name,
+                  true, false, i18n("Read File Info"))
+{
+    QWidget* top = plainPage();
+    QVBoxLayout* lay1 = new QVBoxLayout( top, 6 );
+
+    _label = new QLabel( top );
+    lay1->addWidget( _label );
+
+    _time = new QCheckBox( i18n( "Read time" ), top );
+    lay1->addWidget( _time );
+
+    _date = new QCheckBox( i18n( "Read date" ), top );
+    lay1->addWidget( _date );
+
+    _orientation = new QCheckBox( i18n( "Read EXIF orientation" ), top );
+    lay1->addWidget( _orientation );
+
+    _description = new QCheckBox( i18n( "Read EXIF description" ), top );
+    lay1->addWidget( _description );
+
+    _init = new QCheckBox( i18n( "Read all (time, date, orientation and description)" ), top );
+    lay1->addWidget( _init );
+
+    connect( this, SIGNAL( user1Clicked() ), this, SLOT( readInfo() ) );
+}
+
+int ReadInfoDialog::exec( const ImageInfoList& list )
+{
+    _label->setText( i18n("<qt><b><center><font size=\"+3\">Read File Info<br>%1 selected</font></center></b></qt>").arg( list.count() ) );
+
+    _init->setChecked( false );
+    _time->setChecked( true );
+    _date->setChecked( true );
+    _orientation->setChecked( false );
+    _description->setChecked( false );
+    _list = list;
+
+    return KDialogBase::exec();
+}
+
+void ReadInfoDialog::readInfo()
+{
+    int mode = 0;
+
+    if ( _init->isChecked() )
+            mode |= EXIFMODE_INIT;
+    else {
+            if ( _time->isChecked() )
+                    mode |= EXIFMODE_TIME;
+            if ( _date->isChecked() )
+                    mode |= EXIFMODE_DATE;
+            if ( _orientation->isChecked() )
+                    mode |= EXIFMODE_ORIENTATION;
+            if ( _description->isChecked() )
+                    mode |= EXIFMODE_DESCRIPTION;
+    }
+    ImageDB::instance()->slotReread(_list, mode);
+    accept();
+}
+
+#include "readinfodialog.moc"
---
+++ readinfodialog.h
@@ -0,0 +1,30 @@
+#ifndef READINFODIALOG_H
+#define READINFODIALOG_H
+#include "imageinfo.h"
+#include <kdialogbase.h>
+class QLabel;
+class QCheckBox;
+class QRadioButton;
+
+class ReadInfoDialog :public KDialogBase {
+    Q_OBJECT
+
+public:
+    ReadInfoDialog( QWidget* parent, const char* name = 0 );
+    int exec( const ImageInfoList& );
+
+protected slots:
+    void readInfo();
+private:
+    ImageInfoList _list;
+    QLabel* _label;
+    QCheckBox* _time;
+    QCheckBox* _date;
+    QCheckBox* _orientation;
+    QCheckBox* _description;
+    QCheckBox* _init;
+};
+
+
+#endif /* READINFODIALOG_H */
+

728767d28858036ece6cde599415bb6c798fbff2
Jesper Pedersen <blackie@blackie.dk>
2004-06-14 18:40:57 +0000

    include version number in splash screen - then hopefully I'll not forget to update it before a release
    svn path=/trunk/kdeextragear-2/kimdaba/; revision=320657

--- mysplashscreen.cpp
+++ mysplashscreen.cpp
@@ -1,14 +1,25 @@
 #include "mysplashscreen.h"
 #include <kstandarddirs.h>
+#include <kglobal.h>
+#include <kaboutdata.h>
+#include <qpainter.h>
 
 MySplashScreen* MySplashScreen::_instance = 0;
 
-MySplashScreen::MySplashScreen( )
+MySplashScreen::MySplashScreen()
     :KSplashScreen( locate("data", QString::fromLatin1("kimdaba/pics/splash-large.png") ) )
 {
     _instance = this;
     QPixmap* pix = pixmap();
     resize( pix->size() );
+    QPainter painter( pix );
+    QFont fnt = font();
+    fnt.setPixelSize( 24 );
+    painter.setFont( fnt );
+    QPen pen( white );
+    painter.setPen( pen );
+    painter.drawText( QRect( QPoint(260, 400), QPoint( 620, 470 )), AlignRight | AlignTop,
+                      i18n( "KimDaBa version %1" ).arg( KGlobal::instance()->aboutData()->version() ) );
 }
 
 MySplashScreen* MySplashScreen::instance()

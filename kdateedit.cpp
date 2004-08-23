336c50a7f55ff43933fd2dd47bc8dcdb6cd86158
Jesper Pedersen <blackie@blackie.dk>
2004-08-23 15:52:27 +0000

    for strange reasons (likely cut'n'paste) kimdaba was under LGPL, which makes no sense at all, so now its under GPL.
    svn path=/trunk/kdeextragear-2/kimdaba/; revision=340119

--- kdateedit.cpp
+++ kdateedit.cpp
@@ -1,22 +1,19 @@
-/*
-  This file was originally taken from kdepim/libkdepim/kdateedit.cpp
+/* Copyright (C) 2003-2004 Jesper K. Pedersen <blackie@kde.org>
 
-  Copyright (c) 2002 Cornelius Schumacher <schumacher@kde.org>
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Library General Public
-  License as published by the Free Software Foundation; either
-  version 2 of the License, or (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Library General Public License for more details.
-
-  You should have received a copy of the GNU Library General Public License
-  along with this library; see the file COPYING.LIB.  If not, write to
-  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-  Boston, MA 02111-1307, USA.
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
 */
 
 #include <qevent.h>

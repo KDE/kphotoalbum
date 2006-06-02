de124f6372f3310f9921c6e12eba42deb06ba51d
Tuomas Suutari <thsuut@utu.fi>
2006-06-02 19:45:36 +0000

    Cleaning up sqldb.py
    svn path=/branches/kphotoalbum/soc/; revision=547632

--- script/KPhotoAlbum/mysqldb.py
+++ script/KPhotoAlbum/mysqldb.py
@@ -5,7 +5,6 @@ Currently supports only storing. (no reading)
 """
 
 from db import DatabaseWriter
-#from datatypes import *
 
 class MySQLDatabase(DatabaseWriter):
 	"""
@@ -36,87 +35,82 @@ class MySQLDatabase(DatabaseWriter):
 
 	def __init__(self, mysqlDb):
 		"""
+		Initialize self with given MySQL connection.
+
 		Parameter mysqlDb should be a db object returned by
 		MySQLdb.connect.
 		"""
 		super(MySQLDatabase, self).__init__()
 		self.db = mysqlDb
 		self.c = self.db.cursor()
-		self.createTables(False)
+		self.__createTables()
 
-	def createTables(self, dropFirst):
+	def __createTables(self):
 		for t in self.tableList:
-			if dropFirst:
-				self.c.execute('DROP TABLE IF EXISTS ' + t[0])
 			self.c.execute('CREATE TABLE IF NOT EXISTS ' +
 				       t[0] + '(' + t[1] + ')')
-		self.getIdsFromDatabase()
+		self.__getIds()
 
-	def getIdsFromDatabase(self):
-		self.clearIds()
+	def __decodeString(self, str):
+		return unicode(str, self.db.charset)
+
+	def __getIds(self):
+		self.__clearIds()
 		self.c.execute('SELECT id, filename, md5sum FROM image')
 		for (i, f, m) in self.c:
-			self.imageMap[(unicode(f, self.db.charset), m)] = i
+			self.imageMap[self.__decodeString(f), m)] = i
 		self.c.execute('SELECT id, name FROM category')
 		for (i, n) in self.c:
-			self.categoryMap[unicode(n, self.db.charset)] = i
+			self.categoryMap[self.__decodeString(n)] = i
 		self.c.execute('SELECT tag.id, tag.name, category.name '
 			       'FROM tag JOIN category '
 			       'ON category.id=tag.categoryId')
 		for (i, tn, cn) in self.c:
-			self.tagMap[(unicode(cn, self.db.charset),
-				     unicode(tn, self.db.charset))] = i
+			self.tagMap[(self.__decodeString(cn),
+				     self.__decodeString(tn))] = i
 
-	def clearIds(self):
+	def __clearIds(self):
 		self.imageMap = ItemNumMap()
 		self.categoryMap = ItemNumMap()
 		self.tagMap = ItemNumMap()
 
-	def clear(self):
-		for t in self.tableList:
-			self.c.execute('DELETE FROM ' + t[0])
-		self.clearIds()
-
-	def tableHasCol(self, table, col, colValue):
+	def __tableHasCol(self, table, col, colValue):
 		"""
 		Return true, iff table has a row which has column col
 		set to colValue.
 		"""
-		return 0L != self.c.execute('SELECT ' + col +
-					    ' FROM ' + table +
-					    ' WHERE ' + col + '=%s',
-					    (colValue,))
+		return 0 != self.c.execute('SELECT ' + col +
+					   ' FROM ' + table +
+					   ' WHERE ' + col + '=%s',
+					   (colValue,))
 
 	def insertCategory(self, c):
 		cid = self.categoryMap.numFor(c.name)
-		self.c.execute('DELETE FROM category WHERE id=%s',
-			       (cid,))
+		self.c.execute('DELETE FROM category WHERE id=%s', (cid,))
 		self.c.execute('INSERT INTO category(id, name, icon, '
 			       'visible, viewtype, viewsize) '
 			       'values(%s,%s,%s,%s,%s,%s)',
 			       (cid, c.name, c.icon,
 				int(c.visible), c.viewtype, c.viewsize))
 		for item in c.items.itervalues():
-			self.insertTag((c.name, item))
+			self.__insertTag((c.name, item))
 
-	def insertTag(self, tag):
+	def __insertTag(self, tag):
 		"""
 		Insert tag into tag table, if not already present.
 
 		Assumes that category of tag is already created.
 		"""
 		tid = self.tagMap.numFor(tag)
-		if not self.tableHasCol('tag', 'id', tid):
+		if not self.__tableHasCol('tag', 'id', tid):
 			cid = self.categoryMap.numFor(tag[0])
-			if not self.tableHasCol('category', 'id', cid):
+			if not self.__tableHasCol('category', 'id', cid):
 				self.c.execute('INSERT INTO '
 					       'category(id, name) '
 					       'values(%s,%s)',
 					       (cid, tag[0]))
-			self.c.execute('INSERT INTO tag'
-				       '(id, categoryId, name) '
-				       'values(%s,%s,%s)',
-				       (tid, cid, tag[1]))
+			self.c.execute('INSERT INTO tag(id, categoryId, name) '
+				       'values(%s,%s,%s)', (tid, cid, tag[1]))
 
 	def insertImage(self, i):
 		iid = self.imageMap.numFor((i.filename, i.md5sum))
@@ -133,14 +127,14 @@ class MySQLDatabase(DatabaseWriter):
 				i.width, i.height, i.angle))
 		for tag in i.tags:
 			tid = self.tagMap.numFor(tag)
-			if not self.tableHasCol('tag', 'id', tid):
-				self.insertTag(tag)
-			self.insertImageTag(iid, tid)
+			if not self.__tableHasCol('tag', 'id', tid):
+				self.__insertTag(tag)
+			self.__insertImageTag(iid, tid)
 
-	def insertImageTag(self, iid, tid):
+	def __insertImageTag(self, iid, tid):
 		if self.c.execute('SELECT * FROM image_tag '
 				  'WHERE imageId=%s AND tagId=%s',
-				  (iid, tid)) == 0L:
+				  (iid, tid)) == 0:
 			self.c.execute('INSERT INTO '
 				       'image_tag(imageId, tagId) '
 				       'values(%s,%s)', (iid, tid))
@@ -151,8 +145,17 @@ class MySQLDatabase(DatabaseWriter):
 	def insertBlockItem(self, b):
 		pass
 
+	def clear(self):
+		for t in self.tableList:
+			self.c.execute('DELETE FROM ' + t[0])
+		self.__clearIds()
+
 
 class ItemNumMap(dict):
+	"""
+	Simple extension to dict that can assign numbers to items
+	implicitly.
+	"""
 	def numFor(self, item):
 		"""
 		Get unique number for item.

fd0a5f07a8d968aed1299918fae40b102bb1b726
Jesper Pedersen <blackie@blackie.dk>
2006-08-28 01:35:35 +0000

    Tokens got kind of lost when importing from a KPhotoAlbum 2.2 index file.
    svn path=/trunk/extragear/graphics/kphotoalbum/; revision=577975

--- XMLDB/FileReader.cpp
+++ XMLDB/FileReader.cpp
@@ -86,6 +86,10 @@ void XMLDB::FileReader::createSpecialCategories()
     }
     tokenCat->setSpecialCategory( true );
 
+    // KPhotoAlbum 2.2 did not write the tokens to the category section, so unless we do this small trick they
+    // will not show up when importing.
+    for ( char ch = 'A'; ch < 'Z'; ++ch )
+        tokenCat->addItem( QString::fromLatin1("%1").arg( QChar( ch) ) );
 
     DB::CategoryPtr mediaCat = _db->_categoryCollection.categoryForName( QString::fromLatin1( "Media Type" ) );
     if ( !mediaCat ) {

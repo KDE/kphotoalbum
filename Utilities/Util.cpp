c4f289aad82eb8493b47b9e97d9dcfe9e8f05507
Jesper Pedersen <blackie@blackie.dk>
2006-11-19 09:38:42 +0000

    Do not show image size for videos (currently there are not way to get that information from the KDE video subsystem)
    svn path=/trunk/extragear/graphics/kphotoalbum/; revision=606096

--- Utilities/Util.cpp
+++ Utilities/Util.cpp
@@ -84,15 +84,15 @@ QString Utilities::createInfoText( DB::ImageInfoPtr info, QMap< int,QPair<QStrin
         }
     }
 
-    if ( Settings::SettingsData::instance()->showImageSize() )  {
+    if ( Settings::SettingsData::instance()->showImageSize() && info->mediaType() == DB::Image)  {
         const QSize imageSize = info->size();
-	const double megapix = imageSize.width() * imageSize.height() / 1000000.0;
-	text += i18n("<b>Image Size: </b> ") +
-            QString::number(imageSize.width()) + i18n("x") +
-            QString::number(imageSize.height()) +
-            QString::fromLatin1(" (") + QString::number(megapix, 'f', 1) +
-            i18n("MP") + QString::fromLatin1(")") +
-            linebreak;
+        const double megapix = imageSize.width() * imageSize.height() / 1000000.0;
+        text += i18n("<b>Image Size: </b> ") +
+                QString::number(imageSize.width()) + i18n("x") +
+                QString::number(imageSize.height()) +
+                QString::fromLatin1(" (") + QString::number(megapix, 'f', 1) +
+                i18n("MP") + QString::fromLatin1(")") +
+                linebreak;
     }
 
     QValueList<DB::CategoryPtr> categories = DB::ImageDB::instance()->categoryCollection()->categories();

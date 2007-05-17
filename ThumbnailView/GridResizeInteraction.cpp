b8de3ad5b41e2db3c4f6a3d1fc183fe5a4474e7f
Tuomas Suutari <thsuut@utu.fi>
2007-05-17 01:10:04 +0000

    Use _view->updateCellSize() to change cell size in GridResizeInteraction. Old code didn't set the height correctly when non-1:1 aspect ratio.
    svn path=/trunk/extragear/graphics/kphotoalbum/; revision=665453

--- ThumbnailView/GridResizeInteraction.cpp
+++ ThumbnailView/GridResizeInteraction.cpp
@@ -36,25 +36,14 @@ void ThumbnailView::GridResizeInteraction::mousePressEvent( QMouseEvent* event )
 void ThumbnailView::GridResizeInteraction::mouseMoveEvent( QMouseEvent* event )
 {
     QPoint dist = event->pos() - _mousePressPos;
-    int h = 0;
-    if ( Settings::SettingsData::instance()->displayLabels() )
-        h = QFontMetrics( _view->font() ).height();
 
-    _view->setCellWidth( QMAX( 32, _origSize.width() + (dist.x() + dist.y())/10 ) );
-    _view->setCellHeight( QMAX( 32 + h, _origSize.height() + (dist.x() + dist.y())/10 ) );
-
-    _view->updateGridSize();
+    Settings::SettingsData::instance()->setThumbSize( QMAX( 32, _origSize.width() + dist.x() ) );
+    _view->updateCellSize();
 }
 
 
 void ThumbnailView::GridResizeInteraction::mouseReleaseEvent( QMouseEvent* )
 {
-    int delta = _view->cellWidth() - _origSize.width();
-    Settings::SettingsData::instance()->setThumbSize( Settings::SettingsData::instance()->thumbSize() + delta);
-    if ( !_view->_currentItem.isNull() ) {
-        Cell cell = _view->positionForFileName( _view->_currentItem );
-        _view->ensureCellVisible( cell.row(), cell.col() );
-    }
     _resizing = false;
     _view->repaintScreen();
 }

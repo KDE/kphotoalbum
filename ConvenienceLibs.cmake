c58e1b348e54359c5f70337ca1ecd535c9b6ac61
Jesper Pedersen <blackie@blackie.dk>
2007-06-28 10:04:59 +0000

    OK time to get started porting KPhotoAlbum to KDE 4
    svn path=/trunk/extragear/graphics/kphotoalbum/; revision=681253

---
+++ ConvenienceLibs.cmake
@@ -0,0 +1,60 @@
+
+#former libtool convenience libraries:
+
+# HTMLGenerator/Makefile.am: HTMLGenerator
+
+# ImageManager/Makefile.am: ImageManager
+
+# Utilities/Makefile.am: Utilities
+
+# kexi/migration/Makefile.am: keximigrate
+
+# kexi/migration/pqxx/Makefile.am: keximigrate_pqxx
+
+# kexi/migration/txt/Makefile.am: keximigrate_txtmigrate
+
+# kexi/migration/mysql/Makefile.am: keximigrate_mysql
+
+# kexi/3rdparty/uuid/Makefile.am: kexiuuid
+
+# kexi/3rdparty/kolibs/Makefile.am: kexikolibs
+
+set( kexikolibs_UI
+KoPageLayoutColumnsBase.ui
+KoPageLayoutHeaderBase.ui
+)
+
+# kexi/3rdparty/kexisql/src/Makefile.am: kexisql2
+
+# kexi/3rdparty/kexisql3/src/Makefile.am: kexisql3
+
+# XMLDB/Makefile.am: xmldb
+
+# DB/Makefile.am: DB
+
+# SQLDB/Makefile.am: sqldb
+
+# AnnotationDialog/Makefile.am: AnnotationDialog
+
+# Exif/Makefile.am: exif
+
+# Viewer/Makefile.am: Viewer
+
+# MainWindow/Makefile.am: MainWindow
+
+# ThumbnailView/Makefile.am: ThumbnailView
+
+# Plugins/Makefile.am: Plugins
+
+# DateBar/Makefile.am: datebar
+
+# Survey/Makefile.am: survey
+
+# Browser/Makefile.am: Browser
+
+# ImportExport/Makefile.am: ImportExport
+
+# CategoryListView/Makefile.am: CategoryListView
+
+# Settings/Makefile.am: Settings
+

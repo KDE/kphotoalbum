19d750f96aa2a47d969f20d15cf9d579bda22053
Jan Kundrát <jkt@gentoo.org>
2007-10-13 00:41:54 +0000

    copyright updates
    svn path=/branches/extragear/kde3/graphics/kphotoalbum/; revision=724679

--- Exif/WriteDialog.cpp
+++ Exif/WriteDialog.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2003-2006 Jan Kundrát <jkt@gentoo.org>
+/* Copyright (C) 2003-2007 Jesper K. Pedersen <blackie@kde.org>, Jan Kundrát <jkt@gentoo.org>
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public

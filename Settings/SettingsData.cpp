d477fe03fd73d39f30001ac29a033e5f2272690b
Jan Kundrát <jkt@gentoo.org>
2007-10-18 20:50:32 +0000

    SILENT: did I really forgot to compile before committing?
    svn path=/branches/extragear/kde3/graphics/kphotoalbum/; revision=726774

--- Settings/SettingsData.cpp
+++ Settings/SettingsData.cpp
@@ -528,7 +528,7 @@ QValueList<Exif::Syncable::Kind> Settings::SettingsData::defaultFields( const bo
         w = r;
     } else if ( name == QString::fromAscii("label")) {
         r << Exif::Syncable::FILE_NAME <<
-            Exif::Syncable::STOP << Exif::Syncable::IPTC_HEADLINE
+            Exif::Syncable::STOP << Exif::Syncable::IPTC_HEADLINE <<
             Exif::Syncable::EXIF_USER_COMMENT << Exif::Syncable::EXIF_DESCRIPTION <<
             Exif::Syncable::JPEG_COMMENT << Exif::Syncable::EXIF_XPTITLE <<
             Exif::Syncable::EXIF_XPSUBJECT << Exif::Syncable::IPTC_OBJECT_NAME <<

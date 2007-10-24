d5c9175a1bacd6b0dc76f3f307977531f13e1d0a
Jan Kundrát <jkt@gentoo.org>
2007-10-24 19:42:55 +0000

    Don't ask about IPTC stuff for new users
    svn path=/branches/extragear/kde3/graphics/kphotoalbum/; revision=728964

--- MainWindow/Window.cpp
+++ MainWindow/Window.cpp
@@ -269,7 +269,8 @@ void MainWindow::Window::delayedInit()
 
 #ifdef HASEXIV2
     KConfig* config = kapp->config();
-    if ( ! config->hasGroup( QString::fromAscii("MetadataSyncing") ) )
+    if ( config->hasGroup( QString::fromAscii("EXIF") ) && ! config->hasGroup( QString::fromAscii("MetadataSyncing") ) )
+        // Show only for existing users of KPA that haven't configured IPTC yet
         KMessageBox::information( 0, i18n("<p>This version of KPhotoAlbum includes "
                     "support for importing embedded metadata (like EXIF and IPTC) "
                     "from image files to the database and also for storing them "

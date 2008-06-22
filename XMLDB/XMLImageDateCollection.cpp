b09e0c2da4beeec5d20dee3e47b405f94bb4531b
Jesper Pedersen <blackie@blackie.dk>
2008-06-22 16:04:03 +0000

    Replace linear search in XMLImageDateCollection with range index.
    svn path=/trunk/extragear/graphics/kphotoalbum/; revision=823120

--- XMLDB/XMLImageDateCollection.cpp
+++ XMLDB/XMLImageDateCollection.cpp
@@ -18,83 +18,112 @@
 
 #include "XMLImageDateCollection.h"
 #include "DB/ImageDB.h"
-//Added by qt3to4:
-#include <Q3ValueList>
 
-XMLImageDateCollection::XMLImageDateCollection()
-    : _dirtyLower( false ), _dirtyUpper( false )
+void XMLImageDateCollection::add( const DB::ImageDate& date )
 {
+    _startIndex.insertMulti(date.start(), date);
 }
 
-void XMLImageDateCollection::append( const DB::ImageDate& date )
-{
-    _dates.append( date );
-    _dirtyLower = true;
-    _dirtyUpper = true;
+void XMLImageDateCollection::buildIndex() {
+    StartIndexMap::ConstIterator startSearch = _startIndex.constBegin();
+    QDateTime biggestEnd = QDateTime( QDate( 1900, 1, 1 ) );
+    for (StartIndexMap::ConstIterator it = _startIndex.constBegin();
+         it != _startIndex.end();
+         ++it) {
+        // We want a monotonic mapping end-date -> smallest-in-start-index.
+        // Since we go through the start index sorted, lowest first, we just
+        // have to keep the last pointer as long as we find smaller end-dates.
+        // This should be rare as it only occurs if there are images that
+        // actually represent a range not just a point in time.
+        if (it.value().end() >= biggestEnd) {
+            biggestEnd = it.value().end();
+            startSearch = it;
+        }
+        _endIndex.insert(it.value().end(), startSearch);
+    }
 }
 
+/**
+   Previously, counting the elements was done by going through all elements
+   and count the matches for a particular range, this unfortunately had
+   O(n) complexity multiplied by m ranges we would get O(mn).
+
+   Henner Zeller rewrote it to its current state. The main idea now is to
+   have all dates sorted so that it is possible to only look at the
+   requested range. Since it is not points in time, we can't have just a
+   simple sorted list. So we have two sorted maps, the _startIndex and
+   _endIndex. _startIndex is sorted by the start time of all ImageDates
+   (which are in fact ranges)
+
+   If we would just look for Images that start _after_ the query-range, we
+   would miscount, because there might be Image ranges starting before the
+   query time but whose end time reaches into the query range this is what
+   the _endIndex is for: it is sortd by end-date; here we look for
+   everything that is >= our query start. its value() part is basically a
+   pointer to the position in the _startIndex where we actually have to
+   start looking.
+
+   The rest is simple: we determine the interesting start in
+   _startIndex using the _endIndex and iterate through it until the
+   elements in that sorted list have a start time that is larger than the
+   query-end-range .. there will no more elements coming.
+
+   The above uses the fact that a QMap::constIterator iterates the map in
+   sorted order.
+**/
 DB::ImageCount XMLImageDateCollection::count( const DB::ImageDate& range )
 {
     if ( _cache.contains( range ) )
         return _cache[range];
 
     int exact = 0, rangeMatch = 0;
-    for( Q3ValueList<DB::ImageDate>::Iterator it = _dates.begin(); it != _dates.end(); ++it ) {
-        DB::ImageDate::MatchType tp = (*it).isIncludedIn( range );
-        switch (tp) {
-        case DB::ImageDate::ExactMatch: exact++;break;
-        case DB::ImageDate::RangeMatch: rangeMatch++; break;
-        case DB::ImageDate::DontMatch: break;
+
+    // We start searching in ranges that overlap our start search range, i.e.
+    // where the end-date is higher than our search start.
+    EndIndexMap::ConstIterator endSearch = _endIndex.lowerBound(range.start());
+
+    if (endSearch != _endIndex.end()) {
+        for ( StartIndexMap::ConstIterator it = endSearch.value();
+              it != _startIndex.end() && it.key() < range.end();
+              ++it) {
+            DB::ImageDate::MatchType tp = it.value().isIncludedIn( range );
+            switch (tp) {
+            case DB::ImageDate::ExactMatch: exact++;break;
+            case DB::ImageDate::RangeMatch: rangeMatch++; break;
+            case DB::ImageDate::DontMatch: break;
+            }
         }
     }
 
     DB::ImageCount res( exact, rangeMatch );
-    _cache.insert( range, res );
+    _cache.insert( range, res );  // TODO(hzeller) this might go now
     return res;
 }
 
 QDateTime XMLImageDateCollection::lowerLimit() const
 {
-    static QDateTime _lower = QDateTime( QDate( 1900, 1, 1 ) );
-    if ( _dirtyLower && _dates.count() != 0 ) {
-        bool first = true;
-        for( Q3ValueList<DB::ImageDate>::ConstIterator it = _dates.begin(); it != _dates.end(); ++it ) {
-            if ( first ) {
-                _lower = (*it).start();
-                first = false;
-            }
-            else if ( (*it).start() < _lower )
-                _lower = (*it).start();
-        }
+    if (!_startIndex.empty()) {
+        return _startIndex.constBegin().key();
     }
-    _dirtyLower = false;
-    return _lower;
+    return QDateTime( QDate( 1900, 1, 1 ) );
 }
 
 QDateTime XMLImageDateCollection::upperLimit() const
 {
-    static QDateTime _upper = QDateTime( QDate( 2100, 1, 1 ) );
-    if ( _dirtyUpper && _dates.count() != 0 ) {
-        bool first = true;
-        for( Q3ValueList<DB::ImageDate>::ConstIterator it = _dates.begin(); it != _dates.end(); ++it ) {
-            if ( first ) {
-                _upper = (*it).end();
-                first = false;
-            }
-            else if ( (*it).end() > _upper ) {
-                _upper = (*it).end();
-            }
-        }
+    if (!_endIndex.empty()) {
+        EndIndexMap::ConstIterator highest = _endIndex.end();
+        --highest;
+        return highest.key();
     }
-    _dirtyUpper = false;
-    return _upper;
+    return QDateTime( QDate( 2100, 1, 1 ) );
 }
 
 XMLImageDateCollection::XMLImageDateCollection( const QStringList& list )
 {
     for( QStringList::ConstIterator it = list.begin(); it != list.end(); ++it ) {
         DB::ImageInfoPtr info = DB::ImageDB::instance()->info( *it );
-        append( info->date() );
+        add( info->date() );
     }
+    buildIndex();
 }
 

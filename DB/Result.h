f5f5b3263b1daf2c97e376d8af933c65c0b52809
Henner Zeller <h.zeller@acm.org>
2008-09-10 10:55:00 +0000

    Make copy constructor/assignment operator unaccessible.
    svn path=/trunk/extragear/graphics/kphotoalbum/; revision=859442

--- DB/Result.h
+++ DB/Result.h
@@ -28,16 +28,23 @@ class Result :public KShared
         };
         typedef ConstIterator const_iterator;
 
-        // Never use Result on a stack but rather new it and assign to
-        // some (Const)ResultPtr. The fact that the destructor is private will
-        // remind you about this ;)
+        /** Never use Result on a stack but rather new it and assign to
+         * some (Const)ResultPtr. The fact that the destructor is private will
+         * remind you about this ;) */
         Result();
+
+        /** Create a result with a list of file ids. */
         explicit Result( const QList<int>& ids );
+
+        /** Convenience constructor: create a result only one ResultId. */
         explicit Result( const DB::ResultId& );
 
         void append( const DB::ResultId& );
         void prepend( const DB::ResultId& );
+
+        /** Append the content of another DB::Result */
         void appendAll( const DB::Result& );
+
         DB::ResultId at(int index) const;
         int size() const;
         bool isEmpty() const;
@@ -50,7 +57,11 @@ class Result :public KShared
         // Noone must delete the Result directly. Only SharedPtr may.
         friend class KSharedPtr<Result>;
         friend class KSharedPtr<const Result>;
-        ~Result();  // Don't use Q_FOREACH on DB::Result
+        ~Result();  // Don't use Q_FOREACH on DB::Result.
+
+        // No implicit constructors/assignments.
+        Result(const DB::Result&);
+        DB::Result& operator=(const DB::Result&);
 
     private:
         QList<int> _items;

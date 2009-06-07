1b88033fa2fc0fac341a9100234f1c63eaf40cf1
Jesper Pedersen <blackie@blackie.dk>
2009-06-07 22:06:44 +0000

    Don't show the breadcrumb as a link
    svn path=/branches/kphotoalbum/browser-rework/; revision=978705

--- MainWindow/BreadcrumbViewer.cpp
+++ MainWindow/BreadcrumbViewer.cpp
@@ -6,8 +6,9 @@ void BreadcrumbViewer::setBreadcrumbs( const Browser::BreadcrumbList& list )
     _activeCrumbs = list.latest();
     QStringList tmp;
 
-    for ( int i = 0; i < _activeCrumbs.count(); ++i )
+    for ( int i = 0; i < _activeCrumbs.count()-1; ++i )
         tmp.append( QString::fromLatin1("<a href=\"%1\">%2</a>").arg(i).arg(_activeCrumbs[i].text()) );
+    tmp.append(_activeCrumbs[_activeCrumbs.count()-1].text());
 
     QString text = tmp.join(QString::fromLatin1(" > ") );
 

16c62f6e58260a19ccdbb298bafeee18ab57f017
Jesper Pedersen <blackie@blackie.dk>
2009-07-18 08:22:13 +0000

    updated copyright headers
    svn path=/branches/kphotoalbum/devel/; revision=998606

--- ThumbnailView/ThumbnailCache.h
+++ ThumbnailView/ThumbnailCache.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2008 Henner Zeller <h.zeller@acm.org>
+/* Copyright (C) 2008-2009 Henner Zeller <h.zeller@acm.org>
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public

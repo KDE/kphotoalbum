d5bd7f6fd942bf177954633fe7df67edb8c2b567
Miika Turkia <miika.turkia@gmail.com>
2012-03-31 21:15:25 +0300

    KPA 4.2 released

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@
 extern QTemporaryFile* _tmpFileForThumbnailView;
 
 int main( int argc, char** argv ) {
-    KAboutData aboutData( "kphotoalbum", 0, ki18n("KPhotoAlbum"), "GIT",
+    KAboutData aboutData( "kphotoalbum", 0, ki18n("KPhotoAlbum"), "4.2",
                           ki18n("KDE Photo Album"), KAboutData::License_GPL,
                           KLocalizedString(), KLocalizedString(), "http://www.kphotoalbum.org");
     aboutData.addAuthor( ki18n("Jesper K. Pedersen"), ki18n("Development"), "blackie@kde.org" );

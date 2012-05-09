fae5245ad140a07736262219c3c75fd71db775f5
Jesper K. Pedersen <blackie@kde.org>
2012-05-09 14:33:02 +0200

    introduced new class DB::FileName

---
+++ DB/FileName.h
@@ -0,0 +1,51 @@
+/* Copyright 2012 Jesper K. Pedersen <blackie@kde.org>
+  
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of
+   the License or (at your option) version 3 or any later version
+   accepted by the membership of KDE e.V. (or its successor approved
+   by the membership of KDE e.V.), which shall act as a proxy
+   defined in Section 14 of version 3 of the license.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef FILENAME_H
+#define FILENAME_H
+
+#include <QString>
+
+namespace DB
+{
+
+class FileName
+{
+public:
+    FileName();
+    static FileName fromAbsolutePath( const QString& fileName );
+    static FileName fromRelativePath( const QString& fileName );
+    QString absolute() const;
+    QString relative() const;
+    bool isEmpty() const;
+    bool isNull() const;
+    bool operator==( const FileName& other ) const;
+    bool operator!=( const FileName& other ) const;
+
+private:
+    // During previous profilation it showed that converting between absolute and relative took quite some time,
+    // so to avoid that, I store both.
+    QString m_relativePath;
+    QString m_absoluteFilePath;
+    bool m_isNull;
+};
+
+}
+
+#endif // FILENAME_H

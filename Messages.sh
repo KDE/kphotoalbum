eb27a2ed0d900b84d85cd4956b2e540b97a4093c
Pino Toscano <pino@kde.org>
2013-01-20 16:08:23 +0100

    Messages.sh: extract .ui and .rc files in all the subdirs

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 $PREPARETIPS > tips.cc
-$EXTRACTRC *.rc >> rc.cpp
+$EXTRACTRC `find . -name \*.ui -o -name \*.rc` >> rc.cpp
 $XGETTEXT `find . -name \*.cpp -o -name \*.cc -o -name \*.h | grep -v kexi` -o $podir/kphotoalbum.pot
 rm -f tips.cc

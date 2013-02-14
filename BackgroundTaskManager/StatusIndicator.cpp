2042f68da54bb4cd858a2daeb75bc7278b9df738
Johannes Zarl <isilmendil@isilme.lupus.uberspace.de>
2013-02-14 18:48:29 +0100

    krazy: fix validity of i18n call.

--- BackgroundTaskManager/StatusIndicator.cpp
+++ BackgroundTaskManager/StatusIndicator.cpp
@@ -92,9 +92,9 @@ void StatusIndicator::showToolTip(QHelpEvent* event)
                  "Color codes:"
                  "<ul><li><b>blinking green</b>: Active background jobs</li>"
                  "<li><b>gray</b>: No active jobs</li>"
-                 "<li><b>solid yellow</b>: Job queue is paused<li/>"
+                 "<li><b>solid yellow</b>: Job queue is paused</li>"
                  "<li><b>blinking yellow</b>: Job queue is paused for background jobs, but is executing a foreground job "
-                 "(like extracting a thumbnail for a video file, which is currently shown in the thumbnail viewer)</li></p>"
+                 "(like extracting a thumbnail for a video file, which is currently shown in the thumbnail viewer)</li></ul></p>"
                  ,activeCount,pendingCount);
     QToolTip::showText(event->globalPos(), text);
 }

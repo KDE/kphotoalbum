094dc7363e6fb343a1c17407f36b75952d23ef1f
Tobias Leupold <tobias.leupold@web.de>
2015-01-02 23:12:02 +0100

    Ported findmediabytag.py and focal-length-hist.py to Python 3.

--- script/contrib/findmediabytag.py
+++ script/contrib/findmediabytag.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
-"""
-Script for finding all media items, which have some tag, from
-KPhotoAlbum index.xml.
-"""
+#!/usr/bin/env python3
+
 # Copyright (C) 2006 Tuomas Suutari <thsuut@utu.fi>
+# Copyright (C) 2015 Tobias Leupold <tobias.leupold@web.de>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,49 +18,36 @@ KPhotoAlbum index.xml.
 # Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 # MA 02110-1301 USA.
 
+"""
+Script for finding all media items, which have some tag, from
+KPhotoAlbum index.xml.
+"""
+
 import sys
-from locale import getpreferredencoding
 from KPhotoAlbum import xmldb
 from KPhotoAlbum.datatypes import Tag
 
-def printn(msg, stream=sys.stdout):
-	"""
-	Print to stdout without new line.
-	"""
-	stream.write(msg)
-	stream.flush()
+def main():
+    # Parse command line parameters
+    if len(sys.argv) != 4:
+        print('Usage: {} <XML file> <category name> <item name>'.format(sys.argv[0]),
+            file = sys.stderr)
+        return 1
 
-def main(argv):
-	try:
-		me = argv[0]
-	except:
-		me = 'findmediabytag.py'
-		argv = [me]
+    print('Parsing the XML file ... ', end = '', file = sys.stderr)
 
-	# Parse command line parameters
-	if len(argv) != 4:
-		printn('Usage: ' + me +
-		      ' xml_file category_name item_name\n', sys.stderr)
-		return 1
-	xml_file = argv[1]
-	category_name = unicode(argv[2], getpreferredencoding())
-	item_name = unicode(argv[3], getpreferredencoding())
+    try:
+        db = xmldb.XMLDatabase(sys.argv[1])
+    except:
+        print('failed', file = sys.stderr)
+        print(sys.exc_info()[1], file = sys.stderr)
+        return 2
 
-	printn('Parsing the XML file...', sys.stderr)
-	try:
-		db = xmldb.XMLDatabase(xml_file)
-	except xmldb.Error, (e):
-		printn('failed.\n', sys.stderr)
-		printn(str(e) + '\n', sys.stderr)
-		return 2
-	printn('parsed.\n', sys.stderr)
+    print('done.', file = sys.stderr)
 
-	for i in db.mediaItems:
-		if Tag(category_name, item_name) in i.tags:
-			print i.filename
-
-	return 0
-
+    for item in db.mediaItems:
+        if Tag(sys.argv[2], sys.argv[3]) in item.tags:
+            print(item.filename)
 
 if __name__ == '__main__':
-	sys.exit(main(sys.argv))
+    sys.exit(main())
--- script/contrib/focal-length-hist.py
+++ script/contrib/focal-length-hist.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-A simple script showing ASCII-art histogram of lens focal lengths
-"""
+#!/usr/bin/env python3
 
 # Copyright (C) 2009 Jan Kundrát <jkt@flaska.net>
+# Copyright (C) 2015 Tobias Leupold <tobias.leupold@web.de>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,35 +18,42 @@ A simple script showing ASCII-art histogram of lens focal lengths
 # Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 # MA 02110-1301 USA.
 
+"""
+A simple script showing ASCII-art histogram of lens focal lengths
+"""
+
 import sys
 import sqlite3
 
-def load_data( fname, condition=None ):
-    conn = sqlite3.connect( fname )
+def load_data(fname, condition = None):
+    conn = sqlite3.connect(fname)
     cur = conn.cursor()
     query = 'select distinct Exif_Photo_FocalLength, count(*) from exif '
     args = []
+
     if condition is not None:
         query += 'where filename like ? '
         args += [ condition ]
+
     query += 'group by Exif_Photo_FocalLength;'
 
     cur.execute( query, args )
-    return [ [ row[0], row[1] ] for row in cur ]
+    return [[row[0], row[1]] for row in cur]
 
-def print_histogram( data ):
-    boundary = max( x[1] for x in data )
+def print_histogram(data):
+    boundary = max(x[1] for x in data)
+
     for row in data:
-        stars = int( 80.0 * row[1]/boundary ) * '*'
-        print "%s\t%s" % ( row[0], stars )
+        stars = int(80.0 * row[1] / boundary) * '*'
+        print('{value:9} {bar}'.format(value = row[0], bar = stars))
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
-        data = load_data( sys.argv[1] )
+        data = load_data(sys.argv[1])
     elif len(sys.argv) == 3:
-        data = load_data( sys.argv[1], sys.argv[2] )
+        data = load_data(sys.argv[1], sys.argv[2])
     else:
-        print "Usage: %s path/to/your/images/exif-info.db [filter]" % sys.argv[0]
+        print('Usage: {} <path to exif-info.db> [filter]'.format(sys.argv[0]))
         sys.exit(1)
 
-    print_histogram( data )
+    print_histogram(data)

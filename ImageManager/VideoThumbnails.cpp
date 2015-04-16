f20bc1b127ac32c30bf984e1f0ecc0bc11bb3d63
Rex Dieter <rdieter@math.unl.edu>
2015-04-15 22:02:33 -0500

    fix gcc5 FTBFS
    ImageManager/VideoThumbnails.cpp:47:22:
    error: converting to ‘bool’ from ‘std::nullptr_t’ requires direct-initialization [-fpermissive]
         m_pendingRequest = nullptr;
    
    https://gcc.gnu.org/gcc-5/porting_to.html says
    "... It is recommended to use the false keyword instead of converting nullptr
    to bool."

--- ImageManager/VideoThumbnails.cpp
+++ ImageManager/VideoThumbnails.cpp
@@ -44,7 +44,7 @@ void ImageManager::VideoThumbnails::setVideoFile(const DB::FileName &fileName)
         return;
 
     cancelPreviousJobs();
-    m_pendingRequest = nullptr;
+    m_pendingRequest = false;
     for ( int i= 0; i < 10; ++i )
         m_cache[i] = QImage();
 

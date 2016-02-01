8a395e04cd486b2036ea331b0a7d78f7a3c7c18a
Tobias Leupold <tobias.leupold@web.de>
2016-02-01 22:55:05 +0100

    Re-added searching for un-localized special category names when changing a category's name.

--- Settings/CategoryPage.cpp
+++ Settings/CategoryPage.cpp
@@ -265,8 +265,14 @@ void Settings::CategoryPage::categoryNameChanged(QListWidgetItem* item)
     }
 
     // We don't want to have special category names.
+    // We do have to search both for the localized version and the C locale version, because a user
+    // could start KPA e. g. with a German locale and create a "Folder" category (which would not
+    // be catched by i18n("Folder")), and then start KPA with the C locale, which would produce a
+    // doubled "Folder" category.
     if (newCategoryName == i18n("Folder")
-        || newCategoryName == i18n("Media Type")) {
+        || newCategoryName == QString::fromUtf8("Folder")
+        || newCategoryName == i18n("Media Type")
+        || newCategoryName == QString::fromUtf8("Media Type")) {
 
         resetCategory(item);
         KMessageBox::sorry(this,

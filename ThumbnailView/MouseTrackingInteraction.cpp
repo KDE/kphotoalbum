KDE/kphotoalbum

commit 8dbfadf43ad0d8504f2a9b0987be0c89ebde3c2b

Fix uninitialized static variable.
Interestingly enough, this crashed only on platform i386.
Thanks to Matthias Heukäufer for reporting the bug.

ThumbnailView/MouseTrackingInteraction.cpp
@@ -57,10 +57,15 @@ void ThumbnailView::MouseTrackingInteraction::handleCursorOverNewIcon()
 {
     static DB::FileName lastFileNameUnderCursor;
     const DB::FileName fileName = widget()->mediaIdUnderCursor();
+    Q_ASSERT( !fileName.isNull());
+
     if ( fileName != lastFileNameUnderCursor ) {
-        emit fileIdUnderCursorChanged(fileName);
-        model()->updateCell(lastFileNameUnderCursor);
-        model()->updateCell(fileName);
+        if ( !lastFileNameUnderCursor.isNull() )
+        {
+            emit fileIdUnderCursorChanged(fileName);
+            model()->updateCell(lastFileNameUnderCursor);
+            model()->updateCell(fileName);
+        }
         lastFileNameUnderCursor = fileName;
     }
 }

commit f1dd9d9c4b02afe8972f41339cc1bdf9a7a4372a

Fix regression (incorrect assertion).
Commit 8dbfadf4 introduced a regression (a phony assertion).

ThumbnailView/MouseTrackingInteraction.cpp
@@ -57,7 +57,6 @@ void ThumbnailView::MouseTrackingInteraction::handleCursorOverNewIcon()
 {
     static DB::FileName lastFileNameUnderCursor;
     const DB::FileName fileName = widget()->mediaIdUnderCursor();
-    Q_ASSERT( !fileName.isNull());
 
     if ( fileName != lastFileNameUnderCursor ) {
         if ( !lastFileNameUnderCursor.isNull() )
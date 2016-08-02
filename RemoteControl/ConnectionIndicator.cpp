54e379bf7c40da2d3e1b24fb36c7a595cabce94f
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2016-08-03 00:57:24 +0200

    Replace KIcon with QIcon.

--- RemoteControl/ConnectionIndicator.cpp
+++ RemoteControl/ConnectionIndicator.cpp
@@ -17,16 +17,17 @@
 */
 
 #include "ConnectionIndicator.h"
-#include <KIcon>
 #include "RemoteInterface.h"
-#include <QDialog>
-#include <QTimer>
-#include <QLabel>
+#include <Settings/SettingsData.h>
+
 #include <KLocale>
-#include <QLineEdit>
+#include <QDialog>
 #include <QHBoxLayout>
 #include <QHostAddress>
-#include "Settings/SettingsData.h"
+#include <QIcon>
+#include <QLabel>
+#include <QLineEdit>
+#include <QTimer>
 #include <QValidator>
 
 namespace RemoteControl {
@@ -115,14 +116,16 @@ void ConnectionIndicator::on()
 {
     m_state = On;
     m_timer->stop();
-    setPixmap(KIcon(QString::fromUtf8("network-wireless")).pixmap(32,32));
+    QIcon icon { QIcon::fromTheme(QString::fromUtf8("network-wireless")) };
+    setPixmap(icon.pixmap(32,32));
 }
 
 void ConnectionIndicator::off()
 {
     m_timer->stop();
     m_state = Off;
-    setPixmap(KIcon(QString::fromUtf8("network-disconnect")).pixmap(32,32));
+    QIcon icon { QIcon::fromTheme(QString::fromUtf8("network-disconnect")) };
+    setPixmap(icon.pixmap(32,32));
 }
 
 void ConnectionIndicator::wait()
@@ -136,11 +139,11 @@ void ConnectionIndicator::waitingAnimation()
     static int index = 0;
     static QList<QPixmap> icons;
     if (icons.isEmpty()) {
-        icons.append(KIcon(QString::fromUtf8("network-wireless-disconnected")).pixmap(32,32));
-        icons.append(KIcon(QString::fromUtf8("network-wireless-connected-25")).pixmap(32,32));
-        icons.append(KIcon(QString::fromUtf8("network-wireless-connected-50")).pixmap(32,32));
-        icons.append(KIcon(QString::fromUtf8("network-wireless-connected-75")).pixmap(32,32));
-        icons.append(KIcon(QString::fromUtf8("network-wireless")).pixmap(32,32));
+        icons.append(QIcon::fromTheme(QString::fromUtf8("network-wireless-disconnected")).pixmap(32,32));
+        icons.append(QIcon::fromTheme(QString::fromUtf8("network-wireless-connected-25")).pixmap(32,32));
+        icons.append(QIcon::fromTheme(QString::fromUtf8("network-wireless-connected-50")).pixmap(32,32));
+        icons.append(QIcon::fromTheme(QString::fromUtf8("network-wireless-connected-75")).pixmap(32,32));
+        icons.append(QIcon::fromTheme(QString::fromUtf8("network-wireless")).pixmap(32,32));
     }
     index = (index+1) % icons.count();
     setPixmap(icons[index]);

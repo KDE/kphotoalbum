KDE/kphotoalbum

commit 0e9796c558dd1b5cbf8e6a4fdf2bfc564eb12433

Use ffmpeg/ffprobe for video if available.

ImageManager/AsyncLoader.cpp
@@ -77,19 +77,18 @@ bool ImageManager::AsyncLoader::load( ImageRequest* request )
         return false;
 
     if ( Utilities::isVideo( request->fileSystemFileName() ) ) {
-        if ( MainWindow::FeatureDialog::mplayerBinary().isNull() )
+        if (!loadVideo( request ));
             return false;
-        loadVideo( request );
     } else {
         loadImage( request );
     }
     return true;
 }
 
-void ImageManager::AsyncLoader::loadVideo( ImageRequest* request)
+bool ImageManager::AsyncLoader::loadVideo( ImageRequest* request)
 {
-    if ( MainWindow::FeatureDialog::mplayerBinary().isNull() )
-        return;
+    if ( ! MainWindow::FeatureDialog::hasVideoThumbnailer() )
+        return false;
 
     BackgroundTaskManager::Priority priority =
             (request->priority() > ThumbnailInvisible)
@@ -98,6 +97,7 @@ void ImageManager::AsyncLoader::loadVideo( ImageRequest* request)
 
     BackgroundTaskManager::JobManager::instance()->addJob(
                 new BackgroundJobs::HandleVideoThumbnailRequestJob(request,priority));
+    return true;
 }
 
 void ImageManager::AsyncLoader::loadImage( ImageRequest* request )

DB/NewImageFinder.cpp
@@ -80,7 +80,7 @@ bool NewImageFinder::findImages()
     }
 
     // Man this is not super optimal, but will be changed onces the image finder moves to become a background task.
-    if ( ! MainWindow::FeatureDialog::mplayerBinary().isNull() ) {
+    if ( MainWindow::FeatureDialog::hasVideoThumbnailer() ) {
         BackgroundTaskManager::JobManager::instance()->addJob(
                 new BackgroundJobs::SearchForVideosWithoutVideoThumbnailsJob );
     }
@@ -172,7 +172,7 @@ void NewImageFinder::loadExtraFiles()
     DB::ImageDB::instance()->addImages( newImages );
 
     // I would have loved to do this in loadExtraFile, but the image has not been added to the database yet
-    if ( ! MainWindow::FeatureDialog::mplayerBinary().isNull() ) {
+    if ( MainWindow::FeatureDialog::hasVideoThumbnailer() ) {
         Q_FOREACH( const ImageInfoPtr& info, newImages ) {
             if ( info->isVideo() )
                 BackgroundTaskManager::JobManager::instance()->addJob(

ImageManager/AsyncLoader.h
@@ -52,7 +52,7 @@ public:
 
 protected:
     virtual void customEvent( QEvent* ev );
-    void loadVideo( ImageRequest* );
+    bool loadVideo( ImageRequest* );
     void loadImage( ImageRequest* );
 
 private:

ImageManager/ExtractOneVideoFrame.cpp
@@ -41,7 +41,7 @@ QString ExtractOneVideoFrame::s_tokenForShortVideos;
 #define STR(x) QString::fromUtf8(x)
 void ExtractOneVideoFrame::extract(const DB::FileName &fileName, double offset, QObject* receiver, const char* slot)
 {
-    if ( ! MainWindow::FeatureDialog::mplayerBinary().isEmpty())
+    if ( MainWindow::FeatureDialog::hasVideoThumbnailer())
         new ExtractOneVideoFrame(fileName, offset, receiver, slot);
 }
 
@@ -56,12 +56,24 @@ ExtractOneVideoFrame::ExtractOneVideoFrame(const DB::FileName &fileName, double 
     connect( m_process, SIGNAL(error(QProcess::ProcessError)), this, SLOT(handleError(QProcess::ProcessError)));
     connect( this, SIGNAL(result(QImage)), receiver, slot);
 
-    QStringList arguments;
-    arguments << STR("-nosound") << STR("-ss") << QString::number(offset,'f',4) << STR("-vf")
-              << STR("screenshot") << STR("-frames") << STR("20") << STR("-vo") << STR("png:z=9") << fileName.absolute();
-    //qDebug( "%s %s", qPrintable(MainWindow::FeatureDialog::mplayerBinary()), qPrintable(arguments.join(QString::fromLatin1(" "))));
+    if (MainWindow::FeatureDialog::ffmpegBinary().isEmpty())
+    {
+        QStringList arguments;
+        arguments << STR("-nosound") << STR("-ss") << QString::number(offset,'f',4) << STR("-vf")
+                  << STR("screenshot") << STR("-frames") << STR("20") << STR("-vo") << STR("png:z=9") << fileName.absolute();
+        //qDebug( "%s %s", qPrintable(MainWindow::FeatureDialog::mplayerBinary()), qPrintable(arguments.join(QString::fromLatin1(" "))));
 
-    m_process->start(MainWindow::FeatureDialog::mplayerBinary(), arguments);
+        m_process->start(MainWindow::FeatureDialog::mplayerBinary(), arguments);
+    } else {
+        QStringList arguments;
+        arguments << STR("-i") << fileName.absolute()
+                  << STR("-sn") << STR("-an") << STR("-ss") << QString::number(offset,'f',4)
+                  << STR("-vframes") << STR("20")
+                  << m_workingDirectory + STR("/000000%02d.png");
+        //qDebug( "%s %s", qPrintable(MainWindow::FeatureDialog::ffmpegBinary()), qPrintable(arguments.join(QString::fromLatin1(" "))));
+
+        m_process->start(MainWindow::FeatureDialog::ffmpegBinary(), arguments);
+    }
 }
 
 void ExtractOneVideoFrame::frameFetched()
@@ -73,7 +85,10 @@ void ExtractOneVideoFrame::frameFetched()
     for (int i = 20; i>0; --i) {
         name = m_workingDirectory +STR("/000000%1.png").arg(i,2, 10, QChar::fromLatin1('0'));
         if (QFile::exists(name))
+        {
+            //qDebug() << "Using video frame " << i;
             break;
+        }
     }
 
     QImage image(name);

ImageManager/VideoLengthExtractor.cpp
@@ -46,7 +46,7 @@ void ImageManager::VideoLengthExtractor::extract(const DB::FileName &fileName)
         m_process = nullptr;
     }
 
-    if (MainWindow::FeatureDialog::mplayerBinary().isEmpty()) {
+    if (!MainWindow::FeatureDialog::hasVideoThumbnailer()) {
         emit unableToDetermineLength();
         return;
     }
@@ -55,11 +55,23 @@ void ImageManager::VideoLengthExtractor::extract(const DB::FileName &fileName)
     m_process->setWorkingDirectory(QDir::tempPath());
     connect( m_process, SIGNAL(finished(int)), this, SLOT(processEnded()));
 
-    QStringList arguments;
-    arguments << STR("-identify") << STR("-frames") << STR("0") << STR("-vc") << STR("null")
-              << STR("-vo") << STR("null") << STR("-ao") << STR("null") <<  fileName.absolute();
+    if (MainWindow::FeatureDialog::ffmpegBinary().isEmpty())
+    {
+        QStringList arguments;
+        arguments << STR("-identify") << STR("-frames") << STR("0") << STR("-vc") << STR("null")
+                  << STR("-vo") << STR("null") << STR("-ao") << STR("null") <<  fileName.absolute();
 
-    m_process->start(MainWindow::FeatureDialog::mplayerBinary(), arguments);
+        m_process->start(MainWindow::FeatureDialog::mplayerBinary(), arguments);
+    } else {
+        QStringList arguments;
+        arguments << STR("-v") << STR("error") << STR("-select_streams") << STR("v:0")
+                  << STR("-show_entries") << STR("stream=duration")
+                  << STR("-of") << STR("default=noprint_wrappers=1:nokey=1")
+                  <<  fileName.absolute();
+
+        //qDebug( "%s %s", qPrintable(MainWindow::FeatureDialog::ffprobeBinary()), qPrintable(arguments.join(QString::fromLatin1(" "))));
+        m_process->start(MainWindow::FeatureDialog::ffprobeBinary(), arguments);
+    }
 }
 
 void ImageManager::VideoLengthExtractor::processEnded()
@@ -67,30 +79,45 @@ void ImageManager::VideoLengthExtractor::processEnded()
     if ( !m_process->stderr().isEmpty() )
         Debug() << m_process->stderr();
 
-    QStringList list = m_process->stdout().split(QChar::fromLatin1('\n'));
-    list = list.filter(STR("ID_LENGTH="));
-    if ( list.count() == 0 ) {
-        qWarning() << "Unable to find ID_LENGTH in output from MPlayer for file " << m_fileName.absolute() << "\n"
-                   << "Output was:\n"
-                   << m_process->stdout();
-        emit unableToDetermineLength();
-        return;
+    QString lenStr;
+    if (MainWindow::FeatureDialog::ffmpegBinary().isEmpty())
+    {
+        QStringList list = m_process->stdout().split(QChar::fromLatin1('\n'));
+        list = list.filter(STR("ID_LENGTH="));
+        if ( list.count() == 0 ) {
+            qWarning() << "Unable to find ID_LENGTH in output from MPlayer for file " << m_fileName.absolute() << "\n"
+                       << "Output was:\n"
+                       << m_process->stdout();
+            emit unableToDetermineLength();
+            return;
+        }
+
+        const QString match = list[0];
+        const QRegExp regexp(STR("ID_LENGTH=([0-9.]+)"));
+        if (!regexp.exactMatch(match))
+        {
+            qWarning() << STR("Unable to match regexp for string: %1 (for file %2)").arg(match).arg(m_fileName.absolute());
+            emit unableToDetermineLength();
+            return;
+        }
+
+        lenStr = regexp.cap(1);
+    } else {
+        QStringList list = m_process->stdout().split(QChar::fromLatin1('\n'));
+        if ( list.count() != 2 ) {
+            qWarning() << "Unable to parse video length from ffprobe output!"
+                       << "Output was:\n"
+                       << m_process->stdout();
+            emit unableToDetermineLength();
+            return;
+        }
+        lenStr = list[0].trimmed();
     }
 
-    const QString match = list[0];
-    const QRegExp regexp(STR("ID_LENGTH=([0-9.]+)"));
-    bool ok = regexp.exactMatch(match);
+    bool ok = false;
+    const double length = lenStr.toDouble(&ok);
     if ( !ok ) {
-        qWarning() << STR("Unable to match regexp for string: %1 (for file %2)").arg(match).arg(m_fileName.absolute());
-        emit unableToDetermineLength();
-        return;
-    }
-
-    const QString cap = regexp.cap(1);
-
-    const double length = cap.toDouble(&ok);
-    if ( !ok ) {
-        qWarning() << STR("Unable to convert string \"%1\"to integer (for file %2)").arg(cap).arg(m_fileName.absolute());
+        qWarning() << STR("Unable to convert string \"%1\"to double (for file %2)").arg(lenStr).arg(m_fileName.absolute());
         emit unableToDetermineLength();
         return;
     }

ImageManager/VideoThumbnails.cpp
@@ -39,7 +39,7 @@ void ImageManager::VideoThumbnails::setVideoFile(const DB::FileName &fileName)
         return;
 
     // no video thumbnails without mplayer:
-    if (MainWindow::FeatureDialog::mplayerBinary().isEmpty())
+    if (!MainWindow::FeatureDialog::hasVideoThumbnailer())
         return;
 
     cancelPreviousJobs();

MainWindow/FeatureDialog.cpp
@@ -112,11 +112,18 @@ FeatureDialog::FeatureDialog( QWidget* parent )
         text += i18n("<p>Phonon is capable of playing movies of these mime types:<ul><li>%1</li></ul></p>", mimeTypes.join(QString::fromLatin1( "</li><li>" ) ) );
 
     text += i18n("<h1><a name=\"videoPreview\">Video thumbnail support</a></h1>"
-                 "<p>KPhotoAlbum uses <tt>MPlayer</tt> to extract thumbnails from videos. These thumbnails are used to preview "
+                 "<p>KPhotoAlbum can use <tt>ffmpeg</tt> or <tt>MPlayer</tt> to extract thumbnails from videos. These thumbnails are used to preview "
                  "videos in the thumbnail viewer.</p>"
-                 "<p>If at all possible you should install the <b>MPlayer2</b> package rather than the <b>MPlayer</b> package, as it has important "
-                 "improvements over the MPlayer package. MPlayer (in contrast to MPlayer2) often has problems extracting the length "
-                 "of videos and also often fails to extract the thumbnails used for cycling video thumbnails.</p>");
+                 "<p>In the past, MPlayer (in contrast to MPlayer2) often had problems extracting the length "
+                 "of videos and also often fails to extract the thumbnails used for cycling video thumbnails."
+                 "For that reason, you should prefer ffmpeg or MPlayer2 over MPlayer, if possible.</p>"
+                 );
+
+    text += i18n("<h1><a name=\"videoInfo\">Video metadata support</a></h1>"
+                 "<p>KPhotoAlbum can use <tt>ffprobe</tt> or <tt>MPlayer</tt> to extract length information from videos."
+                 "</p>"
+                 "<p>Correct length information is also necessary for correct rendering of video thumbnails.</p>"
+                 );
 
     browser->setText( text );
 
@@ -194,10 +201,32 @@ bool FeatureDialog::isMplayer2()
     return output.contains(QString::fromLatin1("MPlayer2"));
 }
 
+QString FeatureDialog::ffmpegBinary()
+{
+    QString ffmpeg = QStandardPaths::findExecutable( QString::fromLatin1("ffmpeg"));
+    return ffmpeg;
+}
+
+QString FeatureDialog::ffprobeBinary()
+{
+    QString ffprobe = QStandardPaths::findExecutable( QString::fromLatin1("ffprobe"));
+    return ffprobe;
+}
+
+bool FeatureDialog::hasVideoThumbnailer()
+{
+    return ! ( ffmpegBinary().isEmpty() && mplayerBinary().isEmpty());
+}
+
+bool FeatureDialog::hasVideoProber()
+{
+    return ! ( ffprobeBinary().isEmpty() && mplayerBinary().isEmpty());
+}
+
 bool MainWindow::FeatureDialog::hasAllFeaturesAvailable()
 {
     // Only answer those that are compile time tests, otherwise we will pay a penalty each time we start up.
-    return hasKIPISupport() && hasEXIV2Support() && hasEXIV2DBSupport() && hasKfaceSupport() && hasGeoMapSupport() && !mplayerBinary().isNull() && isMplayer2();
+    return hasKIPISupport() && hasEXIV2Support() && hasEXIV2DBSupport() && hasKfaceSupport() && hasGeoMapSupport() && hasVideoThumbnailer() && hasVideoProber();
 }
 
 struct Data
@@ -222,6 +251,7 @@ QString MainWindow::FeatureDialog::featureString()
 
     QString result = QString::fromLatin1("<p><table>");
     const QString red = QString::fromLatin1("<font color=\"red\">%1</font>");
+    const QString yellow = QString::fromLatin1("<font color=\"yellow\">%1</font>");
     const QString yes = i18nc("Feature available","Yes");
     const QString no =  red.arg( i18nc("Feature not available","No") );
     const QString formatString = QString::fromLatin1( "<tr><td><a href=\"%1\">%2</a></td><td><b>%3</b></td></tr>" );
@@ -230,8 +260,11 @@ QString MainWindow::FeatureDialog::featureString()
                   .arg( (*featureIt).tag ).arg( (*featureIt).title ).arg( (*featureIt).featureFound ? yes : no  );
     }
 
-     QString thumbnailSupport = mplayerBinary().isNull() ? no : ( isMplayer2() ? yes : red.arg(i18n("Only with MPlayer1")));
+     QString thumbnailSupport = hasVideoThumbnailer() ? ( !ffmpegBinary().isEmpty() || isMplayer2() ? yes : yellow.arg(i18n("Only with MPlayer1"))) : no ;
     result += formatString.arg(QString::fromLatin1("#videoPreview")).arg(i18n("Video thumbnail support")).arg(thumbnailSupport);
+
+     QString videoinfoSupport = hasVideoProber() ? yes : no;
+    result += formatString.arg(QString::fromLatin1("#videoInfo")).arg(i18n("Video metadata support")).arg(videoinfoSupport);
     result += QString::fromLatin1( "</table></p>" );
 
     return result;

MainWindow/FeatureDialog.h
@@ -38,6 +38,18 @@ public:
     static QStringList supportedVideoMimeTypes();
     static QString mplayerBinary();
     static bool isMplayer2();
+    static QString ffmpegBinary();
+    static QString ffprobeBinary();
+    /**
+     * @brief hasVideoThumbnailer
+     * @return true, if a program capable of creating video thumbnails is found, false otherwise
+     */
+    static bool hasVideoThumbnailer();
+    /**
+     * @brief hasVideoProber
+     * @return  true, if a program capable of extracting video metadata is found, false otherwise
+     */
+    static bool hasVideoProber();
 
 protected:
     static bool hasKIPISupport();

commit 562e680242a8b4f7a74a3265364ac39abdd2e6b5

Fix regression introduced by 0e9796c.

ImageManager/AsyncLoader.cpp
@@ -77,7 +77,7 @@ bool ImageManager::AsyncLoader::load( ImageRequest* request )
         return false;
 
     if ( Utilities::isVideo( request->fileSystemFileName() ) ) {
-        if (!loadVideo( request ));
+        if (!loadVideo( request ))
             return false;
     } else {
         loadImage( request );
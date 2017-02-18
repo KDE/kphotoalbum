KDE/kphotoalbum

commit 8d0b178e84cc0961988f77fd694a4c5112f3be26

Rewrite iterators using Q_FOREACH.
At the same time, I have also reviewed constness of the iterators and
made some of them const.

DB/MemberMap.cpp
@@ -134,7 +134,10 @@ void MemberMap::calculate() const
         QMap<QString, StringSet> groupMap = categoryIt.value();
 
         // Run through each of the groups for the given categories
-        for( QMap<QString,StringSet>::Iterator groupIt= groupMap.begin(); groupIt != groupMap.end(); ++groupIt ) {
+        for( QMap<QString,StringSet>::const_iterator groupIt= groupMap.constBegin()
+             ; groupIt != groupMap.constEnd()
+             ; ++groupIt )
+        {
             QString group = groupIt.key();
             if ( m_closureMembers[category].find( group ) == m_closureMembers[category].end() ) {
                 (void) calculateClosure( m_closureMembers[category], category, group );
@@ -156,8 +159,7 @@ void MemberMap::renameGroup( const QString& category, const QString& oldName, co
     QMap<QString, StringSet>& groupMap = m_members[category];
     groupMap.insert(newName,m_members[category][oldName] );
     groupMap.remove( oldName );
-    for( QMap<QString,StringSet>::Iterator it= groupMap.begin(); it != groupMap.end(); ++it ) {
-        StringSet& set = it.value();
+    Q_FOREACH( StringSet set, groupMap ) {
         if ( set.contains( oldName ) ) {
             set.remove( oldName );
             set.insert( newName );
@@ -176,8 +178,7 @@ void MemberMap::deleteItem( DB::Category* category, const QString& name)
     if ( !m_loading )
         emit dirty();
     QMap<QString, StringSet>& groupMap = m_members[category->name()];
-    for( QMap<QString,StringSet>::Iterator it= groupMap.begin(); it != groupMap.end(); ++it ) {
-        StringSet& items = it.value();
+    Q_FOREACH( StringSet items, groupMap ) {
         items.remove( name );
     }
     m_members[category->name()].remove(name);
@@ -192,8 +193,7 @@ void MemberMap::renameItem( DB::Category* category, const QString& oldName, cons
     if ( !m_loading )
         emit dirty();
     QMap<QString, StringSet>& groupMap = m_members[category->name()];
-    for( QMap<QString,StringSet>::Iterator it= groupMap.begin(); it != groupMap.end(); ++it ) {
-        StringSet& items = it.value();
+    Q_FOREACH( StringSet items, groupMap ) {
         if (items.contains( oldName ) ) {
             items.remove( oldName );
             items.insert( newName );

AnnotationDialog/Dialog.cpp
@@ -447,13 +447,13 @@ void AnnotationDialog::Dialog::slotCopyPrevious()
     m_lastSelectedPositionableTag.second = QString();
     QList<ResizableFrame *> allAreas = m_preview->preview()->findChildren<ResizableFrame *>();
 
-    for( QList<ListSelect*>::Iterator it = m_optionList.begin(); it != m_optionList.end(); ++it ) {
-        (*it)->setSelection( old_info.itemsOfCategory( (*it)->category() ) );
+    Q_FOREACH( ListSelect *ls, m_optionList ) {
+        ls->setSelection( old_info.itemsOfCategory( ls->category() ) );
 
         // Also set all positionable tag candidates
 
-        if ( (*it)->positionable() ) {
-            QString category = (*it)->category();
+        if ( ls->positionable() ) {
+            QString category = ls->category();
             QSet<QString> selectedTags = old_info.itemsOfCategory( category );
 
             // Add the tag to the positionable candiate list, if no area is already associated with it
@@ -529,22 +529,21 @@ void AnnotationDialog::Dialog::load()
     // or un-marked as positionable in the meantime, so ...
     QMap<QString, bool> categoryIsPositionable;
 
-    for( QList<ListSelect*>::Iterator it = m_optionList.begin(); it != m_optionList.end(); ++it ) {
-        (*it)->setSelection( info.itemsOfCategory( (*it)->category() ) );
-        (*it)->rePopulate();
+    Q_FOREACH( ListSelect *ls, m_optionList ) {
+        ls->setSelection( info.itemsOfCategory( ls->category() ) );
+        ls->rePopulate();
 
         // Get all selected positionable tags and add them to the candidate list
-        if ((*it)->positionable()) {
-            QSet<QString> selectedTags = (*it)->itemsOn();
-            QSet<QString>::iterator tagName;
+        if (ls->positionable()) {
+            QSet<QString> selectedTags = ls->itemsOn();
 
-            for (tagName = selectedTags.begin(); tagName != selectedTags.end(); ++tagName) {
-                addTagToCandidateList( (*it)->category(), *tagName );
+            Q_FOREACH( const QString &tagName, selectedTags ) {
+                addTagToCandidateList( ls->category(), tagName );
             }
         }
 
         // ... create a list of all categories and their positionability ...
-        categoryIsPositionable[(*it)->category()] = (*it)->positionable();
+        categoryIsPositionable[ls->category()] = ls->positionable();
     }
 
     // Create all tagged areas
@@ -590,8 +589,8 @@ void AnnotationDialog::Dialog::load()
 
 void AnnotationDialog::Dialog::writeToInfo()
 {
-    for( QList<ListSelect*>::Iterator it = m_optionList.begin(); it != m_optionList.end(); ++it ) {
-        (*it)->slotReturn();
+    Q_FOREACH( ListSelect *ls, m_optionList ) {
+        ls->slotReturn();
     }
 
     DB::ImageInfo& info = m_editList[ m_current ];
@@ -629,10 +628,10 @@ void AnnotationDialog::Dialog::writeToInfo()
     info.setLabel( m_imageLabel->text() );
     info.setDescription( m_description->toPlainText() );
 
-    for (QList<ListSelect*>::Iterator it = m_optionList.begin(); it != m_optionList.end(); ++it) {
-        info.setCategoryInfo( (*it)->category(), (*it)->itemsOn() );
-        if ((*it)->positionable()) {
-            info.setPositionedTags((*it)->category(), taggedAreas[(*it)->category()]);
+    Q_FOREACH( ListSelect *ls, m_optionList ) {
+        info.setCategoryInfo( ls->category(), ls->itemsOn() );
+        if (ls->positionable()) {
+            info.setPositionedTags(ls->category(), taggedAreas[ls->category()]);
         }
     }
 
@@ -699,8 +698,9 @@ int AnnotationDialog::Dialog::configure( DB::ImageInfoList list, bool oneAtATime
         m_ratingChanged = false;
         m_areasChanged = false;
 
-        for( QList<ListSelect*>::Iterator it = m_optionList.begin(); it != m_optionList.end(); ++it )
-            setUpCategoryListBoxForMultiImageSelection( *it, list );
+        Q_FOREACH( ListSelect *ls, m_optionList ) {
+            setUpCategoryListBoxForMultiImageSelection( ls, list );
+        }
 
         m_imageLabel->setText(QString());
         m_imageFilePattern->setText(QString());
@@ -748,8 +748,8 @@ DB::ImageSearchInfo AnnotationDialog::Dialog::search( DB::ImageSearchInfo* searc
                       m_imageLabel->text(), m_description->toPlainText(),
                       m_imageFilePattern->text());
 
-        for( QList<ListSelect*>::Iterator it = m_optionList.begin(); it != m_optionList.end(); ++it ) {
-            m_oldSearch.setCategoryMatchText( (*it)->category(), (*it)->text() );
+        Q_FOREACH( const ListSelect *ls, m_optionList ) {
+            m_oldSearch.setCategoryMatchText( ls->category(), ls->text() );
         }
         //FIXME: for the user to search for 0-rated images, he must first change the rating to anything > 0
         //then change back to 0 .
@@ -774,8 +774,8 @@ void AnnotationDialog::Dialog::setup()
 {
 // Repopulate the listboxes in case data has changed
     // An group might for example have been renamed.
-    for( QList<ListSelect*>::Iterator it = m_optionList.begin(); it != m_optionList.end(); ++it ) {
-        (*it)->populate();
+    Q_FOREACH( ListSelect *ls, m_optionList ) {
+        ls->populate();
     }
 
     if ( m_setup == SearchMode )  {
@@ -797,8 +797,9 @@ void AnnotationDialog::Dialog::setup()
         setWindowTitle( i18n("Annotations") );
     }
 
-    for( QList<ListSelect*>::Iterator it = m_optionList.begin(); it != m_optionList.end(); ++it )
-        (*it)->setMode( m_setup );
+    Q_FOREACH( ListSelect *ls, m_optionList ) {
+        ls->setMode( m_setup );
+    }
 }
 
 
@@ -812,8 +813,8 @@ void AnnotationDialog::Dialog::loadInfo( const DB::ImageSearchInfo& info )
     m_startDate->setDate( info.date().start().date() );
     m_endDate->setDate( info.date().end().date() );
 
-    for( QList<ListSelect*>::Iterator it = m_optionList.begin(); it != m_optionList.end(); ++it ) {
-        (*it)->setText( info.categoryMatchText( (*it)->category() ) );
+    Q_FOREACH( ListSelect *ls, m_optionList ) {
+        ls->setText( info.categoryMatchText( ls->category() ) );
     }
 
     m_imageLabel->setText( info.label() );
@@ -1019,11 +1020,11 @@ bool AnnotationDialog::Dialog::hasChanges()
         changed |= ( !m_startDate->date().isNull() );
         changed |= ( !m_endDate->date().isNull() );
 
-        for( QList<ListSelect*>::Iterator it = m_optionList.begin(); it != m_optionList.end(); ++it ) {
+        Q_FOREACH( ListSelect *ls, m_optionList ) {
             StringSet on, partialOn;
-            std::tie(on, partialOn) = selectionForMultiSelect( *it, m_origList );
-            changed |= (on != (*it)->itemsOn());
-            changed |= (partialOn != (*it)->itemsUnchanged());
+            std::tie(on, partialOn) = selectionForMultiSelect( ls, m_origList );
+            changed |= (on != ls->itemsOn());
+            changed |= (partialOn != ls->itemsUnchanged());
         }
 
         changed |= ( !m_imageLabel->text().isEmpty() );
@@ -1140,7 +1141,7 @@ void AnnotationDialog::Dialog::setupFocus()
 
         bool inserted = false;
         // Iterate through the ordered list of widgets, and insert the current one, so it is in the right position in the tab chain.
-        for( QList<QWidget*>::Iterator orderedIt = orderedList.begin(); orderedIt != orderedList.end(); ++orderedIt ) {
+        for( QList<QWidget*>::iterator orderedIt = orderedList.begin(); orderedIt != orderedList.end(); ++orderedIt ) {
             const QWidget* w = *orderedIt;
             int wx = w->mapToGlobal( QPoint(0,0) ).x();
             int wy = w->mapToGlobal( QPoint(0,0) ).y();
@@ -1159,13 +1160,13 @@ void AnnotationDialog::Dialog::setupFocus()
     // now setup tab order.
     QWidget* prev = nullptr;
     QWidget* first = nullptr;
-    for( QList<QWidget*>::Iterator orderedIt = orderedList.begin(); orderedIt != orderedList.end(); ++orderedIt ) {
+    Q_FOREACH( QWidget *widget, orderedList ) {
         if ( prev ) {
-            setTabOrder( prev, *orderedIt );
+            setTabOrder( prev, widget );
         } else {
-            first = *orderedIt;
+            first = widget;
         }
-        prev = *orderedIt;
+        prev = widget;
     }
 
     if ( first ) {
@@ -1174,9 +1175,9 @@ void AnnotationDialog::Dialog::setupFocus()
 
 
     // Finally set focus on the first list select
-    for( QList<QWidget*>::Iterator orderedIt = orderedList.begin(); orderedIt != orderedList.end(); ++orderedIt ) {
-        if ( (*orderedIt)->property("FocusCandidate").isValid() && (*orderedIt)->isVisible() ) {
-            (*orderedIt)->setFocus();
+    Q_FOREACH( QWidget *widget, orderedList ) {
+        if ( widget->property("FocusCandidate").isValid() && widget->isVisible() ) {
+            widget->setFocus();
             break;
         }
     }
@@ -1332,8 +1333,8 @@ void AnnotationDialog::Dialog::saveAndClose()
         }
     }
     else if ( m_setup == InputMultiImageConfigMode ) {
-    for( QList<ListSelect*>::Iterator it = m_optionList.begin(); it != m_optionList.end(); ++it ) {
-            (*it)->slotReturn();
+        Q_FOREACH( ListSelect *ls, m_optionList ) {
+            ls->slotReturn();
         }
 
         for( DB::ImageInfoListConstIterator it = m_origList.constBegin(); it != m_origList.constEnd(); ++it ) {
@@ -1342,9 +1343,9 @@ void AnnotationDialog::Dialog::saveAndClose()
             if ( !m_startDate->date().isNull() )
                 info->setDate( DB::ImageDate( m_startDate->date(), m_endDate->date(), m_time->time() ) );
 
-            for( QList<ListSelect*>::Iterator listSelectIt = m_optionList.begin(); listSelectIt != m_optionList.end(); ++listSelectIt ) {
-                info->addCategoryInfo( (*listSelectIt)->category(), (*listSelectIt)->itemsOn() );
-                info->removeCategoryInfo( (*listSelectIt)->category(), (*listSelectIt)->itemsOff() );
+            Q_FOREACH( ListSelect *ls, m_optionList ) {
+                info->addCategoryInfo( ls->category(), ls->itemsOn() );
+                info->removeCategoryInfo( ls->category(), ls->itemsOff() );
             }
 
             if ( !m_imageLabel->text().isEmpty() ) {

Browser/FlatCategoryModel.cpp
@@ -29,8 +29,7 @@ Browser::FlatCategoryModel::FlatCategoryModel( const DB::CategoryPtr& category, 
     QStringList items = m_category->itemsInclCategories();
     items.sort();
 
-    for( QStringList::Iterator itemIt = items.begin(); itemIt != items.end(); ++itemIt ) {
-        const QString name = *itemIt;
+    Q_FOREACH( const QString &name, items ) {
         const int imageCount = m_images.contains(name) ? m_images[name] : 0;
         const int videoCount = m_videos.contains(name) ? m_videos[name] : 0;
 

DB/AndCategoryMatcher.cpp
@@ -20,8 +20,8 @@
 
 bool DB::AndCategoryMatcher::eval(ImageInfoPtr info, QMap<QString, StringSet>& alreadyMatched)
 {
-     for( QList<CategoryMatcher*>::Iterator it = mp_elements.begin(); it != mp_elements.end(); ++it ) {
-        if (!(*it)->eval(info, alreadyMatched))
+    Q_FOREACH( CategoryMatcher *subMatcher, mp_elements ) {
+        if (!subMatcher->eval(info, alreadyMatched))
             return false;
     }
     return true;

DB/CategoryItem.cpp
@@ -34,13 +34,13 @@ DB::CategoryItem* DB::CategoryItem::clone() const
     return result;
 }
 
-void DB::CategoryItem::print( int offset )
+void DB::CategoryItem::print( int offset ) const
 {
     QString spaces;
     spaces.fill( QChar::fromLatin1(' '), offset );
     qDebug( "%s%s", qPrintable(spaces), qPrintable(mp_name) );
-     for( QList< CategoryItem* >::Iterator it = mp_subcategories.begin(); it != mp_subcategories.end(); ++it ) {
-        (*it)->print( offset + 2 );
+    Q_FOREACH( const CategoryItem *subcategory, mp_subcategories ) {
+        subcategory->print( offset + 2 );
     }
 }
 
@@ -59,13 +59,13 @@ bool DB::CategoryItem::isDescendentOf( const QString& child, const QString& pare
     return false;
 }
 
-bool DB::CategoryItem::hasChild( const QString& child )
+bool DB::CategoryItem::hasChild( const QString& child ) const
 {
     if ( mp_name == child )
         return true;
 
-     for( QList< CategoryItem* >::Iterator it = mp_subcategories.begin(); it != mp_subcategories.end(); ++it ) {
-        if ( (*it)->hasChild( child ) )
+    Q_FOREACH( const CategoryItem *subcategory, mp_subcategories ) {
+        if ( subcategory->hasChild( child ) )
             return true;
     }
     return false;

DB/CategoryItem.h
@@ -33,8 +33,8 @@ public:
     bool isDescendentOf( const QString& child, const QString& parent ) const;
 
 protected:
-    void print( int offset );
-    bool hasChild( const QString& child );
+    void print( int offset ) const;
+    bool hasChild( const QString& child ) const;
 
 public:
     QString mp_name;

DB/GroupCounter.cpp
@@ -58,14 +58,15 @@ GroupCounter::GroupCounter( const QString& category )
     m_groupCount.reserve( 2729 /* A large prime */ );
 
     // Populate the m_memberToGroup map
-    for( QMap<QString,StringSet>::Iterator groupToMemberIt= groupToMemberMap.begin();
-         groupToMemberIt != groupToMemberMap.end(); ++groupToMemberIt ) {
-
+    for( QMap<QString,StringSet>::Iterator groupToMemberIt= groupToMemberMap.begin()
+         ; groupToMemberIt != groupToMemberMap.end()
+         ; ++groupToMemberIt )
+    {
         StringSet members = groupToMemberIt.value();
         QString group = groupToMemberIt.key();
 
-        for( StringSet::const_iterator memberIt = members.begin(); memberIt != members.end(); ++memberIt ) {
-            m_memberToGroup[*memberIt].append( group );
+        Q_FOREACH( const auto &member, members ) {
+            m_memberToGroup[member].append( group );
         }
         m_groupCount.insert( group, 0 );
     }

DB/ImageSearchInfo.cpp
@@ -160,8 +160,8 @@ bool ImageSearchInfo::match( ImageInfoPtr info ) const
     QString txt = info->description();
     if ( !m_description.isEmpty() ) {
         QStringList list = m_description.split(QChar::fromLatin1(' '), QString::SkipEmptyParts);
-        for( QStringList::Iterator it = list.begin(); it != list.end(); ++it ) {
-            ok = ok && ( txt.indexOf( *it, 0, Qt::CaseInsensitive ) != -1 );
+        Q_FOREACH( const QString &word, list ) {
+            ok = ok && ( txt.indexOf( word, 0, Qt::CaseInsensitive ) != -1 );
         }
     }
 
@@ -360,17 +360,16 @@ void ImageSearchInfo::compile() const
         QStringList orParts = matchText.split(QString::fromLatin1("|"), QString::SkipEmptyParts);
         DB::ContainerCategoryMatcher* orMatcher = new DB::OrCategoryMatcher;
 
-        for( QStringList::Iterator itOr = orParts.begin(); itOr != orParts.end(); ++itOr ) {
+        Q_FOREACH( QString orPart, orParts ) {
             // Split by " & ", not only by "&", so that the doubled "&"s won't be used as a split point
-            QStringList andParts = (*itOr).split(QString::fromLatin1(" & "), QString::SkipEmptyParts);
+            QStringList andParts = orPart.split(QString::fromLatin1(" & "), QString::SkipEmptyParts);
 
             DB::ContainerCategoryMatcher* andMatcher;
             bool exactMatch=false;
             bool negate = false;
             andMatcher = new DB::AndCategoryMatcher;
 
-            for( QStringList::Iterator itAnd = andParts.begin(); itAnd != andParts.end(); ++itAnd ) {
-                QString str = *itAnd;
+            Q_FOREACH( QString str, andParts ) {
                 static QRegExp regexp( QString::fromLatin1("^\\s*!\\s*(.*)$") );
                 if ( regexp.exactMatch( str ) )
                 { // str is preceded with NOT
@@ -499,8 +498,8 @@ Utilities::StringSet ImageSearchInfo::findAlreadyMatched( const QString &group )
     }
 
     QStringList list = str.split(QString::fromLatin1( "&" ), QString::SkipEmptyParts);
-    for( QStringList::Iterator it = list.begin(); it != list.end(); ++it ) {
-        QString nm = (*it).trimmed();
+    Q_FOREACH( QString part, list ) {
+        QString nm = part.trimmed();
         if (! nm.contains( QString::fromLatin1( "!" ) ) )
             result.insert(nm);
     }

commit f5457379cbd44ea4406a07a3cf1296dc33e7e9bf

Fix renaming and deletion of memberMap members.
The methods renameItem, renameGroup and deleteItem in MemberMap
inadvertingly operated on a copy of the member item set instead of the
real data.

This issue was introduced in commit 8d0b178e8 and is present since
release 4.7.

BUG: 376635

DB/MemberMap.cpp
@@ -18,6 +18,7 @@
 
 #include "MemberMap.h"
 #include "DB/Category.h"
+#include <QDebug>
 
 using namespace DB;
 
@@ -159,7 +160,7 @@ void MemberMap::renameGroup( const QString& category, const QString& oldName, co
     QMap<QString, StringSet>& groupMap = m_members[category];
     groupMap.insert(newName,m_members[category][oldName] );
     groupMap.remove( oldName );
-    Q_FOREACH( StringSet set, groupMap ) {
+    for( StringSet &set: groupMap ) {
         if ( set.contains( oldName ) ) {
             set.remove( oldName );
             set.insert( newName );
@@ -174,14 +175,14 @@ MemberMap::MemberMap( const MemberMap& other )
 
 void MemberMap::deleteItem( DB::Category* category, const QString& name)
 {
-    m_dirty = true;
-    if ( !m_loading )
-        emit dirty();
     QMap<QString, StringSet>& groupMap = m_members[category->name()];
-    Q_FOREACH( StringSet items, groupMap ) {
+    for( StringSet &items: groupMap ) {
         items.remove( name );
     }
     m_members[category->name()].remove(name);
+    m_dirty = true;
+    if ( !m_loading )
+        emit dirty();
 }
 
 void MemberMap::renameItem( DB::Category* category, const QString& oldName, const QString& newName )
@@ -189,11 +190,8 @@ void MemberMap::renameItem( DB::Category* category, const QString& oldName, cons
     if (oldName == newName)
         return;
 
-    m_dirty = true;
-    if ( !m_loading )
-        emit dirty();
     QMap<QString, StringSet>& groupMap = m_members[category->name()];
-    Q_FOREACH( StringSet items, groupMap ) {
+    for( StringSet &items: groupMap ) {
         if (items.contains( oldName ) ) {
             items.remove( oldName );
             items.insert( newName );
@@ -203,6 +201,9 @@ void MemberMap::renameItem( DB::Category* category, const QString& oldName, cons
         groupMap[newName] = groupMap[oldName];
         groupMap.remove(oldName);
     }
+    m_dirty = true;
+    if ( !m_loading )
+        emit dirty();
 }
 
 
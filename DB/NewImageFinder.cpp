73964af203bb9d1216a2e875b348959815753543
Robert Krawitz <rlk@alum.mit.edu>
2018-05-26 00:12:04 -0400

    Add notes on load performance.

--- DB/NewImageFinder.cpp
+++ DB/NewImageFinder.cpp
@@ -49,6 +49,316 @@
 
 using namespace DB;
 
+/*****************************************************************
+ *
+ * NOTES ON PERFORMANCE
+ * ===== == ===========
+ *
+ * - Robert Krawitz <rlk@alum.mit.edu> 2018-05-24
+ *
+ *
+ * GENERAL NOTES ON STORAGE I/O
+ * ------- ----- -- ------- ---
+ *
+ * The two main gates to loading new images are:
+ *
+ * 1) I/O (how fast can we read images off mass storage)
+ *
+ *    Different I/O devices have different characteristics in terms of
+ *    througput, media latency, and protocol latency.
+ *
+ *    - Throughput is the raw speed at which data can be transferred,
+ *      limited by the physical and/or electronic characteristics of
+ *      the medium and the interface.  Short of reducing the amount of
+ *      data that's transferred, or clever games with using the most
+ *      efficient part of the medium (the outer tracks only for HDD's,
+ *      a practice referred to as "short stroking" because it reduces
+ *      the distance the head has to seek, at the cost of wasting a
+ *      lot of capacity), there's nothing that can be done about this.
+ *
+ *    - Media latency is the latency component due to characteristics
+ *      of the underlying storage medium.  For spinning disks, this is
+ *      a function of rotational latency and sek latency.  In some
+ *      cases, particularly with hard disks, it is possible to reduce
+ *      media latency by arranging to access the data in a way that
+ *      reduces seeking.  See DB/FastDir.cpp for an example of this.
+ *
+ *      While media latency can sometimes be hidden by overlapping
+ *      I/O, generally not possible to avoid it.  Sometimes trying too
+ *      hard can actually increase media latency if it results in I/O
+ *      operations competing against each other requiring additional
+ *      seeks.
+ *
+ *      Overlapping I/O with computation is another matter; that can
+ *      easily yield benefit, especially if it eliminates rotational
+ *      latency.
+ *
+ *    - Protocol latency.  This refers to things like SATA overhead,
+ *      network overhead (for images stored on a network), and so
+ *      forth.  This can encompass multiple things, and often they can
+ *      be pipelined by means of multiple queued I/O operations.  For
+ *      example, multiple commands can be issued to modern interfaces
+ *      (SATA, NVMe) and many network interfaces without waiting for
+ *      earlier operations to return.
+ *
+ *      If protocol latency is high compared with media latency,
+ *      having multiple requests outstanding simultaneously can
+ *      yield significant benefits.
+ *
+ *    iostat is a valuable tool for investigating throughput and
+ *    looking for possible optimizations.  The IO/sec and data
+ *    read/written per second when compared against known media
+ *    characteristics (disk and SSD throughput, network bandwidth)
+ *    provides valuable information about whether we're getting close
+ *    to full performance from the I/O, and user and system CPU time
+ *    give us additional clues about whether we're I/O-bound or
+ *    CPU-bound.
+ *
+ *    Historically in the computer field, operations that require
+ *    relatively simple processing on large volumes of data are I/O
+ *    bound.  But with very fast I/O devices such as NVMe SSDs, some
+ *    of which reach 3 GB/sec, that's not always the case.
+ *
+ * 2) Image (mostly JPEG) loading.
+ *
+ *    This is a function of image characteristics and image processing
+ *    libraries.  Sometimes it's possible to apply parameters to
+ *    the underlying image loader to speed it up.  This shows up as user
+ *    CPU time.  Usually the only way to improve this performance
+ *    characteristic is to use more or faster CPU cores (sometimes GPUs
+ *    can assist here) or use better image loading routines (better
+ *    libraries).
+ *
+ *
+ * DESCRIPTION OF KPHOTOALBUM IMAGE LOAD PROCESS
+ * ----------- -- ----------- ----- ---- -------
+ *
+ * KPhotoAlbum, when it loads an image, performs three processing steps:
+ *
+ * 1) Compute the MD5 checksum
+ *
+ * 2) Extract the EXIF metadata
+ *
+ * 3) Generate a thumbnail
+ *
+ * Previous to this round of performance tuning, the first two steps
+ * were performed in the first pass, and thumbnails were generated in
+ * a separate pass.  Assuming that the set of new images is large enough
+ * that they cannot all fit in RAM buffers, this results in the I/O
+ * being performed twice.  The rewrite results in I/O being performed once.
+ *
+ * In addition, I have made many other changes:
+ *
+ * 1) Prior to the MD5 calculation step, a new thread, called a "scout
+ *    thread", reads the files into memory.  While this memory is not
+ *    directly used in the later computations, it results in the images
+ *    being in RAM when they are later needed, making the I/O very fast
+ *    (copying data in memory rather than reading it from storage).
+ *
+ *    This is a way to overlap I/O with computation.
+ *
+ * 2) The MD5 checksum uses its own I/O to read the data in in larger
+ *    chunks than the Qt MD5 routine does.  The Qt routine reads it in
+ *    in 4KiB chunks; my experimentation has found that 256KiB chunks
+ *    are more efficient, even with a scout thread (it reduces the
+ *    number of system calls).
+ *
+ * 3) When searching for other images to stack with the image being
+ *    loaded, the new image loader no longer attempts to determine
+ *    whether other candidate filenames are present, nor does it
+ *    compute the MD5 checksum of any such files it does find.  Rather,
+ *    it only checks for files that are already in KPhotoAlbum, either
+ *    previously or as a result of the current load.  Merely checking
+ *    for the presence of another file is not cheap, and it's not
+ *    necessary; if an image will belong to a stack, we'll either know
+ *    it now or when other images that can be stacked are loaded.
+ *
+ * 4) The EXIF metadata extraction is now done only once; previously
+ *    it was performed several times at different stages of the loading
+ *    process.
+ *
+ * 5) The thumbnail index is now written out incrementally rather than
+ *    the entire index (which can be many megabytes in a large image
+ *    database) being rewritten frequently.  The index is fully rewritten
+ *    prior to exit.
+ *
+ *
+ * BASELINE PERFORMANCE
+ * -------- -----------
+ *
+ * These measurements were all taken on a Lenovo ThinkPad P70 with 32
+ * GB of dual-channel DDR4-2400 DRAM, a Xeon E3-1505M CPU (4 cores/8
+ * total hyperthreads, 2.8-3.7 GHz Skylake; usually runs around
+ * 3.1-3.2 GHz in practice), a Seagate ST2000LM015-2E8174 2TB HDD, and
+ * a Crucial MX300 1TB SATA SSD.  Published numbers and measurements I
+ * took otherwise indicate that the HDD can handle about 105-110
+ * MB/sec with a maximum of 180 IO/sec (in a favorable case).  The SSD
+ * is rated to handle 530 MB/sec read, 510 MB/sec write, 92K random
+ * reads/sec, and 83K random writes/sec.
+ *
+ * The image set I used for all measurements, except as noted,
+ * consists of 10839 total files of which about 85% are 20 MP JPEG and
+ * the remainder (with a few exceptions are 20 MP RAW files from a
+ * Canon EOS 7D mkII camera.  The total dataset is about 92 GB in
+ * size.
+ *
+ * I baselined both drives by reading the same dataset by means of
+ *
+ * % ls | xargs cat | dd bs=1048576 of=/dev/null
+ *
+ * The HDD required between 850 and 870 seconds (14'10" to 14'30") to
+ * perform this operation, yielding about 105-108 MB/sec.  The SSD
+ * achieved about 271 MB/sec, which is well under its rated throughput
+ * (hdparm -Tt yields 355 MB/sec, which is likewise nowhere close to
+ * its rated throughput).  hdparm -Tt on the HDD yields about 120
+ * MB/sec, but throughput to an HDD depends upon which part of the
+ * disk is being read.  The outer tracks have a greater angular
+ * density to achieve the same linear density (in other words, the
+ * circumference of an outer track is longer than that of an inner
+ * track, and the data is stored at a constant linear density).  So
+ * hdparm isn't very useful on an HDD except as a best case.
+ *
+ * Note also that hdparm does a single stream read from the device.
+ * It does not take advantage of the ability to queue multiple
+ * requests.
+ * 
+ *
+ * ANALYSIS OF KPHOTOALBUM LOAD PERFORMANCE
+ * -------- -- ----------- ---- -----------
+ *
+ * I analyzed the following cases, with images stored both on the
+ * HDD and the SSD:
+ *
+ * 1) Images loaded (All, JPEG only, RAW only)
+ *
+ * B) Thumbnail creation (Including, Excluding)
+ *
+ * C) Scout threads (0, 1, 2, 3)
+ *
+ * The JPG image set constitutes 9293 images totaling about 55 GB.
+ * The RAW image set constitutes 1544 images totaling about 37 GB.
+ * The ALL set consists of 10839 or 10840 images totaling about 92 GB.
+ * 
+ * Times are elapsed times; CPU consumption is approximate user+system
+ * CPU consumption.  Numbers in parentheses are with thumbnail building
+ * disabled.  Note that in the cases with no scout threads on the SSD the
+ * times were reproducibly shorter with thumbnail building enabled
+ * (reasons are not determined at this time).
+ * 
+ * Cases building RAW thumbnails generally consumed somewhat more system
+ * CPU (in the range of 10-15%) than JPEG-only cases.  This may be due to
+ * custom I/O routines used for generating thumbnails with JPEG files;
+ * RAW files used the I/O provided by libkdcraw, which uses smaller I/O
+ * operations.
+ * 
+ * Elapsed Time
+ * ------- ----
+ * 
+ *                                 SSD                     HDD
+ * 
+ * JPG - 0 scouts                  4:03 (3:59)
+ * JPG - 1 scout                   2:46 (2:44)
+ * JPG - 2 scouts                  2:20 (2:07)
+ * JPG - 3 scouts                  2:21 (1:58)
+ * 
+ * ALL - 0 scouts                  6:32 (7:03)            16:01
+ * ALL - 1 scout                   4:24 (4:31)            15:01
+ * ALL - 2 scouts                  3:40 (3:28)            16:59
+ * ALL - 3 scouts                  3:42 (3:15)
+ * 
+ * RAW - 0 scouts                  2:18 (2:46)
+ * RAW - 1 scout                   1:46 (1:46)
+ * RAW - 2 scouts                  1:20 (1:17)
+ * RAW - 3 scouts                  1:18 (1:13)
+ * 
+ * User+System CPU
+ * ----------- ---
+ * 
+ *                                 SSD                     HDD
+ * 
+ * JPG - 0 scouts                  40% (12%)
+ * JPG - 1 scout                   70% (20%)
+ * JPG - 2 scouts                  85% (15%)
+ * JPG - 3 scouts                  85% (15%)
+ * 
+ * ALL - 0 scouts                  40% (10%)               10%
+ * ALL - 1 scout                   65% (18%)               20%
+ * ALL - 2 scouts                  70% (15%)                8%
+ * ALL - 3 scouts                  75% (15%)
+ * 
+ * RAW - 0 scouts                  25% (10%)
+ * RAW - 1 scout                   50% (25%)
+ * RAW - 2 scouts                  65% (15%)
+ * RAW - 3 scouts                  60% (15%)
+ *
+ * I also used kcachegrind to measure CPU consumption on smaller
+ * subsets of images (with and without thumbnail creation).  In terms
+ * of user CPU consumption, thumbnail creation constitutes the large
+ * majority of CPU cycles, followed by MD5 computation, with EXIF
+ * parsing lagging far behind.
+ * 
+ * With thumbnail generation:
+ * ---- --------- -----------
+ * 
+ *                                 RAW             JPEG
+ * 
+ * Thumbnail generation            74%             82%
+ *   libjpeg processing              69%             82%
+ *   scale/rotate                     2%
+ *   convert to RGB                   3%
+ * MD5 computation                 23%             13%
+ * Read EXIF                        0.5%           1.0%
+ * 
+ * Without thumbnail generation:
+ * ------- --------- -----------
+ * 
+ *                                 RAW             JPEG
+ * 
+ * MD5 computation                 92%             80%
+ * Read EXIF                        4%             10%
+ *
+ *
+ * CONCLUSIONS
+ * -----------
+ *
+ * For loading files from hard disk (likely the most common case),
+ * there's no reason to consider any loading method other than using a
+ * single scout thread and computing thumbnails concurrently.  Even
+ * with thumbnail computation, there is very little CPU utilization.
+ *
+ * Loading from SATA SSD benefits from two scout threads, and possibly
+ * more.  For minimal time to regain control, there is some benefit
+ * seen from separating thumbnail generation from the rest of the
+ * processing stages at the cost of more total elapsed time.  This is
+ * more evident with JPEG files than with RAW files in this test case.
+ * RAW files typically have smaller thumbnail images which can be
+ * extracted and processed more quickly than full-size JPEG files.  On
+ * a slower CPU, it may be desirable to return control to the user
+ * even if the thumbnails are not built yet.
+ *
+ * Two other cases would be NVMe (or other very fast) SSDs and network
+ * storage.  Since we're seeing evidence of CPU saturation on SATA
+ * SSDs, we would likely see this even more strongly with NVMe; with
+ * large numbers of images it may be desirable to separate the
+ * thumbnail building from the rest of the processing.  It may also be
+ * beneficial to use more scout threads.
+ *
+ * Network storage presents a different problem.  It is likely to have
+ * lower throughput -- and certainly much higher latency -- than even
+ * HDD, unless the underlying storage medium is SSD and the data is
+ * located on a very fast, low latency network.  So there would be no
+ * benefit to separating thumbnail processing.  However, due to
+ * protocol vs. media latency discussed above, it may well work to use
+ * more scout threads.  However, this may saturate the network and the
+ * storage, to the detriment of other users, and there's probably no
+ * general (or easily discoverable) optimum for this.
+ *
+ * It's my judgment that most images will be stored on HDDs for at
+ * least the next few years, so tuning for that use case is probably
+ * the best single choice to be made.
+ *
+ *****************************************************************/
+
 // Number of scout threads for preloading images.  More than one scout thread
 // yields about 10% less performance with higher IO/sec but lower I/O throughput,
 // most probably due to thrashing.

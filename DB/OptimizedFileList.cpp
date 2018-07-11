34520924c134fa24f32970d63a4efaa4a66c2aa4
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2018-07-12 01:25:16 +0200

    Fix compilation for FreeBSD
    Note: at least I hope so (I don't have a FreeBSD system handy right now)

--- DB/OptimizedFileList.cpp
+++ DB/OptimizedFileList.cpp
@@ -22,7 +22,16 @@
 extern "C" {
 #include <sys/types.h>
 #include <dirent.h>
-#include <sys/vfs.h>
+
+// Note: on FreeBSD, __linux__ is also defined
+#ifdef __BSD__
+# include <sys/param.h>
+# include <sys/mount.h>
+#else
+#ifdef __linux__
+# include <sys/vfs.h>
+#endif  // __linux__
+#endif
 }
 
 #include <QFile>

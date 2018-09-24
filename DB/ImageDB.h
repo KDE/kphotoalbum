KDE/kphotoalbum

commit fdbfad649b637b433ceed347280dae051b86af82

Further perf improvements for loading and correct autostacking behavior.
This involves several changes:

1) Insert EXIF data for each loaded image as it's loaded, but within a
   transaction.  This may reduce I/O for big loads over batching up
   all the loads at the end because it avoids a second pass over the
   files.

2) Add an image hash to the XMLDB, to allow fast lookup by name.  This
   enables the new image loader to add new files in while looking for
   duplicates and stack candidates.  The newly added images are kept
   in a pending list until the load is complete.

   This also improves correctness by making image load be a transaction.
   It also allows for full autostacking during load, which wasn't
   working before.

3) Add a filename->MD5 map in addition to MD5->filename map to allow
   duplicate detection without having to compute the MD5 checksum
   on possible duplicate files, which is expensive in computation and
   very expensive in I/O.

4) Add image scout thread during load, to read files in from disk
   ahead of the loader and reducing or eliminating time the new
   image loader waits on I/O.  It limits its readahead to 20 images
   to reduce the risk of blowing out RAM and making problems worse.
   This appears to usually benefit if I/O is needed, and I haven't
   found cases where it makes things worse (the most likely scenario
   would be big images with slow I/O and low memory).

In addition, print the progress bar after the final image is loaded.

DB/ImageDB.h
@@ -87,6 +87,7 @@ public: // Methods that must be overridden
     virtual QMap<QString,uint> classify( const ImageSearchInfo& info, const QString & category, MediaType typemask ) = 0;
     virtual FileNameList images() = 0;
     virtual void addImages( const ImageInfoList& images, bool doUpdate=true ) = 0;
+    virtual void clearDelayedImages() = 0;
     /** @short Update file name stored in the DB */
     virtual void renameImage( const ImageInfoPtr info, const DB::FileName& newName ) = 0;
 

DB/NewImageFinder.cpp
@@ -18,6 +18,7 @@
 #include "NewImageFinder.h"
 #include "FastDir.h"
 #include "Logging.h"
+#include "ImageScout.h"
 
 #include <BackgroundJobs/ReadVideoLengthJob.h>
 #include <BackgroundJobs/SearchForVideosWithoutVideoThumbnailsJob.h>
@@ -42,6 +43,8 @@
 
 #include <KLocalizedString>
 #include <KMessageBox>
+#include <QDataStream>
+#include <QFile>
 
 using namespace DB;
 
@@ -152,8 +155,22 @@ void NewImageFinder::loadExtraFiles( bool storeExif )
 
     int count = 0;
     ImageInfoList newImages;
+
+    ImageScoutList asyncPreloadList;
+    for( LoadList::Iterator it = m_pendingLoad.begin(); it != m_pendingLoad.end(); ++it ) {
+        asyncPreloadList.append((*it).first);
+    }
+
+    ImageScoutThread *scoutThread = new ImageScoutThread(asyncPreloadList);
+    scoutThread->start();
+
+    Exif::Database::instance()->startInsertTransaction();
+    dialog.setValue( count ); // ensure to call setProgress(0)
     for( LoadList::Iterator it = m_pendingLoad.begin(); it != m_pendingLoad.end(); ++it, ++count ) {
-        dialog.setValue( count ); // ensure to call setProgress(0)
+        if (scoutThread && scoutThread->isFinished()) {
+            delete scoutThread;
+            scoutThread = NULL;
+        }
         qApp->processEvents( QEventLoop::AllEvents );
 
         if ( dialog.wasCanceled() )
@@ -162,20 +179,39 @@ void NewImageFinder::loadExtraFiles( bool storeExif )
             // try to build thumbnails for images w/o DB entry:
             m_pendingLoad.clear();
             return;
+            // Ask the scout to interrupt first, then do our cleanup, then wait for
+            // the scout thread to finish.  One more line of code, but lets us
+            // overlap the operations.
+            if (scoutThread)
+                scoutThread->requestInterruption();
+            m_pendingLoad.clear();
+            Exif::Database::instance()->abortInsertTransaction();
+
+            if (scoutThread) {
+                while (! scoutThread->isFinished())
+                    QThread::msleep(10);
+                delete scoutThread;
+            }
+            return;
         }
         // (*it).first: DB::FileName
         // (*it).second: DB::MediaType
         ImageInfoPtr info = loadExtraFile( (*it).first, (*it).second, storeExif );
+        if (scoutThread)
+            scoutThread->incrementLoadedCount();
         if ( info ) {
             markUnTagged(info);
             newImages.append(info);
         }
+        dialog.setValue( count );
     }
+    dialog.setValue( count );
     // loadExtraFile() has already called addImages() on any images
     // that it stacked, but without updating the EXIF database or
     // the UI.  Even if there are no images left at this point,
     // we need to call addImages() to pick up any delayed ones.
     DB::ImageDB::instance()->addImages( newImages );
+    Exif::Database::instance()->commitInsertTransaction();
 
     // I would have loved to do this in loadExtraFile, but the image has not been added to the database yet
     if ( MainWindow::FeatureDialog::hasVideoThumbnailer() ) {
@@ -184,6 +220,11 @@ void NewImageFinder::loadExtraFiles( bool storeExif )
                 BackgroundTaskManager::JobManager::instance()->addJob(
                         new BackgroundJobs::ReadVideoLengthJob(info->fileName(), BackgroundTaskManager::BackgroundVideoPreviewRequest));
         }
+    }
+    if (scoutThread) {
+        scoutThread->requestInterruption();
+        while (! scoutThread->isFinished())
+            QThread::msleep(10);
     }
 
 }
@@ -219,7 +260,13 @@ ImageInfoPtr NewImageFinder::loadExtraFile( const DB::FileName& newFileName, DB:
                 tmp.replace(m_modifiedFileComponent, (*it));
                 originalFileName = DB::FileName::fromRelativePath(tmp);
 
-                MD5 originalSum = (newFileName == originalFileName ? sum : Utilities::MD5Sum( originalFileName ));
+                MD5 originalSum;
+                if (newFileName == originalFileName)
+                  originalSum = sum;
+                else if (DB::ImageDB::instance()->md5Map()->containsFile( originalFileName ) )
+                  originalSum = DB::ImageDB::instance()->md5Map()->lookupFile( originalFileName );
+                else
+                  originalSum = Utilities::MD5Sum( originalFileName );
                 if ( DB::ImageDB::instance()->md5Map()->contains( originalSum ) ) {
                     // we have a previous copy of this file; copy it's data
                     // from the original.
@@ -253,20 +300,16 @@ ImageInfoPtr NewImageFinder::loadExtraFile( const DB::FileName& newFileName, DB:
             }
         }
     }
+    ImageInfoList newImages;
+    newImages.append( info );
+    DB::ImageDB::instance()->addImages( newImages, false );
 
     // also inserts image into exif db if present:
-    info->setMD5Sum(sum, false);
+    info->setMD5Sum( sum );
     DB::ImageDB::instance()->md5Map()->insert( sum, info->fileName());
 
     if (originalInfo &&
         Settings::SettingsData::instance()->autoStackNewFiles() ) {
-        // we have to do this immediately to get the ids, but we don't
-        // want to take the hit of updating the EXIF database and the
-        // UI at this point.  We call addImages() later in loadExtraFiles()
-        // which will pick these up.
-        ImageInfoList newImages;
-        newImages.append(info);
-        DB::ImageDB::instance()->addImages( newImages, false );
 
         // stack the files together
         DB::FileName olderfile = originalFileName;
@@ -289,10 +332,9 @@ ImageInfoPtr NewImageFinder::loadExtraFile( const DB::FileName& newFileName, DB:
 
         // ordering: XXX we ideally want to place the new image right
         // after the older one in the list.
-
-        info = nullptr;  // we already added it, so don't process again
     }
 
+    info = nullptr;  // we already added it, so don't process again
     return info;
 }
 

XMLDB/Database.cpp
@@ -150,6 +150,7 @@ void XMLDB::Database::deleteList(const DB::FileNameList& list)
                 m_stackMap.insert(inf->stackId(), newCache);
             }
         }
+        m_imageCache.remove( inf->fileName().absolute() );
         m_images.remove( inf );
     }
     Exif::Database::instance()->remove( list );
@@ -188,62 +189,69 @@ void XMLDB::Database::lockDB( bool lock, bool exclude  )
 }
 
 
-void XMLDB::Database::forceUpdate( const DB::ImageInfoList& images )
+void XMLDB::Database::clearDelayedImages()
 {
-    DB::FileNameList list;
-    if ( images.count() > 0 )
-        for( DB::ImageInfoListConstIterator imageIt = images.constBegin(); imageIt != images.constEnd(); ++imageIt )
-            list << ((*imageIt)->fileName());
-
-    if ( m_delayedUpdate.count() > 0 )
-        for( DB::ImageInfoListConstIterator imageIt = m_delayedUpdate.constBegin(); imageIt != m_delayedUpdate.constEnd(); ++imageIt )
-            list << ((*imageIt)->fileName());
-    if ( list.count() > 0 ) {
-        Exif::Database::instance()->add( list );
-        emit totalChanged( m_images.count() );
-        emit dirty();
-    }
+    m_delayedCache.clear();
     m_delayedUpdate.clear();
 }
 
-void XMLDB::Database::addImages( const DB::ImageInfoList& images,
-                                 bool doUpdate )
+void XMLDB::Database::forceUpdate( const DB::ImageInfoList& images )
 {
     // FIXME: merge stack information
     DB::ImageInfoList newImages = images.sort();
     if ( m_images.count() == 0 ) {
         // case 1: The existing imagelist is empty.
+        Q_FOREACH( const DB::ImageInfoPtr& imageInfo, newImages )
+            m_imageCache.insert( imageInfo->fileName().absolute(), imageInfo );
         m_images = newImages;
     }
     else if ( newImages.count() == 0 ) {
         // case 2: No images to merge in - that's easy ;-)
-        // Update any pending images
-        if (doUpdate)
-            forceUpdate( images );
         return;
     }
     else if ( newImages.first()->date().start() > m_images.last()->date().start() ) {
         // case 2: The new list is later than the existsing
+        Q_FOREACH( const DB::ImageInfoPtr& imageInfo, newImages )
+            m_imageCache.insert( imageInfo->fileName().absolute(), imageInfo );
         m_images.appendList(newImages);
     }
     else if ( m_images.isSorted() ) {
         // case 3: The lists overlaps, and the existsing list is sorted
+        Q_FOREACH( const DB::ImageInfoPtr& imageInfo, newImages )
+            m_imageCache.insert( imageInfo->fileName().absolute(), imageInfo );
         m_images.mergeIn( newImages );
     }
     else{
         // case 4: The lists overlaps, and the existsing list is not sorted in the overlapping range.
+        Q_FOREACH( const DB::ImageInfoPtr& imageInfo, newImages )
+            m_imageCache.insert( imageInfo->fileName().absolute(), imageInfo );
         m_images.appendList( newImages );
     }
+}
 
-    for( DB::ImageInfoListConstIterator imageIt = images.constBegin(); imageIt != images.constEnd(); ++imageIt ) {
-        DB::ImageInfoPtr info = *imageIt;
+void XMLDB::Database::addImages( const DB::ImageInfoList& images,
+                                 bool doUpdate )
+{
+    Q_FOREACH( const DB::ImageInfoPtr& info, images ) {
         info->addCategoryInfo( i18n( "Media Type" ),
                                info->mediaType() == DB::Image ? i18n( "Image" ) : i18n( "Video" ) );
-        if ( !doUpdate )
-            m_delayedUpdate << info ;
+        m_delayedCache.insert( info->fileName().absolute(), info );
+        m_delayedUpdate << info;
     }
-    if ( doUpdate )
-        forceUpdate( images );
+    if ( doUpdate ) {
+        uint imagesAdded = m_delayedUpdate.count();
+        if ( imagesAdded > 0 ) {
+            forceUpdate(m_delayedUpdate);
+            m_delayedCache.clear();
+            m_delayedUpdate.clear();
+            // It's the responsility of the caller to add the EXIF information.
+            // It's more efficient from an I/O perspective to minimize the number
+            // of passes over the images, and with the ability to add the EXIF
+            // data in a transaction, there's no longer any need to read it here.
+            emit totalChanged( m_images.count() );
+            emit dirty();
+        }
+    }
 }
 
 void XMLDB::Database::renameImage( DB::ImageInfoPtr info, const DB::FileName& newName )
@@ -254,30 +262,25 @@ void XMLDB::Database::renameImage( DB::ImageInfoPtr info, const DB::FileName& ne
 
 DB::ImageInfoPtr XMLDB::Database::info( const DB::FileName& fileName ) const
 {
-    typedef QHash<QString, DB::ImageInfoPtr > Cache;
-    static Cache fileMap;
-
     if ( fileName.isNull() )
         return DB::ImageInfoPtr();
 
     const QString name = fileName.absolute();
 
-    Cache::iterator lookup = fileMap.find(name);
+    if (m_imageCache.contains( name ))
+        return m_imageCache[name];
 
-    if ( lookup != fileMap.end() )
-        return *lookup;
-    else {
-        for( DB::ImageInfoListConstIterator it = m_images.constBegin(); it != m_images.constEnd(); ++it ) {
-            fileMap.insert( (*it)->fileName().absolute(), *it );
-        }
-        if ( fileMap.contains( name ) )
-            return fileMap[ name ];
+    if (m_delayedCache.contains( name ))
+        return m_delayedCache[name];
+
+    Q_FOREACH( const DB::ImageInfoPtr& imageInfo, m_images )
+        m_imageCache.insert( imageInfo->fileName().absolute(), imageInfo );
+
+    if ( m_imageCache.contains( name ) ) {
+        return m_imageCache[ name ];
     }
 
-    if (fileMap.contains( name ))
-        return DB::ImageInfoPtr(new DB::ImageInfo);
-    else
-        return DB::ImageInfoPtr();
+    return DB::ImageInfoPtr();
 }
 
 bool XMLDB::Database::rangeInclude( DB::ImageInfoPtr info ) const
@@ -402,6 +405,7 @@ DB::ImageInfoList XMLDB::Database::takeImagesFromSelection(const DB::FileNameLis
             ++it;
         } else {
             result << *it;
+            m_imageCache.remove( (*it)->fileName().absolute() );
             it = m_images.erase(it);
         }
         // if all images from selection are in result (size of lists is equal) break.
@@ -429,6 +433,7 @@ void XMLDB::Database::insertList(
     for( DB::ImageInfoListConstIterator it = list.begin(); it != list.end(); ++it ) {
         // the call to insert() destroys the given iterator so use the new one after the call
         imageIt = m_images.insert( imageIt, *it );
+        m_imageCache.insert( (*it)->fileName().absolute(), *it );
         // increment always to retain order of selected images
         imageIt++;
     }

XMLDB/Database.h
@@ -52,6 +52,7 @@ namespace XMLDB {
         QMap<QString,uint> classify( const DB::ImageSearchInfo& info, const QString &category, DB::MediaType typemask ) override;
         DB::FileNameList images() override;
         void addImages( const DB::ImageInfoList& images, bool doUpdate ) override;
+        void clearDelayedImages() override;
         void renameImage( DB::ImageInfoPtr info, const DB::FileName& newName ) override;
 
         void addToBlockList(const DB::FileNameList& list) override;
@@ -115,6 +116,8 @@ namespace XMLDB {
         typedef QMap<DB::StackID, DB::FileNameList> StackMap;
         mutable  StackMap m_stackMap;
         DB::ImageInfoList m_delayedUpdate;
+	mutable QHash<const QString, DB::ImageInfoPtr> m_imageCache;
+	mutable QHash<const QString, DB::ImageInfoPtr> m_delayedCache;
 
         // used for checking if any images are without image attribute from the database.
         static bool s_anyImageWithEmptySize;

Exif/Database.cpp
@@ -277,13 +277,13 @@ void Exif::Database::remove( const DB::FileNameList& list )
     m_db.commit();
 }
 
-bool Exif::Database::insert(const DB::FileName& filename, Exiv2::ExifData data )
+QSqlQuery *Exif::Database::getInsertQuery()
 {
-    static QString _queryString;
     if ( !isUsable() )
-        return false;
-
-    if (_queryString.isEmpty())
+        return NULL;
+    if ( m_insertTransaction )
+        return m_insertTransaction;
+    if (m_queryString.isEmpty())
     {
         QStringList formalList;
         Database::ElementList elms = elements();
@@ -291,62 +291,101 @@ bool Exif::Database::insert(const DB::FileName& filename, Exiv2::ExifData data )
         {
             formalList.append( e->queryString() );
         }
-        _queryString = QString::fromLatin1( "INSERT OR REPLACE into exif values (?, %1) " ).arg( formalList.join( QString::fromLatin1( ", " ) ) );
+        m_queryString = QString::fromLatin1( "INSERT OR REPLACE into exif values (?, %1) " ).arg( formalList.join( QString::fromLatin1( ", " ) ) );
     }
-    QSqlQuery query(m_db);
-    query.prepare( _queryString );
-    query.bindValue(  0, filename.absolute() );
+    QSqlQuery *query = new QSqlQuery(m_db);
+    if (query)
+        query->prepare( m_queryString );
+    return query;
+}
+
+QSqlQuery *Exif::Database::openInsertQuery()
+{
+    if ( m_insertTransaction )
+        return m_insertTransaction;
+    m_db.transaction();
+    QSqlQuery *query = getInsertQuery();
+    m_db.transaction();
+    return query;
+}
+
+void Exif::Database::concludeInsertQuery( QSqlQuery *query )
+{
+    if ( m_insertTransaction )
+        return;
+    m_db.commit();
+    delete query;
+}
+
+bool Exif::Database::startInsertTransaction()
+{
+    m_insertTransaction = getInsertQuery();
+    m_db.transaction();
+    return ( m_insertTransaction != NULL );
+}
+
+bool Exif::Database::commitInsertTransaction()
+{
+    if (m_insertTransaction) {
+        m_db.commit();
+        delete m_insertTransaction;
+        m_insertTransaction = NULL;
+    }
+    return true;
+}
+
+bool Exif::Database::abortInsertTransaction()
+{
+    if (m_insertTransaction) {
+        m_db.rollback();
+        delete m_insertTransaction;
+        m_insertTransaction = NULL;
+    }
+    return true;
+}
+
+bool Exif::Database::insert(const DB::FileName& filename, Exiv2::ExifData data )
+{
+    if ( !isUsable() )
+        return false;
+
+    QSqlQuery *query = getInsertQuery();
+    query->bindValue(  0, filename.absolute() );
     int i = 1;
     for( const DatabaseElement *e : elements() )
     {
-        query.bindValue( i++, e->valueFromExif(data));
+        query->bindValue( i++, e->valueFromExif(data));
     }
 
-    if ( !query.exec() )
-    {
-        showError( query );
-        return false;
-    } else {
-        return true;
-    }
-
+    bool status = query->exec();
+    if ( !status )
+        showError( *query );
+    concludeInsertQuery( query );
+    return status;
 }
 
 bool Exif::Database::insert(QList<DBExifInfo> map )
 {
-    static QString _queryString;
     if ( !isUsable() )
         return false;
 
-    if (_queryString.isEmpty())
-    {
-        QStringList formalList;
-        Database::ElementList elms = elements();
-        for( const DatabaseElement *e : elms )
-        {
-            formalList.append( e->queryString() );
-        }
-        _queryString = QString::fromLatin1( "INSERT OR REPLACE into exif values (?, %1) " ).arg( formalList.join( QString::fromLatin1( ", " ) ) );
-    }
-
-    m_db.transaction();
-    QSqlQuery query( _queryString, m_db );
+    QSqlQuery *query = getInsertQuery();
     // not a const reference because DatabaseElement::valueFromExif uses operator[] on the exif datum
     Q_FOREACH ( DBExifInfo elt, map )
     {
-        query.bindValue(  0, elt.first.absolute() );
+        query->bindValue(  0, elt.first.absolute() );
         int i = 1;
         for( const DatabaseElement *e : elements() )
         {
-            query.bindValue( i++, e->valueFromExif(elt.second));
+            query->bindValue( i++, e->valueFromExif(elt.second));
         }
 
-        if ( !query.exec() )
+        if ( !query->exec() )
         {
-            showError( query );
+            showError( *query );
         }
     }
-    m_db.commit();
+    concludeInsertQuery( query );
     return true;
 }
 
@@ -533,6 +572,7 @@ void Exif::Database::init()
     if ( !isAvailable() )
         return;
 
+    m_insertTransaction = NULL;
     bool dbExists = QFile::exists( exifDBFile() );
 
     openDatabase();

Exif/Database.h
@@ -100,6 +100,9 @@ public:
     CameraList cameras() const;
     LensList lenses() const;
     void recreate();
+    bool startInsertTransaction();
+    bool commitInsertTransaction();
+    bool abortInsertTransaction();
 
 protected:
     enum DBSchemaChangeType { SchemaChanged, SchemaAndDataChanged };
@@ -118,8 +121,13 @@ private:
     Database();
     ~Database();
     void init();
+    QSqlQuery *getInsertQuery();
+    QSqlQuery *openInsertQuery();
+    void concludeInsertQuery(QSqlQuery *);
     static Database* s_instance;
+    QString m_queryString;
     QSqlDatabase m_db;
+    QSqlQuery *m_insertTransaction;
 };
 
 }

CMakeLists.txt
@@ -283,6 +283,7 @@ set(libDB_SRCS
     ${CMAKE_CURRENT_SOURCE_DIR}/DB/FileInfo.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/DB/NegationCategoryMatcher.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/DB/NewImageFinder.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/DB/ImageScout.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/DB/NoTagCategoryMatcher.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/DB/GroupCounter.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/DB/CategoryMatcher.cpp

DB/ImageInfo.cpp
@@ -475,7 +475,6 @@ void ImageInfo::readExif(const DB::FileName& fullPath, DB::ExifMode mode)
 
     // Database update
     if ( mode & EXIFMODE_DATABASE_UPDATE ) {
-        Exif::Database::instance()->remove( fullPath );
         Exif::Database::instance()->add( fullPath );
 #ifdef HAVE_KGEOMAP
         // GPS coords might have changed...

commit e0bbac459ec86bd5cf60bf8cc8d93d0bee836eb1

Review and fix logic in NewImageFinder.
This fixes the bug that caused new images to not be tagged as untagged,
which was introduced in fdbfad64.

DB/ImageDB.h
@@ -86,7 +86,16 @@ public: // Methods that must be overridden
 
     virtual QMap<QString,uint> classify( const ImageSearchInfo& info, const QString & category, MediaType typemask ) = 0;
     virtual FileNameList images() = 0;
+    /**
+     * @brief addImages to the database.
+     * The parameter \p doUpdate decides whether all bookkeeping should be done right away
+     * (\c true; the "normal" use-case), or if it should be deferred until later(\c false).
+     * If doUpdate is deferred, either commitDelayedImages() or clearDelayedImages() needs to be called afterwards.
+     * @param images
+     * @param doUpdate
+     */
     virtual void addImages( const ImageInfoList& images, bool doUpdate=true ) = 0;
+    virtual void commitDelayedImages() = 0;
     virtual void clearDelayedImages() = 0;
     /** @short Update file name stored in the DB */
     virtual void renameImage( const ImageInfoPtr info, const DB::FileName& newName ) = 0;

DB/NewImageFinder.cpp
@@ -478,7 +478,6 @@ void NewImageFinder::loadExtraFiles()
     setupFileVersionDetection();
 
     int count = 0;
-    ImageInfoList newImages;
 
     ImageScoutQueue asyncPreloadQueue;
     for( LoadList::Iterator it = m_pendingLoad.begin(); it != m_pendingLoad.end(); ++it ) {
@@ -502,35 +501,19 @@ void NewImageFinder::loadExtraFiles()
         }
         // (*it).first: DB::FileName
         // (*it).second: DB::MediaType
-        ImageInfoPtr info = loadExtraFile( (*it).first, (*it).second );
+        loadExtraFile( (*it).first, (*it).second );
         loadedCount++;          // Atomic
-        if ( info ) {
-            markUnTagged(info);
-            newImages.append(info);
-        }
         if ( timeSinceProgressUpdate.elapsed() >= 1000 ) {
             dialog.setValue( count );
             timeSinceProgressUpdate.restart();
         }
     }
     dialog.setValue( count );
-    // loadExtraFile() should have inserted all images into the
-    // database on the fly, but we need to tell the database to
-    // commit the changes.
-    if ( newImages.count() > 0 ) {
-        qCWarning(DBLog) << newImages.count() << " images were left to insert after load!";
-    }
-    DB::ImageDB::instance()->addImages( newImages );
+    // loadExtraFile() has already inserted all images into the
+    // database, but without committing the changes
+    DB::ImageDB::instance()->commitDelayedImages();
     Exif::Database::instance()->commitInsertTransaction();
 
-    // I would have loved to do this in loadExtraFile, but the image has not been added to the database yet
-    if ( MainWindow::FeatureDialog::hasVideoThumbnailer() ) {
-        Q_FOREACH( const ImageInfoPtr& info, newImages ) {
-            if ( info->isVideo() )
-                BackgroundTaskManager::JobManager::instance()->addJob(
-                        new BackgroundJobs::ReadVideoLengthJob(info->fileName(), BackgroundTaskManager::BackgroundVideoPreviewRequest));
-        }
-    }
     ImageManager::ThumbnailBuilder::instance()->save();
 }
 
@@ -643,10 +626,18 @@ ImageInfoPtr NewImageFinder::loadExtraFile( const DB::FileName& newFileName, DB:
 
         // ordering: XXX we ideally want to place the new image right
         // after the older one in the list.
-    }
-    ImageManager::ThumbnailBuilder::instance()->buildOneThumbnail( info );
 
-    info = nullptr;  // we already added it, so don't process again
+        // no post-processing is needed in loadExtraFiles()
+        info = nullptr;
+    }
+
+    markUnTagged(info);
+    ImageManager::ThumbnailBuilder::instance()->buildOneThumbnail( info );
+    if ( info->isVideo() && MainWindow::FeatureDialog::hasVideoThumbnailer() ) {
+        // needs to be done *after* insertion into database
+        BackgroundTaskManager::JobManager::instance()->addJob(
+                    new BackgroundJobs::ReadVideoLengthJob(info->fileName(), BackgroundTaskManager::BackgroundVideoPreviewRequest));
+    }
     return info;
 }
 

XMLDB/Database.cpp
@@ -240,18 +240,23 @@ void XMLDB::Database::addImages( const DB::ImageInfoList& images,
         m_delayedUpdate << info;
     }
     if ( doUpdate ) {
-        uint imagesAdded = m_delayedUpdate.count();
-        if ( imagesAdded > 0 ) {
-            forceUpdate(m_delayedUpdate);
-            m_delayedCache.clear();
-            m_delayedUpdate.clear();
-            // It's the responsility of the caller to add the EXIF information.
-            // It's more efficient from an I/O perspective to minimize the number
-            // of passes over the images, and with the ability to add the EXIF
-            // data in a transaction, there's no longer any need to read it here.
-            emit totalChanged( m_images.count() );
-            emit dirty();
-        }
+        commitDelayedImages();
+    }
+}
+
+void XMLDB::Database::commitDelayedImages()
+{
+    uint imagesAdded = m_delayedUpdate.count();
+    if ( imagesAdded > 0 ) {
+        forceUpdate(m_delayedUpdate);
+        m_delayedCache.clear();
+        m_delayedUpdate.clear();
+        // It's the responsibility of the caller to add the EXIF information.
+        // It's more efficient from an I/O perspective to minimize the number
+        // of passes over the images, and with the ability to add the EXIF
+        // data in a transaction, there's no longer any need to read it here.
+        emit totalChanged( m_images.count() );
+        emit dirty();
     }
 }
 

XMLDB/Database.h
@@ -52,6 +52,7 @@ namespace XMLDB {
         QMap<QString,uint> classify( const DB::ImageSearchInfo& info, const QString &category, DB::MediaType typemask ) override;
         DB::FileNameList images() override;
         void addImages( const DB::ImageInfoList& images, bool doUpdate ) override;
+        void commitDelayedImages() override;
         void clearDelayedImages() override;
         void renameImage( DB::ImageInfoPtr info, const DB::FileName& newName ) override;
 
KDE/kphotoalbum

commit b6e17a9692d64011dac843d0e8c53b36a710d77e

Remove erroneously added bits

MainWindow/Window.cpp
@@ -898,10 +898,6 @@ void MainWindow::Window::setupMenuBar()
     a = actionCollection()->addAction( QString::fromLatin1("buildThumbs"), this, SLOT(slotBuildThumbnails()) );
     a->setText( i18n("Build Thumbnails") );
 
-    a = actionCollection()->addAction( QString::fromLatin1("optimizeThumbs"), this, SLOT(slotOptimizeThumbnails()) );
-    a->setText( i18n("Optimize Thumbnails") );
-
-    a = actionCollection()->addAction( QString::fromLatin1("statistics"), this, SLOT(slotStatistics()) );
     a->setText( i18n("Statistics...") );
 
     m_markUntagged = actionCollection()->addAction(QString::fromUtf8("markUntagged"),
@@ -1756,11 +1752,6 @@ void MainWindow::Window::showImage( const DB::FileName& fileName )
 void MainWindow::Window::slotBuildThumbnails()
 {
     ImageManager::ThumbnailBuilder::instance()->buildAll( ImageManager::StartNow );
-}
-
-void MainWindow::Window::slotOptimizeThumbnails()
-{
-    ImageManager::ThumbnailBuilder::instance()->optimizeThumbnails();
 }
 
 void MainWindow::Window::slotBuildThumbnailsIfWanted()

ImageManager/ThumbnailBuilder.cpp
@@ -63,7 +63,6 @@ void ImageManager::ThumbnailBuilder::cancelRequests()
 void ImageManager::ThumbnailBuilder::terminateScout()
 {
     if (m_scout) {
-        qDebug() << "Terminating scout:" << m_count << m_expectedThumbnails;
         delete m_scout;
         m_scout = nullptr;
     }
@@ -85,7 +84,6 @@ void ImageManager::ThumbnailBuilder::pixmapLoaded(ImageManager::ImageRequest* re
     m_loadedCount++;
     m_statusBar->setProgress( ++m_count );
     if ( m_count >= m_expectedThumbnails ) {
-        qDebug() << "pixmapLoaded terminating scout" << m_count << m_expectedThumbnails;
         terminateScout();
     }
 }
@@ -102,11 +100,6 @@ void ImageManager::ThumbnailBuilder::buildAll( ThumbnailBuildStart when )
         ImageManager::ThumbnailCache::instance()->flush();
         scheduleThumbnailBuild( DB::ImageDB::instance()->images(), when );
     }
-}
-
-void ImageManager::ThumbnailBuilder::optimizeThumbnails()
-{
-    ImageManager::ThumbnailCache::instance()->optimizeThumbnails();
 }
 
 ImageManager::ThumbnailBuilder* ImageManager::ThumbnailBuilder::instance()
@@ -198,7 +191,6 @@ void ImageManager::ThumbnailBuilder::doThumbnailBuild()
     m_expectedThumbnails = numberOfThumbnailsToBuild;
     if (numberOfThumbnailsToBuild == 0) {
         m_statusBar->setProgressBarVisible(false);
-        qDebug() << "doThumbnailBuild terminating scout" << m_count << m_expectedThumbnails;
         terminateScout();
     }
 }
@@ -213,7 +205,6 @@ void ImageManager::ThumbnailBuilder::requestCanceled()
     m_statusBar->setProgress( ++m_count );
     m_loadedCount++;
     if ( m_count >= m_expectedThumbnails ) {
-        qDebug() << "requestCanceled terminating scout" << m_count << m_expectedThumbnails;
         terminateScout();
     }
 }

ImageManager/ThumbnailBuilder.h
@@ -46,7 +46,6 @@ public:
 
 public slots:
     void buildAll(ThumbnailBuildStart when=ImageManager::StartDelayed );
-    void optimizeThumbnails();
     void buildMissing();
     void cancelRequests( );
     void scheduleThumbnailBuild( const DB::FileNameList& list, ThumbnailBuildStart when );

ImageManager/ThumbnailCache.cpp
@@ -421,111 +421,6 @@ void ImageManager::ThumbnailCache::load()
     qDebug() << "Loaded thumbnails in " << timer.elapsed() / 1000.0 << " seconds";
 }
 
-void ImageManager::ThumbnailCache::optimizeThumbnails()
-{
-    const QString tmpdir(QString::fromLatin1(".thumbnails.tmp/"));
-    const QString dir = thumbnailPath(QString(), tmpdir);
-    const QString index = thumbnailPath(QString::fromLatin1("thumbnailindex"), tmpdir);
-    QElapsedTimer timer;
-
-    timer.start();
-    int currentInputFile = -1;
-    if ( !QFile::exists(dir) )
-        QDir().mkpath(dir);
-
-    int maxFile = 0;
-    int maxOffset = 0;
-    for ( const DB::FileName& fileName : DB::ImageDB::instance()->images()) {
-        const CacheFileInfo& cacheInfo = m_hash[fileName];
-        if ( cacheInfo.fileIndex > maxFile ) {
-            maxFile = cacheInfo.fileIndex;
-            maxOffset = cacheInfo.offset + cacheInfo.size;
-        } else if ( cacheInfo.fileIndex == maxFile && 
-                    cacheInfo.offset >= maxOffset + cacheInfo.size ) {
-            maxOffset = cacheInfo.offset + cacheInfo.size;
-        }
-    }
-
-    int currentFile = 0;
-    int currentOffset = 0;
-
-    QFile *outFile = nullptr;
-    
-    QFile *inFile = nullptr;
-
-    QFile indexFile( index );
-    if ( ! indexFile.open(QIODevice::ReadWrite ) ) {
-        qCWarning(ImageManagerLog, "Failed to open index file for writing");
-        return;
-    }
-
-    QDataStream stream(&indexFile);
-    stream << FILEVERSION
-           << maxFile
-           << maxOffset
-           << DB::ImageDB::instance()->images().count();
-
-    for ( const DB::FileName& fileName : DB::ImageDB::instance()->images()) {
-        const CacheFileInfo& cacheInfo = m_hash[fileName];
-        if ( cacheInfo.fileIndex != currentInputFile ) {
-            if ( inFile ) {
-                inFile->close();
-                delete inFile;
-            }
-            currentInputFile = cacheInfo.fileIndex;
-            inFile = new QFile( fileNameForIndex(currentInputFile) );
-            if ( ! inFile->open(QIODevice::ReadOnly ) ) {
-                qDebug() << "Failed to open thumbnail file " << fileNameForIndex(currentInputFile) << " for reading";
-                return;
-            }
-        }
-        if ( ! inFile->seek( cacheInfo.offset ) ) {
-            qCWarning(ImageManagerLog, "Failed to seek in input thumbnail file");
-            return;
-        }
-
-        QByteArray data( inFile->read( cacheInfo.size ) );
-        if ( data.size() != cacheInfo.size ) {
-            qCWarning(ImageManagerLog, "Incomplete size!");
-            return;
-        }
-        if ( ! outFile ) {
-            outFile = new QFile( fileNameForIndex( currentFile, tmpdir ) );
-            if ( ! outFile->open(QIODevice::ReadWrite ) ) {
-                qCWarning(ImageManagerLog, "Failed to open thumbnail file for inserting");
-                return;
-            }
-        }
-
-        if ( ! ( outFile->write(data.data(), data.size() ) ) ) {
-            qCWarning(ImageManagerLog, "Failed to write image data to thumbnail file!");
-            return;
-        }
-        stream << fileName.relative()
-               << currentFile
-               << currentOffset
-               << data.size();
-        currentOffset += data.size();
-        if ( currentOffset > MAXFILESIZE ) {
-            outFile->close();
-            delete outFile;
-            outFile = nullptr;
-            currentFile++;
-            currentOffset = 0;
-        }
-    }
-    indexFile.close();
-    if ( inFile ) {
-        inFile->close();
-        delete inFile;
-    }
-    if ( outFile ) {
-        outFile->close();
-        delete outFile;
-    }
-    qDebug() << "Optimized thumbnails in " << timer.elapsed() / 1000.0 << " seconds";
-}
-
 bool ImageManager::ThumbnailCache::contains( const DB::FileName& name ) const
 {
     m_dataLock.lock();

ImageManager/ThumbnailCache.h
@@ -46,7 +46,6 @@ public:
     void load();
     void removeThumbnail( const DB::FileName& );
     void removeThumbnails( const DB::FileNameList& );
-    void optimizeThumbnails();
 
 public slots:
     void save() const;

commit ce2479f18ca823c922805ef70a6e6e6d46b3d126

Fix regression: missing statistics dialog.
Revision b6e17a96 accidentally removed part of the setup-code for the
statistics action, leaving the menu item with an incorrect action.

MainWindow/Window.cpp
@@ -898,6 +898,7 @@ void MainWindow::Window::setupMenuBar()
     a = actionCollection()->addAction( QString::fromLatin1("buildThumbs"), this, SLOT(slotBuildThumbnails()) );
     a->setText( i18n("Build Thumbnails") );
 
+    a = actionCollection()->addAction( QString::fromLatin1("statistics"), this, SLOT(slotStatistics()) );
     a->setText( i18n("Statistics...") );
 
     m_markUntagged = actionCollection()->addAction(QString::fromUtf8("markUntagged"),
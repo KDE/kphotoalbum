6d1eff30f4635b31d1e76808a6d45675fb50a149
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2019-03-11 21:39:24 +0100

    Improved log message.

--- DB/MemberMap.cpp
+++ DB/MemberMap.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2003-2018 Jesper K. Pedersen <blackie@kde.org>
+/* Copyright (C) 2003-2019 The KPhotoAlbum Development Team
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
@@ -243,7 +243,7 @@ void MemberMap::addMemberToGroup( const QString& category, const QString& group,
         return;
 
     if ( item.isEmpty() ) {
-        qCWarning(DBLog, "Null item tried inserted into group %s", qPrintable(group));
+        qCWarning(DBLog, "Tried to insert null item into group %s/%s. Ignoring...", qPrintable(category), qPrintable(group));
         return;
     }
 

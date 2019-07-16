30929eaae92e7f48271949828480ce6c3e45b44e
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2019-07-16 23:23:10 +0200

    ImageSearchInfo: add getter for rating

--- DB/ImageSearchInfo.cpp
+++ DB/ImageSearchInfo.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2003-2015 Jesper K. Pedersen <blackie@kde.org>
+/* Copyright (C) 2003-2019 The KPhotoAlbum Development Team
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
@@ -595,6 +595,11 @@ QList<QList<SimpleCategoryMatcher*> > ImageSearchInfo::convertMatcher( CategoryM
     else
         result.append( extractAndMatcher( item ) );
     return result;
+}
+
+short ImageSearchInfo::rating() const
+{
+    return m_rating;
 }
 
 ImageDate ImageSearchInfo::date() const
--- DB/ImageSearchInfo.h
+++ DB/ImageSearchInfo.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2003-2015 Jesper K. Pedersen <blackie@kde.org>
+/* Copyright (C) 2003-2019 The KPhotoAlbum Development Team
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
@@ -65,6 +65,7 @@ public:
     QList<QList<SimpleCategoryMatcher*> > query() const;
 
     void addAnd( const QString& category, const QString& value );
+    short rating() const;
     void setRating( short rating);
     QString toString() const;
 

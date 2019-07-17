bf1f1b8019bc18d87b4aabfd420759356bc1ab6d
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2019-07-17 15:04:25 +0200

    Add ThumbnailView::FilterWidget

---
+++ ThumbnailView/FilterWidget.h
@@ -0,0 +1,57 @@
+/* Copyright (C) 2019 The KPhotoAlbum Development Team
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of
+   the License or (at your option) version 3 or any later version
+   accepted by the membership of KDE e. V. (or its successor approved
+   by the membership of KDE e. V.), which shall act as a proxy
+   defined in Section 14 of version 3 of the license.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef FILTERWIDGET_H
+#define FILTERWIDGET_H
+
+#include <QWidget>
+
+#include <DB/ImageSearchInfo.h>
+
+class KRatingWidget;
+class QLabel;
+
+namespace ThumbnailView
+{
+/**
+ * @brief The FilterWidget class provides a widget to interact with the thumbnail filter.
+ * You can use it to set the rating filter, and it gives some visual feedback when the filter changes.
+ */
+class FilterWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit FilterWidget(QWidget *parent = nullptr);
+
+signals:
+    void ratingChanged(short rating);
+
+public slots:
+    void setFilter(const DB::ImageSearchInfo &filter);
+
+protected slots:
+    void slotRatingChanged(int rating);
+
+private:
+    KRatingWidget *m_rating;
+    QLabel *m_label;
+};
+}
+
+#endif // FILTERWIDGET_H

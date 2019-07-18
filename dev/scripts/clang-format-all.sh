618cb95a04ceb9640a32ad53243aae5314a42236
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2019-07-19 00:29:29 +0200

    Add script to run clang-format over *.{cpp,h}
    The script works on all currently known source directories except
    AndroidRemoteControl.

---
+++ dev/scripts/clang-format-all.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Apply clang-format in-place to all source files
+# Note: AndroidRemoteControl is excluded for now:
+
+clang-format -i \
+	DB/*.{cpp,h} \
+	Utilities/*.{cpp,h} \
+	ThumbnailView/*.{cpp,h} \
+	AnnotationDialog/*.{cpp,h} \
+	BackgroundJobs/*.{cpp,h} \
+	BackgroundTaskManager/*.{cpp,h} \
+	Browser/*.{cpp,h} \
+	CategoryListView/*.{cpp,h} \
+	DateBar/*.{cpp,h} \
+	Exif/*.{cpp,h} \
+	HTMLGenerator/*.{cpp,h} \
+	ImageManager/*.{cpp,h} \
+	ImportExport/*.{cpp,h} \
+	MainWindow/*.{cpp,h} \
+	MainWindow/DuplicateMerger/*.{cpp,h} \
+	Map/*.{cpp,h} \
+	Plugins/*.{cpp,h} \
+	RemoteControl/*.{cpp,h} \
+	Settings/*.{cpp,h} \
+	Viewer/*.{cpp,h} \
+	XMLDB/*.{cpp,h}

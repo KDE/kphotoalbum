KDE/kphotoalbum

commit 1d2ab9337d142b5acd3f53347fa2192772d930e4

Viewer::TaggedArea: add highlight property.

Viewer/TaggedArea.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2014 Tobias Leupold <tobias.leupold@web.de>
+/* Copyright (C) 2014-2019 The KPhotoAlbum Development Team
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
@@ -21,7 +21,11 @@
 Viewer::TaggedArea::TaggedArea(QWidget *parent) : QFrame(parent)
 {
     setFrameShape(QFrame::Box);
-    resetViewStyle();
+    setStyleSheet(QString::fromLatin1(
+        "Viewer--TaggedArea { border: none; background-color: none; }"
+        "Viewer--TaggedArea:hover, Viewer--TaggedArea[selected=\"true\"]{ border: 1px solid rgb(0,255,0,99); background-color: rgb(255,255,255,30); }"
+        "Viewer--TaggedArea[highlighted=\"true\"]{ border: 1px solid rgb(255,128,0,99); background-color: rgb(255,255,255,30); }"
+    ));
 }
 
 Viewer::TaggedArea::~TaggedArea()
@@ -44,19 +48,34 @@ QRect Viewer::TaggedArea::actualGeometry() const
     return m_actualGeometry;
 }
 
-void Viewer::TaggedArea::resetViewStyle()
+void Viewer::TaggedArea::setSelected(bool selected)
 {
-    setStyleSheet(QString::fromLatin1(
-        "Viewer--TaggedArea { border: none; background-color: none; }"
-        "Viewer--TaggedArea:hover { border: 1px solid rgb(0,255,0,99); background-color: rgb(255,255,255,30); }"
-    ));
+    m_selected = selected;
 }
 
-void Viewer::TaggedArea::checkShowArea(QPair<QString, QString> tagData)
+bool Viewer::TaggedArea::selected() const
 {
-    if (tagData == m_tagInfo) {
-        setStyleSheet(QString::fromLatin1("Viewer--TaggedArea { border: 1px solid rgb(0,255,0,99); background-color: rgb(255,255,255,30); }"));
-    }
+    return m_selected;
+}
+
+void Viewer::TaggedArea::deselect()
+{
+    setSelected(false);
+}
+
+void Viewer::TaggedArea::checkIsSelected(QPair<QString, QString> tagData)
+{
+    m_selected = (tagData == m_tagInfo);
+}
+
+bool Viewer::TaggedArea::highlighted() const
+{
+    return m_highlighted;
+}
+
+void Viewer::TaggedArea::setHighlighted(bool highlighted)
+{
+    m_highlighted = highlighted;
 }
 
 // vi:expandtab:tabstop=4 shiftwidth=4:

Viewer/TaggedArea.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2014 Tobias Leupold <tobias.leupold@web.de>
+/* Copyright (C) 2014-2019 The KPhotoAlbum Development Team
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
@@ -25,6 +25,8 @@ namespace Viewer {
 class TaggedArea : public QFrame
 {
     Q_OBJECT
+    Q_PROPERTY(bool selected MEMBER m_selected READ selected WRITE setSelected RESET deselect)
+    Q_PROPERTY(bool highlighted MEMBER m_highlighted READ highlighted WRITE setHighlighted)
 
 public:
     explicit TaggedArea(QWidget *parent = 0);
@@ -33,15 +35,39 @@ public:
     void setActualGeometry(QRect geometry);
     QRect actualGeometry() const;
 
+    /**
+     * @brief When selected, the TaggedArea is shown (just like when hovering with the mouse).
+     * This is used to make the area visible when the corresponding tag in the ViewerWidget is hovered.
+     * @return \c true, if the area is visible.
+     */
+    bool selected() const;
+    void setSelected(bool selected);
+    void deselect();
+
+    /**
+     * @brief highlighted
+     * @return \c true, when the area should be visibly highlighted, \c false otherwise.
+     */
+    bool highlighted() const;
+    /**
+     * @brief setHighlighted sets the highlighted property of the area.
+     * An area with the highlighted tag set to \c true will be visibly highlighted.
+     * @param highlighted
+     */
+    void setHighlighted(bool highlighted);
+
 public slots:
-    void checkShowArea(QPair<QString, QString> tagData);
-    void resetViewStyle();
+    /**
+     * @brief checkIsSelected set the \c selected property if tagData matches the tag.
+     * @param tagData
+     */
+    void checkIsSelected(QPair<QString, QString> tagData);
 
 private:
     QPair<QString, QString> m_tagInfo;
     QRect m_actualGeometry;
-    QString m_styleDefault;
-    QString m_styleHighlighted;
+    bool m_selected;
+    bool m_highlighted;
 };
 
 }

Viewer/ViewerWidget.cpp
@@ -1369,8 +1369,8 @@ void Viewer::ViewerWidget::setTaggedAreas(QMap<QString, QMap<QString, QRect>> ta
             newArea->setActualGeometry(areaData.value());
             newArea->show();
 
-            connect(m_infoBox, &InfoBox::tagHovered, newArea, &TaggedArea::checkShowArea);
-            connect(m_infoBox, &InfoBox::noTagHovered, newArea, &TaggedArea::resetViewStyle);
+            connect(m_infoBox, &InfoBox::tagHovered, newArea, &TaggedArea::checkIsSelected);
+            connect(m_infoBox, &InfoBox::noTagHovered, newArea, &TaggedArea::deselect);
         }
     }
 

commit e705d535267b035b209c34412250b767ce524cb7

Fix hover-effect for TaggedArea in Viewer.
The bug was introduced in 1d2ab933 and did not make it into a release.

Viewer/InfoBox.cpp
@@ -70,7 +70,9 @@ Viewer::InfoBox::InfoBox(Viewer::ViewerWidget *viewer)
     m_jumpToContext->setIcon(QIcon::fromTheme(QString::fromUtf8("kphotoalbum")));
     m_jumpToContext->setFixedSize(16, 16);
     connect(m_jumpToContext, &QToolButton::clicked, this, &InfoBox::jumpToContext);
-    connect(this, SIGNAL(highlighted(QString)), SLOT(linkHovered(QString)));
+    // overloaded signal requires explicit cast:
+    void (InfoBox::*highlighted)(const QString &) = &InfoBox::highlighted;
+    connect(this, highlighted, this, &InfoBox::linkHovered);
 
 #ifdef HAVE_KGEOMAP
     m_showOnMap = new QToolButton(this);

Viewer/TaggedArea.cpp
@@ -18,6 +18,8 @@
 #include "TaggedArea.h"
 
 #include <KLocalizedString>
+#include <QDebug>
+#include <QStyle>
 
 Viewer::TaggedArea::TaggedArea(QWidget *parent)
     : QFrame(parent)
@@ -25,7 +27,9 @@ Viewer::TaggedArea::TaggedArea(QWidget *parent)
     setFrameShape(QFrame::Box);
     setStyleSheet(QString::fromLatin1(
         "Viewer--TaggedArea { border: none; background-color: none; }"
-        "Viewer--TaggedArea:hover, Viewer--TaggedArea[selected=\"true\"]{ border: 1px solid rgb(0,255,0,99); background-color: rgb(255,255,255,30); }"
+        "Viewer--TaggedArea:hover, Viewer--TaggedArea[selected=\"true\"] {"
+        " border: 1px solid rgb(0,255,0,99); background-color: rgb(255,255,255,30);"
+        " }"
         "Viewer--TaggedArea[highlighted=\"true\"]{ border: 1px solid rgb(255,128,0,99); background-color: rgb(255,255,255,30); }"));
 }
 
@@ -52,6 +56,7 @@ QRect Viewer::TaggedArea::actualGeometry() const
 void Viewer::TaggedArea::setSelected(bool selected)
 {
     m_selected = selected;
+    repolish();
 }
 
 bool Viewer::TaggedArea::selected() const
@@ -66,7 +71,14 @@ void Viewer::TaggedArea::deselect()
 
 void Viewer::TaggedArea::checkIsSelected(QPair<QString, QString> tagData)
 {
-    m_selected = (tagData == m_tagInfo);
+    setSelected(tagData == m_tagInfo);
+}
+
+void Viewer::TaggedArea::repolish()
+{
+    style()->unpolish(this);
+    style()->polish(this);
+    update();
 }
 
 bool Viewer::TaggedArea::highlighted() const
@@ -77,6 +89,7 @@ bool Viewer::TaggedArea::highlighted() const
 void Viewer::TaggedArea::setHighlighted(bool highlighted)
 {
     m_highlighted = highlighted;
+    repolish();
 }
 
 // vi:expandtab:tabstop=4 shiftwidth=4:

Viewer/TaggedArea.h
@@ -64,11 +64,18 @@ public slots:
      */
     void checkIsSelected(QPair<QString, QString> tagData);
 
+protected:
+    /**
+     * @brief repolish tells the widget to reevaluate its style.
+     * This required when the style is dynamically changed because a property changed.
+     */
+    void repolish();
+
 private:
     QPair<QString, QString> m_tagInfo;
     QRect m_actualGeometry;
-    bool m_selected;
-    bool m_highlighted;
+    bool m_selected = false;
+    bool m_highlighted = false;
 };
 
 }
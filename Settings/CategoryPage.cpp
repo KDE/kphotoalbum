KDE/kphotoalbum

commit f682cb333c827bd6073a66de86153dcf3b749fa3

CategoryPage: Silence deprecation warning

Settings/CategoryPage.cpp
@@ -190,7 +190,7 @@ Settings::CategoryPage::CategoryPage(QWidget *parent)
 void Settings::CategoryPage::resetInterface()
 {
     enableDisable(false);
-    m_categoriesListWidget->setItemSelected(m_categoriesListWidget->currentItem(), false);
+    m_categoriesListWidget->currentItem()->setSelected(false);
     resetCategoryLabel();
     m_renameLabel->hide();
 }

commit 5e6d8af26b14d885b2dc5be7f632dd0af4c9e0a8

Fix crash introduced by f682cb33

Settings/CategoryPage.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2003-2019 The KPhotoAlbum Development Team
+/* Copyright (C) 2003-2020 The KPhotoAlbum Development Team
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
@@ -190,7 +190,8 @@ Settings::CategoryPage::CategoryPage(QWidget *parent)
 void Settings::CategoryPage::resetInterface()
 {
     enableDisable(false);
-    m_categoriesListWidget->currentItem()->setSelected(false);
+    if (m_categoriesListWidget->currentItem())
+        m_categoriesListWidget->currentItem()->setSelected(false);
     resetCategoryLabel();
     m_renameLabel->hide();
 }
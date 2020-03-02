8b28ed903f6ce9608e2221e64ef73c0c81a5a673
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2020-03-03 00:06:58 +0100

    Fix compilation without marble.

--- config-kpa-marble.h.cmake
+++ config-kpa-marble.h.cmake
@@ -1,8 +1,10 @@
 /* Define to 1 if Marble should be compiled in */
 #cmakedefine HAVE_MARBLE 1
+#ifdef HAVE_MARBLE
 #cmakedefine MARBLE_HAS_regionSelected_OLD
 #cmakedefine MARBLE_HAS_regionSelected_NEW
 
 #if !(defined(MARBLE_HAS_regionSelected_OLD) || defined(MARBLE_HAS_regionSelected_NEW))
 #error "Feature detection for Marble failed!"
+#endif
 #endif

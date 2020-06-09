0bd28ceb7e58ea0ba2f1c55a06fb0acc82ec7245
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2020-06-09 23:34:16 +0200

    Refactor: improve testability of convertV5ToV4Cache

--- kpa-thumbnailtool/ThumbnailCacheConverter.cpp
+++ kpa-thumbnailtool/ThumbnailCacheConverter.cpp
@@ -27,49 +27,59 @@
 
 int KPAThumbnailTool::convertV5ToV4Cache(const QString &indexFilename, QTextStream &err)
 {
-    QFile indexFile { indexFilename };
-    if (!indexFile.open(QIODevice::ReadOnly)) {
+    QFile fromFile { indexFilename };
+    if (!fromFile.open(QIODevice::ReadOnly)) {
         err << i18nc("@info:shell", "Could not open thumbnailindex file!\n");
         err << i18nc("@info:shell", "Thumbnailindex was not changed.\n");
         return 1;
     }
-    QDataStream stream { &indexFile };
-    int version;
-    stream >> version;
-    if (version != 5) {
-        err << i18nc("@info:shell", "Thumbnailindex is not a version 5 file!\n");
-        err << i18nc("@info:shell", "Thumbnailindex was not changed.\n");
-        return 1;
-    }
-    // skip dimensions
-    stream.skipRawData(sizeof(int));
-
-    QTemporaryFile newIndexFile;
-    if (!newIndexFile.open()) {
+    QTemporaryFile toFile;
+    if (!toFile.open()) {
         err << i18nc("@info:shell", "Could not open temporary file for writing!\n");
         err << i18nc("@info:shell", "Thumbnailindex was not changed.\n");
         return 1;
     }
-    QDataStream newStream { &newIndexFile };
-    constexpr int v4FileVersion = 4;
-    newStream << v4FileVersion;
-    int numBytes = 0;
-    char buf[256];
-    do {
-        numBytes = stream.readRawData(buf, 256);
-        newStream.writeRawData(buf, numBytes);
-    } while (numBytes != 0);
-    if (!indexFile.rename(QString::fromUtf8("%1.bak").arg(indexFilename))) {
+
+    if (!convertV5ToV4Cache(fromFile, toFile, err))
+        return 1;
+
+    if (!fromFile.rename(QString::fromUtf8("%1.bak").arg(indexFilename))) {
         err << i18nc("@info:shell", "Could not back up thumbnailindex file!\n");
         err << i18nc("@info:shell", "Thumbnailindex was not changed.\n");
         return 1;
     }
-    if (!newIndexFile.copy(indexFilename)) {
+    if (!toFile.copy(indexFilename)) {
         err << i18nc("@info:shell", "Could not copy temporary thumbnailindex file to final location!\n");
-        err << i18nc("@info:shell", "Error message was: %1\n", newIndexFile.errorString());
+        err << i18nc("@info:shell", "Error message was: %1\n", toFile.errorString());
         return 1;
     }
     return 0;
+}
+
+bool KPAThumbnailTool::convertV5ToV4Cache(QIODevice &fromFile, QIODevice &toFile, QTextStream &err)
+{
+    QDataStream fromStream { &fromFile };
+    int version;
+    fromStream >> version;
+    if (version != 5) {
+        err << i18nc("@info:shell", "Thumbnailindex is not a version 5 file!\n");
+        err << i18nc("@info:shell", "Thumbnailindex was not changed.\n");
+        return false;
+    }
+    // skip dimensions
+    fromStream.skipRawData(sizeof(int));
+
+    QDataStream toStream { &toFile };
+    constexpr int v4FileVersion = 4;
+    toStream << v4FileVersion;
+    int numBytes = 0;
+    char buf[256];
+    do {
+        numBytes = fromStream.readRawData(buf, 256);
+        toStream.writeRawData(buf, numBytes);
+    } while (numBytes != 0);
+
+    return true;
 }
 
 // vi:expandtab:tabstop=4 shiftwidth=4:
--- kpa-thumbnailtool/ThumbnailCacheConverter.h
+++ kpa-thumbnailtool/ThumbnailCacheConverter.h
@@ -19,6 +19,7 @@
 #ifndef KPA_THUMBNAILTOOL_THUMBNAILCACHECONVERTER_H
 #define KPA_THUMBNAILTOOL_THUMBNAILCACHECONVERTER_H
 
+class QIODevice;
 class QString;
 class QTextStream;
 
@@ -26,6 +27,8 @@ namespace KPAThumbnailTool
 {
 /**
  * @brief Convert a version 5 ThumbnailCache index file to version 4.
+ * The old thumbnailindex file is backed up with a suffic '.bak'.
+ *
  * This function does not use the ThumbnailCache code at all,
  * but just reads the file and converts the header accordingly.
  *
@@ -36,6 +39,19 @@ namespace KPAThumbnailTool
  * @return 0 on success, 1 otherwise.
  */
 int convertV5ToV4Cache(const QString &indexFilename, QTextStream &err);
+
+/**
+ * @brief Convert a version 5 ThumbnailCache index file to version 4.
+ * This function does not use the ThumbnailCache code at all,
+ * but just reads the file and converts the header accordingly.
+ *
+ * This way, code in ThumbnailCache is not bloated by a niche-usecase.
+ * @param fromFile the QIODevice containing the old index data
+ * @param toFile a QIODevice to write to
+ * @param err a stream for error messages.
+ * @return \c true on success, \c false if the input file is not valid.
+ */
+bool convertV5ToV4Cache(QIODevice &fromFile, QIODevice &toFile, QTextStream &err);
 }
 
 #endif

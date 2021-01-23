c636229aa3cc48853b3d0a8adf3b2db436fff103
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2021-01-24 00:11:44 +0100

    Add tests for Utilities::relativeFolderName

--- lib/kpabase/TestFileNameUtil.cpp
+++ lib/kpabase/TestFileNameUtil.cpp
@@ -24,18 +24,33 @@ void KPATest::TestFileNameUtil::stripEndingForwardSlash()
 {
     const auto emptyString = QString();
     const auto absolutePath = QStringLiteral("/path/to/imageroot");
-    const auto absolutePathWithSlash = QStringLiteral("/path/to/imageroot/");
-    //const auto absolutePathWithSlashes = QStringLiteral("/path/to/imageroot//");
+    const auto slash = QStringLiteral("/");
 
     using Utilities::stripEndingForwardSlash;
     QCOMPARE(stripEndingForwardSlash(emptyString), emptyString);
     QCOMPARE(stripEndingForwardSlash(absolutePath), absolutePath);
-    QCOMPARE(stripEndingForwardSlash(absolutePathWithSlash), absolutePath);
-    // QCOMPARE(stripEndingForwardSlash(absolutePathWithSlashes), absolutePath);
+    QCOMPARE(stripEndingForwardSlash(absolutePath + slash), absolutePath);
+    // currently not implemented that way:
+    // QCOMPARE(stripEndingForwardSlash(absolutePath + slash + slash), absolutePath);
 }
 
 void KPATest::TestFileNameUtil::folderName()
 {
+    const auto emptyString = QString();
+    const auto absolutePath = QStringLiteral("/path/to/imageroot");
+    const auto relativePath = QStringLiteral("dirname/otherdir");
+    const auto slash = QStringLiteral("/");
+    const auto filename = QStringLiteral("filename.jpg");
+    using Utilities::relativeFolderName;
+
+    QCOMPARE(relativeFolderName(emptyString), emptyString);
+    QCOMPARE(relativeFolderName(absolutePath), QStringLiteral("/path/to"));
+    QCOMPARE(relativeFolderName(absolutePath + slash), absolutePath);
+    QCOMPARE(relativeFolderName(absolutePath + slash + filename), absolutePath);
+    // currently not implemented that way:
+    //QCOMPARE(relativeFolderName(absolutePath + slash + slash + filename), absolutePath);
+    QCOMPARE(relativeFolderName(relativePath + slash), relativePath);
+    QCOMPARE(relativeFolderName(relativePath + slash + filename), relativePath);
 }
 
 //void KPATest::TestFileNameUtil::cleanupTestCase()

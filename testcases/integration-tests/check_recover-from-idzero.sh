0cb58f9fd297ae5639b9b929bbc52691c46c03ff
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2021-01-24 02:13:31 +0100

    Adjust integration tests for new log messages

--- testcases/integration-tests/check_recover-from-idzero.sh
+++ testcases/integration-tests/check_recover-from-idzero.sh
@@ -1,4 +1,5 @@
 # SPDX-FileCopyrightText: 2020 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
+# SPDX-FileCopyrightText: 2021 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
 #
 # SPDX-License-Identifier: BSD-2-Clause
 
@@ -50,6 +51,7 @@ check_recover-from-idzero()
 			return $result_failed
 		fi
 
+		sed -i "s:$TEMPDIR:IMAGEROOT:g" "$subcheck_dir/log"
 		if ! diff -u "$data_dir/$subcheck.expected.log" "$subcheck_dir/log"
 		then
 			log notice "$check_name/$subcheck: Mismatch in log messages!"

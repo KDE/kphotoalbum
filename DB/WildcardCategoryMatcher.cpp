1003658692bff178484e81bac51be54c23b6ca88
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2021-01-26 00:43:09 +0100

    Add WildcardCategoryMatcher

---
+++ DB/WildcardCategoryMatcher.cpp
@@ -0,0 +1,49 @@
+// SPDX-FileCopyrightText: 2021 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
+//
+// SPDX-License-Identifier: LicenseRef-KDE-Accepted-GPL
+
+#include "WildcardCategoryMatcher.h"
+
+#include "CategoryCollection.h"
+#include "ImageDB.h"
+
+#include <kpabase/Logging.h>
+
+void DB::WildcardCategoryMatcher::debug(int level) const
+{
+    qCDebug(DBCategoryMatcherLog, "%s: %s", qPrintable(spaces(level)), qPrintable(m_re.pattern()));
+}
+
+void DB::WildcardCategoryMatcher::setRegularExpression(const QRegularExpression &re)
+{
+    m_re = re;
+    m_matchingTags.clear();
+    for (const auto &category : ImageDB::instance()->categoryCollection()->categories()) {
+        if (category->isSpecialCategory())
+            continue;
+        for (const auto &tag : category->itemsInclCategories()) {
+            if (m_re.match(tag).hasMatch()) {
+                m_matchingTags[category->name()] += tag;
+            }
+        }
+    }
+}
+
+bool DB::WildcardCategoryMatcher::eval(ImageInfoPtr info, QMap<QString, StringSet> &alreadyMatched)
+{
+    Q_UNUSED(alreadyMatched)
+    return eval(info);
+}
+
+bool DB::WildcardCategoryMatcher::eval(const DB::ImageInfoPtr info) const
+{
+    for (auto it = m_matchingTags.constKeyValueBegin(); it != m_matchingTags.constKeyValueEnd(); ++it) {
+        const auto categoryName = (*it).first;
+        const auto tags = (*it).second;
+        if (info->hasCategoryInfo(categoryName, tags))
+            return true;
+    }
+    return false;
+}
+
+// vi:expandtab:tabstop=4 shiftwidth=4:
---
+++ DB/WildcardCategoryMatcher.h
@@ -0,0 +1,48 @@
+// SPDX-FileCopyrightText: 2021 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
+//
+// SPDX-License-Identifier: LicenseRef-KDE-Accepted-GPL
+
+#ifndef WILDCARDCATEGORYMATCHER_H
+#define WILDCARDCATEGORYMATCHER_H
+
+#include "SimpleCategoryMatcher.h"
+
+#include <QRegularExpression>
+
+namespace DB
+{
+
+/**
+ * @brief The WildcardCategoryMatcher class matches any tag in any plain category.
+ * It can be used to search over all categories when the user is not sure which category a tag belongs to.
+ *
+ * @note At initialisation, a list of categories and matching tags is compiled.
+ * Therefore, the matcher can only match against tags that exist when it is initialized.
+ */
+class WildcardCategoryMatcher : public SimpleCategoryMatcher
+{
+public:
+    /**
+     * @brief Set a new match expression and compile a list of matching tags.
+     * @param re a regular expression matching against the desired tags
+     */
+    void setRegularExpression(const QRegularExpression &re);
+    bool eval(ImageInfoPtr, QMap<QString, StringSet> &alreadyMatched) override;
+    /**
+     * @brief evaluate the matcher for an image
+     * @param info
+     * @return \c true, if the ImageInfo is matched, \c false otherwise
+     */
+    bool eval(const ImageInfoPtr info) const;
+    void debug(int level) const override;
+
+private:
+    QRegularExpression m_re;
+    QHash<QString, StringSet> m_matchingTags; ///< The hash contains a list of matching tags for every category with matching tags.
+};
+
+}
+
+#endif /* WILDCARDCATEGORYMATCHER_H */
+
+// vi:expandtab:tabstop=4 shiftwidth=4:

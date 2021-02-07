57ec2a68bc0ac993be6c3861334258002061b36e
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2021-02-07 21:42:01 +0100

    BreadcrumbViewer: don't crash if updateText is called w/o breadcrumbs
    BreadcrumbViewer::updateText() is only called when the window is not
    wide enough to hold the full breadcrumb texts.
    If, however, the main window is very narrow on startup, the text is
    called at a time when there is no breadcrumb in the breadcrumb list,
    leading to updateText accessing an invalid index of the list.

--- MainWindow/BreadcrumbViewer.cpp
+++ MainWindow/BreadcrumbViewer.cpp
@@ -1,7 +1,8 @@
-/* SPDX-FileCopyrightText: 2003-2010 Jesper K. Pedersen <blackie@kde.org>
+// SPDX-FileCopyrightText: 2003-2010 Jesper K. Pedersen <blackie@kde.org>
+// SPDX-FileCopyrightText: 2021 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-   SPDX-License-Identifier: GPL-2.0-or-later
-*/
 #include "BreadcrumbViewer.h"
 
 #include <QTextDocument>
@@ -29,6 +30,9 @@ MainWindow::BreadcrumbViewer::BreadcrumbViewer()
  */
 void MainWindow::BreadcrumbViewer::updateText()
 {
+    if (m_activeCrumbs.isEmpty())
+        return;
+
     QStringList htmlList;
 
     for (int i = 0; i < m_activeCrumbs.count() - 1; ++i)

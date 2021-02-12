5794c339e884f009ca1875f3fb5a95919e4a1734
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2021-02-12 23:59:43 +0100

    Api documentation

--- MainWindow/SearchBar.h
+++ MainWindow/SearchBar.h
@@ -13,6 +13,19 @@ class KMainWindow;
 namespace MainWindow
 {
 
+/**
+ * @brief The SearchBar class is a thin wrapper around a search box (that is a QLineEdit).
+ * It makes the search box usable as a toolbar.
+ *
+ * It also installs an event filter for the search box that clears the search text when the escape key is pressed,
+ * and exposes movement keys via signal.
+ *
+ * ## Signals:
+ *
+ * The textChanged() and returnPressed() signals are what you would expect from a QLineEdit.
+ * Additionally, there is cleared() signal that is emitted when the SearchBar is reset,
+ * and keyPressed() that is emitted for a a few special keys.
+ */
 class SearchBar : public KToolBar
 {
     Q_OBJECT
@@ -24,13 +37,37 @@ protected:
     bool eventFilter(QObject *watched, QEvent *e) override;
 
 public slots:
+    /**
+     * @brief Clears the content of the search box.
+     */
     void reset();
-    void setLineEditEnabled(bool b);
+    /**
+     * @brief setLineEditEnabled calls setEnabled() on the search box.
+     * @param enabled
+     */
+    void setLineEditEnabled(bool enabled);
 
 signals:
+    /**
+     * @see QLineEdit::textChanged
+     */
     void textChanged(const QString &);
+    /**
+     * @see QLineEdit::returnPressed
+     */
     void returnPressed();
+    /**
+     * @brief cleared is emitted whenever the search box contents are cleared.
+     * This can happen either via key press (Escape key) or programatically.
+     */
     void cleared();
+    /**
+     * @brief keyPressed is emitted when a movement key is pressed.
+     * QKeyEvents that are signalled this way are:
+     *  - arrow keys
+     *  - Page up and Page down keys
+     *  - Home and End keys
+     */
     void keyPressed(QKeyEvent *);
 
 private:

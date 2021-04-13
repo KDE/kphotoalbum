b3d849b42e48a8f97691bf1858eb49043f85317f
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2021-04-14 00:05:08 +0200

    Review coding-standards

--- dev/documentation/coding-standards.h
+++ dev/documentation/coding-standards.h
@@ -1,14 +1,17 @@
-// SPDX-FileCopyrightText: 2013, 2015-2016, 2018-2020 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
-// SPDX-FileCopyrightText: 2012 Miika Turkia <miika.turkia@gmail.com>
 // SPDX-FileCopyrightText: 2009, 2013 Jesper K. Pedersen <blackie@kde.org>
+// SPDX-FileCopyrightText: 2012 Miika Turkia <miika.turkia@gmail.com>
+// SPDX-FileCopyrightText: 2013, 2015-2016, 2018-2020 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
+// SPDX-FileCopyrightText: 2021 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
+//
 // SPDX-License-Identifier: CC-BY-SA-4.0
-//krazy:skip
+
 /**
 \page coding-standards Coding Standards
 
 <h2>Compatibility</h2>
 
 Not every distro ships the latest version of Qt. As a rule of thumb, we aim for compatibility with the latest Ubuntu LTS release.
+Since invent.kde.org makes it easier to get a continuous integration pipeline for Debian stable, we are currently supporting that as well.
 
 <h3>Why Ubuntu?</h3>
 
@@ -27,9 +30,11 @@ We use clang-format to change our code. Please do use our commit-hooks that are 
 Here are a few pointers regarding coding standards in KPhotoAlbum. Please do stick to them, so the
 code is easier to get into for new people, and easier to maintain for everyone.
 
+For best results and minimum hassle, please do use clang-format to format your code:
+KPhotoAlbum ships with a `.clang-format` file.
+
 \li Basically, we try to stick by the
     <a href="https://community.kde.org/Policies/Frameworks_Coding_Style">kdelibs coding style</a>.
-\li Indentation is 4 spaces.
 \li Instance variables are prefixed with `m_` (not only with `_`).
 \li Static instance variables are prefixed with `s_`.
 \li Methods that are overridden from a superclass should be marked as such using the C++11
@@ -46,7 +51,7 @@ The order of include statements should always be the following:
 
 \li "config-kpa-xxx.h" includes
 \li file-specific header file
-\li KPA-includes
+\li KPhotoAlbum-includes
 \li other includes
 
 To speed up compilation and make things easier to understand, you should be careful about what you

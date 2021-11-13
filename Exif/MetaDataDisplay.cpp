28d62b2b743a82dfa67ff535af65525738ee276f
Tobias Leupold <tl@l3u.de>
2021-11-13 12:54:50 +0100

    Filled the MetaDataDisplay with meta data

--- Exif/MetaDataDisplay.cpp
+++ Exif/MetaDataDisplay.cpp
@@ -2,15 +2,142 @@
 //
 // SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 
+// Local includes
 #include "MetaDataDisplay.h"
 
-#include <QDebug>
+// KDE includes
+#include <KLocalizedString>
 
-Exif::MetaDataDisplay::MetaDataDisplay(QWidget *parent) : QLabel(parent)
+// Qt includes
+#include <QVBoxLayout>
+#include <QGridLayout>
+#include <QLabel>
+#include <QFileInfo>
+#include <QLocale>
+#include <QDebug>
+#include <QDateTime>
+#include <QMimeDatabase>
+
+static QString s_noPerm = QStringLiteral("-");
+static const QMimeDatabase s_mimeDB;
+
+Exif::MetaDataDisplay::MetaDataDisplay(QWidget *parent) : QWidget(parent)
 {
+    auto *mainLayout = new QVBoxLayout(this);
+    auto *layout = new QGridLayout;
+    mainLayout->addLayout(layout);
+
+    int row = 0;
+
+    layout->addWidget(keyLabel(i18n("Absolute path:")), row, 0);
+    m_absolutePath = valueLabel();
+    layout->addWidget(m_absolutePath, row++, 1);
+
+    layout->addWidget(keyLabel(i18n("MIME type:")), row, 0);
+    m_mimeType = valueLabel();
+    layout->addWidget(m_mimeType, row++, 1);
+
+    layout->addWidget(keyLabel(i18n("Size:")), row, 0);
+    m_size = valueLabel();
+    layout->addWidget(m_size, row++, 1);
+
+    layout->addWidget(keyLabel(i18n("Created:")), row, 0);
+    m_created = valueLabel();
+    layout->addWidget(m_created, row++, 1);
+
+    layout->addWidget(keyLabel(i18n("Changed:")), row, 0);
+    m_modified = valueLabel();
+    layout->addWidget(m_modified, row++, 1);
+
+    layout->addWidget(keyLabel(i18n("Owner:")), row, 0);
+    m_owner = valueLabel();
+    layout->addWidget(m_owner, row++, 1);
+
+    layout->addWidget(keyLabel(i18n("Group:")), row, 0);
+    m_group = valueLabel();
+    layout->addWidget(m_group, row++, 1);
+
+    layout->addWidget(keyLabel(i18n("Permissions:")), row, 0);
+    m_permissions = valueLabel();
+    layout->addWidget(m_permissions, row++, 1);
+
+    mainLayout->addStretch();
+}
+
+QLabel *Exif::MetaDataDisplay::keyLabel(const QString &text)
+{
+    auto *label = new QLabel;
+    label->setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum);
+    label->setText(text);
+    return label;
+}
+
+QLabel *Exif::MetaDataDisplay::valueLabel()
+{
+    auto *label = new QLabel;
+    label->setWordWrap(true);
+    label->setTextInteractionFlags(Qt::TextBrowserInteraction);
+    return label;
 }
 
 void Exif::MetaDataDisplay::setFileName(const QString &fileName)
 {
-    qDebug() << "fill me with info about" << fileName;
+    const QFileInfo info(fileName);
+    const QLocale locale;
+
+    m_absolutePath->setText(fileName);
+
+    m_mimeType->setText(s_mimeDB.mimeTypeForFile(fileName).name());
+
+    const auto size = info.size();
+    m_size->setText(i18nc("File size composed of a pre-formatted (already localized) "
+                          "human-readable file size (%1) and the number of bytes (%2)",
+                          "%1 (%2 B)", locale.formattedDataSize(size), size));
+
+    m_created->setText(locale.toString(info.birthTime()));
+
+    m_modified->setText(locale.toString(info.lastModified()));
+
+    m_owner->setText(info.owner());
+
+    m_group->setText(info.group());
+
+    const auto permissions = QFile::permissions(fileName);
+
+    QString parsedPermissions;
+    parsedPermissions.append(permissions & QFile::ReadOwner
+        ? i18nc("File permission shortcut for \"reading allowed\"", "r") : s_noPerm);
+    parsedPermissions.append(permissions & QFile::WriteOwner
+        ? i18nc("File permission shortcut for \"writing allowed\"", "w") : s_noPerm);
+    parsedPermissions.append(permissions & QFile::ExeOwner
+        ? i18nc("File permission shortcut for \"executing allowed\"", "x") : s_noPerm);
+    parsedPermissions.append(permissions & QFile::ReadGroup
+        ? i18nc("File permission shortcut for \"reading allowed\"", "r") : s_noPerm);
+    parsedPermissions.append(permissions & QFile::WriteGroup
+        ? i18nc("File permission shortcut for \"writing allowed\"", "w") : s_noPerm);
+    parsedPermissions.append(permissions & QFile::ExeGroup
+        ? i18nc("File permission shortcut for \"executing allowed\"", "x") : s_noPerm);
+    parsedPermissions.append(permissions & QFile::ReadOther
+        ? i18nc("File permission shortcut for \"reading allowed\"", "r") : s_noPerm);
+    parsedPermissions.append(permissions & QFile::WriteOther
+        ? i18nc("File permission shortcut for \"writing allowed\"", "w") : s_noPerm);
+    parsedPermissions.append(permissions & QFile::ExeOther
+        ? i18nc("File permission shortcut for \"executing allowed\"", "x") : s_noPerm);
+
+    auto hex = 0x0000;
+    if (permissions & QFile::ReadOwner)  { hex |= QFile::ReadOwner;  }
+    if (permissions & QFile::WriteOwner) { hex |= QFile::WriteOwner; }
+    if (permissions & QFile::ExeOwner)   { hex |= QFile::ExeOwner;   }
+    if (permissions & QFile::ReadGroup)  { hex |= QFile::ReadGroup;  }
+    if (permissions & QFile::WriteGroup) { hex |= QFile::WriteGroup; }
+    if (permissions & QFile::ExeGroup)   { hex |= QFile::ExeGroup;   }
+    if (permissions & QFile::ReadOther)  { hex |= QFile::ReadOther;  }
+    if (permissions & QFile::WriteOther) { hex |= QFile::WriteOther; }
+    if (permissions & QFile::ExeOther)   { hex |= QFile::ExeOther;   }
+    auto octalPermissions = QString::number(hex, 16);
+    octalPermissions = octalPermissions.remove(1, 1).prepend(QStringLiteral("0"));
+
+    m_permissions->setText(i18nc("File permissions string compiled from a parsed variant (e.g. "
+                                 "\"rw-rw-r--\", %1) and it's octal representation (e.g. \"0644\")",
+                                 "%1 (%2)", parsedPermissions, octalPermissions));
 }
--- Exif/MetaDataDisplay.h
+++ Exif/MetaDataDisplay.h
@@ -5,18 +5,34 @@
 #ifndef METADATADISPLAY_H
 #define METADATADISPLAY_H
 
-#include <QLabel>
+#include <QWidget>
+
+class QLabel;
 
 namespace Exif
 {
 
-class MetaDataDisplay : public QLabel
+class MetaDataDisplay : public QWidget
 {
     Q_OBJECT
 
 public:
     explicit MetaDataDisplay(QWidget *parent = nullptr);
     void setFileName(const QString &fileName);
+
+private: // Functions;
+    QLabel *keyLabel(const QString &text);
+    QLabel *valueLabel();
+
+private: // Variables
+    QLabel *m_absolutePath;
+    QLabel *m_size;
+    QLabel *m_created;
+    QLabel *m_modified;
+    QLabel *m_owner;
+    QLabel *m_group;
+    QLabel *m_permissions;
+    QLabel *m_mimeType;
 
 };
 

a2d1c7185d404e94bcbafe5a5a4788b3a64d4e4d
Tobias Leupold <tl@stonemx.de>
2022-07-18 12:17:30 +0200

    Updated the license headers for "my" files ...
    ... where I erroneously updated the license header to a not meaningful
    (and not intended) combination of licenses.

--- AnnotationDialog/DescriptionEdit.h
+++ AnnotationDialog/DescriptionEdit.h
@@ -1,6 +1,6 @@
-// SPDX-FileCopyrightText: 2014 Tobias Leupold <tl at l3u dot de>
+// SPDX-FileCopyrightText: 2014 Tobias Leupold <tl at stonemx dot de>
 //
-// SPDX-License-Identifier: GPL-2.0-or-later OR GPL-3.0-or-later OR LicenseRef-KDE-Accepted-GPL
+// SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 
 #ifndef DESCRIPTIONEDIT_H
 #define DESCRIPTIONEDIT_H

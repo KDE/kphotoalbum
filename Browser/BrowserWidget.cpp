d175ff04ede8fe1ccbced7b7cd5f8230b8851bb7
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2022-08-19 00:47:16 +0200

    Fix Coverity: control flow issues

--- Browser/BrowserWidget.cpp
+++ Browser/BrowserWidget.cpp
@@ -1,5 +1,14 @@
-// SPDX-FileCopyrightText: 2003-2020 The KPhotoAlbum Development Team
-// SPDX-FileCopyrightText: 2021 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
+// SPDX-FileCopyrightText: 2003-2010, 2012, 2022 Jesper K. Pedersen <jesper.pedersen@kdab.com>
+// SPDX-FileCopyrightText: 2005, 2007 Dirk Mueller <mueller@kde.org>
+// SPDX-FileCopyrightText: 2006 Tuomas Suutari <tuomas@nepnep.net>
+// SPDX-FileCopyrightText: 2008 Henner Zeller <h.zeller@acm.org>
+// SPDX-FileCopyrightText: 2008-2009 Jan Kundrát <jkt@flaska.net>
+// SPDX-FileCopyrightText: 2011 Andreas Neustifter <andreas.neustifter@gmail.com>
+// SPDX-FileCopyrightText: 2012 Miika Turkia <miika.turkia@gmail.com>
+// SPDX-FileCopyrightText: 2013, 2015-2016, 2018-2021 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
+// SPDX-FileCopyrightText: 2015, 2017-2018 Tobias Leupold <tl@stonemx.de>
+// SPDX-FileCopyrightText: 2016 Matthias Füssel <matthias.fuessel@gmx.net>
+// SPDX-FileCopyrightText: 2021-2022 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
 
@@ -440,9 +449,6 @@ void Browser::BrowserWidget::handleResizeEvent(QMouseEvent *event)
         return;
 
     DB::CategoryPtr category = action->category();
-
-    if (!action)
-        return;
 
     if (event->type() == QEvent::MouseButtonPress) {
         m_resizePressPos = event->pos();

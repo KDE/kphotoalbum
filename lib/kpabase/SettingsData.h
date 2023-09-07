b548679195f92213785ad53311ec433f7120181d
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2023-09-07 22:52:41 +0200

    Fix -Wpedantic warning

--- lib/kpabase/SettingsData.h
+++ lib/kpabase/SettingsData.h
@@ -1,6 +1,14 @@
-// SPDX-FileCopyrightText: 2003-2020 The KPhotoAlbum Development Team
-// SPDX-FileCopyrightText: 2021 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
-// SPDX-FileCopyrightText: 2022 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
+// SPDX-FileCopyrightText: 2003-2021 Jesper K. Pedersen <jesper.pedersen@kdab.com>
+// SPDX-FileCopyrightText: 2005-2007 Dirk Mueller <mueller@kde.org>
+// SPDX-FileCopyrightText: 2006-2008 Tuomas Suutari <tuomas@nepnep.net>
+// SPDX-FileCopyrightText: 2007-2008 Jan Kundrát <jkt@flaska.net>
+// SPDX-FileCopyrightText: 2008 Henner Zeller <h.zeller@acm.org>
+// SPDX-FileCopyrightText: 2009-2012 Miika Turkia <miika.turkia@gmail.com>
+// SPDX-FileCopyrightText: 2010 Wes Hardaker <kpa@capturedonearth.com>
+// SPDX-FileCopyrightText: 2011 Andreas Neustifter <andreas.neustifter@gmail.com>
+// SPDX-FileCopyrightText: 2012-2023 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
+// SPDX-FileCopyrightText: 2014-2022 Tobias Leupold <tl@stonemx.de>
+// SPDX-FileCopyrightText: 2019 Robert Krawitz <rlk@alum.mit.edu>
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
 
@@ -66,8 +74,8 @@ enum class VideoBackend { NotConfigured = 0b0000,
                           Phonon = 0b0001,
                           QtAV = 0b0010,
                           VLC = 0b0100 };
-Q_DECLARE_FLAGS(VideoBackends, VideoBackend);
-Q_FLAG_NS(VideoBackend);
+Q_DECLARE_FLAGS(VideoBackends, VideoBackend)
+Q_FLAG_NS(VideoBackend)
 
 typedef const char *WindowType;
 extern const WindowType MainWindow, AnnotationDialog;

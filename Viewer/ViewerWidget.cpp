KDE/kphotoalbum

commit 3a130ed45089c1b043bea8834a23be0438b4ee17

Support annotating images from the viewer

Viewer/ViewerWidget.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2003-2022 The KPhotoAlbum Development Team
+// SPDX-FileCopyrightText: 2003-2023 The KPhotoAlbum Development Team
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
 
@@ -25,6 +25,8 @@ class QMenu;
 class QResizeEvent;
 class QStackedWidget;
 class QWheelEvent;
+class CursorVisibilityHandler;
+class QLabel;
 
 namespace DB
 {
@@ -42,10 +44,12 @@ class InfoDialog;
 }
 namespace Viewer
 {
+
+class AnnotationHandler;
 class AbstractDisplay;
 class ImageDisplay;
 class InfoBox;
-class SpeedDisplay;
+class TransientDisplay;
 class TextDisplay;
 class VideoDisplay;
 class VideoShooter;
@@ -57,8 +61,7 @@ public:
     enum UsageType { InlineViewer,
                      ViewerWindow };
 
-    ViewerWidget(UsageType type = ViewerWindow,
-                 QMap<Qt::Key, QPair<QString, QString>> *macroStore = nullptr);
+    ViewerWidget(UsageType type = ViewerWindow);
     ~ViewerWidget() override;
     static ViewerWidget *latest();
     void load(const DB::FileNameList &list, int index = 0);
@@ -121,6 +124,7 @@ protected:
     void createFilterMenu();
     void changeSlideShowInterval(int delta);
     void createVideoViewer();
+    void createAnnotationMenu();
     void inhibitScreenSaver(bool inhibit);
     DB::ImageInfoPtr currentInfo() const;
     friend class InfoBox;
@@ -130,11 +134,15 @@ protected:
 private:
     void showNextN(int);
     void showPrevN(int);
-    int find_tag_in_list(const QStringList &list, QString &namefound);
     void invalidateThumbnail() const;
     enum RemoveAction { RemoveImageFromDatabase,
                         OnlyRemoveFromViewer };
     void removeOrDeleteCurrent(RemoveAction);
+
+    enum class TagMode { Locked,
+                         Annotating,
+                         Tokenizing };
+    void setTagMode(TagMode tagMode);
 
 protected Q_SLOTS:
     void showNext();
@@ -174,6 +182,9 @@ protected Q_SLOTS:
     void zoomFull();
     void zoomPixelForPixel();
     void makeThumbnailImage();
+    void addTag();
+    void editDescription();
+    void showAnnotationHelp();
 
     /** Set the current window title (filename) and add the given detail */
     void setCaptionWithDetail(const QString &detail);
@@ -185,10 +196,13 @@ protected Q_SLOTS:
     void slotRemoveDeletedImages(const DB::FileNameList &imageList);
 
     void triggerCopyLinkAction(MainWindow::CopyLinkEngine::Action action);
+    void toggleTag(const QString &category, QString value);
+    void copyTagsFromPreviousImage();
 
 private:
     static ViewerWidget *s_latest;
     friend class VideoShooter;
+    friend class TemporarilyDisableCursorHandling;
 
     QList<QAction *> m_forwardActions;
     QList<QAction *> m_backwardActions;
@@ -233,12 +247,11 @@ private:
     QAction *m_linkToAction;
 
     InfoBox *m_infoBox;
-    QImage m_currentImage;
 
     bool m_showingFullScreen;
 
     int m_slideShowPause;
-    SpeedDisplay *m_speedDisplay;
+    TransientDisplay *m_transientDisplay;
     KActionCollection *m_actions;
     bool m_forward;
     QTimer *m_slideShowTimer;
@@ -251,20 +264,15 @@ private:
     bool m_videoPlayerStoppedManually;
     UsageType m_type;
 
-    enum InputMode { InACategory,
-                     AlwaysStartWithCategory };
-
-    InputMode m_currentInputMode;
-    QString m_currentInput;
-    QString m_currentCategory;
-    QString m_currentInputList;
-
-    QString m_lastFound;
-    QString m_lastCategory;
-    QMap<Qt::Key, QPair<QString, QString>> *m_inputMacros;
-    QMap<Qt::Key, QPair<QString, QString>> *m_myInputMacros;
-
     MainWindow::CopyLinkEngine *m_copyLinkEngine;
+    CursorVisibilityHandler *m_cursorHandlerForImageDisplay;
+    CursorVisibilityHandler *m_cursorHandlerForVideoDisplay;
+    AnnotationHandler *m_annotationHandler;
+
+    TagMode m_tagMode = TagMode::Locked;
+    QAction *m_addTagAction;
+    QAction *m_copyAction;
+    QAction *m_addDescriptionAction;
 };
 
 }

.reuse/dep5
@@ -25,3 +25,7 @@ License: CC0-1.0
 Files: demo/*.jpg demo/*.avi demo/index.xml
 Copyright: 2003-2007 Jesper K. Pedersen
 License: CC-BY-SA-4.0
+
+Files: *.ui
+Copyright: 2023 The KPhotoAlbum Development Team
+License: GPL-2.0-or-later

CMakeLists.txt
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2007 Gustavo P. Boiko <gustavo.boiko@kdemail.net>
 # SPDX-FileCopyrightText: 2007-2008 Laurent Montel <montel@kde.org>
 # SPDX-FileCopyrightText: 2007-2010 Tuomas Suutari <tuomas@nepnep.net>
-# SPDX-FileCopyrightText: 2007-2014 Jesper K. Pedersen <blackie@kde.org>
+# SPDX-FileCopyrightText: 2007-2023 Jesper K. Pedersen <blackie@kde.org>
 # SPDX-FileCopyrightText: 2008, 2013-2014, 2016 Pino Toscano <pino@kde.org>
 # SPDX-FileCopyrightText: 2008-2009 Henner Zeller <h.zeller@acm.org>
 # SPDX-FileCopyrightText: 2008-2009 Jan Kundrát <jkt@flaska.net>
@@ -36,6 +36,7 @@ math(EXPR MINIMUM_Qt5_VERSION_HEX "${MINIMUM_Qt5_VERSION_MAJOR} << 24 | ${MINIMU
 
 set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_AUTOUIC ON)
 
 # provide drop-down menu for build-type in cmake-gui:
 set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS ";Debug;Release;RelWithDebInfo;MinSizeRel")
@@ -228,8 +229,8 @@ set(libViewer_SRCS
     "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/ImageDisplay.h"
     "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/ViewHandler.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/ViewHandler.h"
-    "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/SpeedDisplay.cpp"
-    "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/SpeedDisplay.h"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/TransientDisplay.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/TransientDisplay.h"
     "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/InfoBox.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/InfoBox.h"
     "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/CategoryImageConfig.cpp"
@@ -254,7 +255,18 @@ set(libViewer_SRCS
     "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/VideoToolBar.h"
     "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/Slider.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/Slider.h"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/CursorVisibilityHandler.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/CursorVisibilityHandler.h"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/AnnotationHandler.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/AnnotationHandler.h"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/SelectCategoryAndValue.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/SelectCategoryAndValue.h"
     )
+
+ki18n_wrap_ui(libViewer_SRCS
+    "${CMAKE_CURRENT_SOURCE_DIR}/Viewer/SelectCategoryAndValue.ui"
+    )
+
 
 set(libCategoryListView_SRCS
     "${CMAKE_CURRENT_SOURCE_DIR}/CategoryListView/documentation.h"
@@ -488,6 +500,8 @@ set(libDB_SRCS
     "${CMAKE_CURRENT_SOURCE_DIR}/DB/XML/NumberedBackup.h"
     "${CMAKE_CURRENT_SOURCE_DIR}/DB/XML/XmlReader.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/DB/XML/XmlReader.h"
+    "${CMAKE_CURRENT_SOURCE_DIR}/DB/GlobalCategorySortOrder.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/DB/GlobalCategorySortOrder.h"
     )
 
 set(libImportExport_SRCS
@@ -694,7 +708,6 @@ add_subdirectory(themes)
 add_subdirectory(scripts)
 
 add_subdirectory(doc)
-
 
 ########### next target ###############
 

DB/CategoryCollection.cpp
@@ -12,6 +12,8 @@
 
 #include <DB/ImageDB.h>
 
+DB::CategoryCollection::~CategoryCollection() = default;
+
 DB::CategoryPtr DB::CategoryCollection::categoryForName(const QString &name) const
 {
     for (QList<DB::CategoryPtr>::ConstIterator it = m_categories.begin(); it != m_categories.end(); ++it) {
@@ -78,6 +80,11 @@ void DB::CategoryCollection::rename(const QString &oldName, const QString &newNa
     Q_EMIT categoryCollectionChanged();
 }
 
+DB::GlobalCategorySortOrder *DB::CategoryCollection::globalSortOrder()
+{
+    return m_globalSortOrder.get();
+}
+
 void DB::CategoryCollection::initIdMap()
 {
     for (DB::CategoryPtr categoryPtr : qAsConst(m_categories)) {
@@ -93,6 +100,11 @@ void DB::CategoryCollection::slotItemRenamed(const QString &oldName, const QStri
 void DB::CategoryCollection::slotItemRemoved(const QString &item)
 {
     Q_EMIT itemRemoved(static_cast<Category *>(const_cast<QObject *>(sender())), item);
+}
+
+DB::CategoryCollection::CategoryCollection()
+    : m_globalSortOrder(new GlobalCategorySortOrder())
+{
 }
 
 #include "moc_CategoryCollection.cpp"

DB/CategoryCollection.h
@@ -12,8 +12,9 @@
 
 #include "Category.h"
 #include "CategoryPtr.h"
-
+#include "GlobalCategorySortOrder.h"
 #include <QList>
+#include <memory>
 
 namespace DB
 {
@@ -28,6 +29,8 @@ class CategoryCollection : public QObject
     Q_OBJECT
 
 public:
+    ~CategoryCollection();
+
     enum class IncludeSpecialCategories {
         Yes,
         No
@@ -42,6 +45,7 @@ public:
                      int thumbnailSize, bool show, bool positionable = false);
     void removeCategory(const QString &name);
     void rename(const QString &oldName, const QString &newName);
+    GlobalCategorySortOrder *globalSortOrder();
 
     // FIXME(jzarl): this should be private and FileWriter should be a friend class
     void initIdMap();
@@ -57,8 +61,12 @@ protected Q_SLOTS:
     void slotItemRemoved(const QString &item);
 
 private:
+    friend class ImageDB;
+    CategoryCollection();
+
     QList<DB::CategoryPtr> m_categories;
     QMap<DB::Category::CategoryType, DB::CategoryPtr> m_specialCategories;
+    std::unique_ptr<GlobalCategorySortOrder> m_globalSortOrder;
 };
 
 }

DB/GlobalCategorySortOrder.cpp
@@ -1,0 +1,74 @@
+// SPDX-FileCopyrightText: 2003-2023 The KPhotoAlbum Development Team
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "GlobalCategorySortOrder.h"
+#include "CategoryCollection.h"
+#include "ImageDB.h"
+#include <QDebug>
+#include <algorithm>
+#include <set>
+
+namespace DB
+{
+
+void GlobalCategorySortOrder::pushToFront(const QString &category, const QString &value)
+{
+    const Item item { category, value };
+    m_sortOrder.removeAll(item);
+    m_sortOrder.push_front(item);
+}
+
+QList<GlobalCategorySortOrder::Item> GlobalCategorySortOrder::modifiedSortOrder()
+{
+    return m_sortOrder;
+}
+
+QList<GlobalCategorySortOrder::Item> GlobalCategorySortOrder::completeSortOrder()
+{
+    QList<Item> result;
+    result.append(m_sortOrder);
+
+    // This is the set of all those categories already kept at the front.
+    // We have it as a set to allow fast lookup when going through all categories
+    // and appending them (as those found here should not be appended)
+    QSet<Item> currentSet(m_sortOrder.cbegin(), m_sortOrder.cend());
+
+    // To make it possible to remove entries no longer present in the categories
+    // (either because they were removed or renamed, or gone some other magic way),
+    // we initial copy all categories from m_sortOrder, and then remove them as we see them
+    // while populating from each category
+    QSet<Item> unknownSet = currentSet;
+
+    const auto categories = DB::ImageDB::instance()->categoryCollection()->categories();
+    for (const auto &category : categories) {
+        if (!category->isSpecialCategory() || category->type() == DB::Category::TokensCategory) {
+            auto items = category->items();
+
+            for (const auto &categoryItem : qAsConst(items)) {
+                const Item item { category->name(), categoryItem };
+                unknownSet.remove(item);
+                if (!currentSet.contains(item))
+                    result.append(item);
+            }
+        }
+    }
+
+    for (const auto &item : unknownSet) {
+        result.removeAll(item);
+    }
+
+    return result;
+}
+
+bool operator==(const GlobalCategorySortOrder::Item &x, const GlobalCategorySortOrder::Item &y)
+{
+    return x.category == y.category && x.item == y.item;
+}
+
+size_t qHash(const GlobalCategorySortOrder::Item &item)
+{
+    return qHash(item.category) + qHash(item.item);
+}
+
+} // namespace DB

DB/GlobalCategorySortOrder.h
@@ -1,0 +1,49 @@
+// SPDX-FileCopyrightText: 2003-2023 The KPhotoAlbum Development Team
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "CategoryPtr.h"
+#include <QList>
+
+namespace DB
+{
+class FileReader;
+
+/**
+   Just as each category has a most-recently-used sorting,
+   that makes it possible to see the most likely hit in the List boxes in the annotation dialog,
+   we also keep a global most-recent-used list across all categories, so that you can
+   get a sorting like People/Jesper, Places/Las Vegas, People Jim
+   This is what this class encapsulate.
+
+   When an item has been used for tagging, it is pushed to the front of the list using
+   \ref pushToFront.
+
+   To get the global sort order for use for completion, call \ref completeSortOrder.
+
+   On her other hand, to save the sort order, only those ever used should be retrieved,
+   which is possible using \ref modifiedSortOrder
+ */
+class GlobalCategorySortOrder
+{
+public:
+    void pushToFront(const QString &category, const QString &value);
+
+    struct Item {
+        QString category;
+        QString item;
+        friend bool operator==(const Item &x, const Item &y);
+        friend size_t qHash(const Item &item);
+    };
+
+    QList<Item> completeSortOrder();
+    QList<Item> modifiedSortOrder();
+
+private:
+    friend class DB::FileReader;
+    QList<Item> m_sortOrder;
+};
+
+} // namespace DB

commit d84d2d535fd32f9f95918b8716b20e6db8474757

Ensure that tokens are upper case.
This was a regression introduced by commit
3a130ed45089c1b043bea8834a23be0438b4ee17.

BUG: 474406

Viewer/ViewerWidget.cpp
@@ -1553,21 +1553,25 @@ void Viewer::ViewerWidget::triggerCopyLinkAction(MainWindow::CopyLinkEngine::Act
     m_copyLinkEngine->selectTarget(this, selectedFiles, action);
 }
 
-void Viewer::ViewerWidget::toggleTag(const QString &category, QString value)
+void Viewer::ViewerWidget::toggleTag(const QString &category, const QString &value)
 {
-    const bool set = !currentInfo()->hasCategoryInfo(category, value);
-    if (set)
-        currentInfo()->addCategoryInfo(category, value);
+    QString tag = value;
+    if (category == DB::ImageDB::instance()->categoryCollection()->categoryForSpecial(DB::Category::TokensCategory)->name())
+        tag = value.toUpper();
+
+    const bool tagIsSet = !currentInfo()->hasCategoryInfo(category, tag);
+    if (tagIsSet)
+        currentInfo()->addCategoryInfo(category, tag);
     else
-        currentInfo()->removeCategoryInfo(category, value);
+        currentInfo()->removeCategoryInfo(category, tag);
 
     // Assume we've now annotated this image - this is to avoid removing the untagged item all the time.
     currentInfo()->removeCategoryInfo(Settings::SettingsData::instance()->untaggedCategory(), Settings::SettingsData::instance()->untaggedTag());
     updateInfoBox();
 
     if (category == DB::ImageDB::instance()->categoryCollection()->categoryForSpecial(DB::Category::TokensCategory)->name())
-        value = i18n("Token %1", value.toUpper());
-    m_transientDisplay->display(set ? value : QLatin1String("<s>%1</s>").arg(value), 500ms, TransientDisplay::NoFadeOut);
+        tag = i18n("Token %1", tag);
+    m_transientDisplay->display(tagIsSet ? tag : QLatin1String("<s>%1</s>").arg(tag), 500ms, TransientDisplay::NoFadeOut);
 }
 
 void Viewer::ViewerWidget::copyTagsFromPreviousImage()

Viewer/ViewerWidget.h
@@ -196,7 +196,7 @@ protected Q_SLOTS:
     void slotRemoveDeletedImages(const DB::FileNameList &imageList);
 
     void triggerCopyLinkAction(MainWindow::CopyLinkEngine::Action action);
-    void toggleTag(const QString &category, QString value);
+    void toggleTag(const QString &category, const QString &value);
     void copyTagsFromPreviousImage();
 
 private:
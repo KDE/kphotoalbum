KDE/kphotoalbum

commit cf90f45e92a1343f5747bc951681341e2c178048

Properly adjust current viewer info if files are deleted.
If images are deleted while the viewer window is open, the viewer widget
would not change the currently displayed image.
This issue also could lead to a crash if the currently displayed image
is invalidated.

BUG: 474392

Viewer/ViewerWidget.cpp
@@ -512,8 +512,27 @@ void Viewer::ViewerWidget::setCaptionWithDetail(const QString &detail)
 
 void Viewer::ViewerWidget::slotRemoveDeletedImages(const DB::FileNameList &imageList)
 {
+    const auto currentFile = m_list[m_current];
     for (const auto &filename : imageList) {
         m_list.removeAll(filename);
+    }
+    if (m_list.isEmpty()) {
+        close();
+        return;
+    }
+
+    const int newIndex = m_list.indexOf(currentFile);
+    if (newIndex == -1) {
+        // find some sensible file to display in place of the deleted file
+        if (m_current >= m_list.count()) {
+            m_current = m_list.size();
+            showPrev();
+        } else {
+            showNextN(0);
+        }
+    } else {
+        m_current = newIndex;
+        showNextN(0);
     }
 }
 

commit f1e9d5b7ad7e11d7402c5704dfd0f00600dac102

Fix crash/regression caused by cf90f45e
BUG: 474392

Viewer/ViewerWidget.cpp
@@ -512,7 +512,7 @@ void Viewer::ViewerWidget::setCaptionWithDetail(const QString &detail)
 
 void Viewer::ViewerWidget::slotRemoveDeletedImages(const DB::FileNameList &imageList)
 {
-    const auto currentFile = m_list[m_current];
+    const auto currentFile = m_list.value(m_current);
     for (const auto &filename : imageList) {
         m_list.removeAll(filename);
     }
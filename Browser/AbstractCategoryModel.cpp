KDE/kphotoalbum

commit 44d16fef5aac0568b38db571e9cba2c7e46bba9e

Add natural sort order to Browser::TreeFilter.
This adds locale-specific numerical sort order to the category browser
pages.

Browser/TreeFilter.cpp
@@ -1,13 +1,18 @@
-/* SPDX-FileCopyrightText: 2003-2010 Jesper K. Pedersen <blackie@kde.org>
-
-   SPDX-License-Identifier: GPL-2.0-or-later
-*/
+// SPDX-FileCopyrightText: 2003 - 2010 Jesper K. Pedersen <blackie@kde.org>
+// SPDX-FileCopyrightText: 2023 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "TreeFilter.h"
 
+#include <QCollator>
+
 Browser::TreeFilter::TreeFilter(QObject *parent)
     : QSortFilterProxyModel(parent)
+    , m_naturalCollator(std::make_unique<QCollator>())
 {
+    m_naturalCollator->setNumericMode(true);
+    m_naturalCollator->setIgnorePunctuation(true); // only works if Qt is set up to use ICU
 }
 
 bool Browser::TreeFilter::filterAcceptsRow(int row, const QModelIndex &parent) const
@@ -45,4 +50,27 @@ void Browser::TreeFilter::resetCache()
 {
     m_matchedMap.clear();
 }
+
+bool Browser::TreeFilter::naturalSortOrder() const
+{
+    return m_naturalSortOrder;
+}
+
+void Browser::TreeFilter::setNaturalSortOrder(bool naturalSortOrder)
+{
+    m_naturalSortOrder = naturalSortOrder;
+}
+
+bool Browser::TreeFilter::lessThan(const QModelIndex &source_left, const QModelIndex &source_right) const
+{
+    if (m_naturalSortOrder) {
+        // numeric sort
+        const QString &string_left = source_left.data(sortRole()).toString();
+        const QString &string_right = source_right.data(sortRole()).toString();
+        return m_naturalCollator->compare(string_left, string_right) < 0;
+    } else {
+        return QSortFilterProxyModel::lessThan(source_left, source_right);
+    }
+}
+
 // vi:expandtab:tabstop=4 shiftwidth=4:

Browser/BrowserWidget.cpp
@@ -315,7 +315,7 @@ void Browser::BrowserWidget::switchToViewType(DB::Category::ViewType type)
     } else {
         m_curView = m_listView;
         m_filterProxy->invalidate();
-        m_filterProxy->sort(0, Qt::AscendingOrder);
+        m_filterProxy->sort(0, Qt::AscendingOrder); // sort by column 0
 
         m_listView->setViewMode(dynamic_cast<OverviewPage *>(currentAction()) == nullptr ? CenteringIconView::NormalIconView : CenteringIconView::CenterView);
     }

Browser/TreeFilter.h
@@ -1,16 +1,20 @@
-/* SPDX-FileCopyrightText: 2003-2010 Jesper K. Pedersen <blackie@kde.org>
-
-   SPDX-License-Identifier: GPL-2.0-or-later
-*/
+// SPDX-FileCopyrightText: 2003 - 2010 Jesper K. Pedersen <blackie@kde.org>
+// SPDX-FileCopyrightText: 2023 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef TREEFILTER_H
 #define TREEFILTER_H
 #include <QSortFilterProxyModel>
+#include <memory>
+
+class QCollator;
 
 namespace Browser
 {
 /**
  * \brief Filter proxy that keeps parent branches if child branches matches
+ * Additionally, it supports changing the sort order from QString comparison to localized numeric sort (aka natural sort order).
  *
  * See \ref Browser for a detailed description of how this fits in with the rest of the classes in this module
  *
@@ -25,10 +29,34 @@ public:
     explicit TreeFilter(QObject *parent = nullptr);
     void resetCache();
 
+    /**
+     * @brief setNaturalSortOrder sets the sort order to be natural or not.
+     * "Natural" sort order sorts according to the current locale and properly sorts numeric values, so that e.g. 2 sorts before 10.
+     *
+     * Note: Natural sort order is more expensive than string comparison, so even if it defaults to being enabled, it needs to be user-configurable for people with large databases.
+     * @param naturalSortOrder
+     */
+    void setNaturalSortOrder(bool naturalSortOrder);
+
+    /**
+     * @brief naturalSortOrder
+     * @return \c true, if natural sort order is enabled, \c false otherwise.
+     */
+    bool naturalSortOrder() const;
+
 protected:
     bool filterAcceptsRow(int source_row, const QModelIndex &source_parent) const override;
+    /**
+     * @brief lessThan overrides QSortFilterProxyModel::lessThan to enable "natural" sorting of numeric values.
+     * @see setNaturalSort(bool)
+     */
+    bool lessThan(const QModelIndex &source_left, const QModelIndex &source_right) const override;
 
     mutable QMap<QModelIndex, bool> m_matchedMap;
+
+private:
+    bool m_naturalSortOrder = true;
+    std::unique_ptr<QCollator> m_naturalCollator;
 };
 }
 

commit 7452e018361908b6f6b6146a8dd8de75461346ce

Fix sort order of "no other" and "none"
This fixes a regression introduced by
44d16fef5aac0568b38db571e9cba2c7e46bba9e (natural sort order), causing
"no other" or "none" and their localized versions to be sorted with the
rest of the list items when natual sort order was active.

BUG: 476504

Browser/AbstractCategoryModel.cpp
@@ -1,15 +1,19 @@
-/* SPDX-FileCopyrightText: 2003-2020 The KPhotoAlbum Development Team
-
-   SPDX-License-Identifier: GPL-2.0-or-later
-*/
+// SPDX-FileCopyrightText: 2009 Yuri Chornoivan <yurchor@ukr.net>
+// SPDX-FileCopyrightText: 2009-2010 Jan Kundrát <jkt@flaska.net>
+// SPDX-FileCopyrightText: 2009-2010 Jesper K. Pedersen <jesper.pedersen@kdab.com>
+// SPDX-FileCopyrightText: 2011 Andreas Neustifter <andreas.neustifter@gmail.com>
+// SPDX-FileCopyrightText: 2013-2023 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
+// SPDX-FileCopyrightText: 2015-2016 Tobias Leupold <tl@stonemx.de>
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "AbstractCategoryModel.h"
 
-#include "BrowserWidget.h"
 #include "enums.h"
 
 #include <DB/ImageDB.h>
 #include <DB/MemberMap.h>
+#include <kpabase/SettingsData.h>
 
 #include <KLocalizedString>
 #include <QApplication>
@@ -108,7 +112,7 @@ QVariant Browser::AbstractCategoryModel::data(const QModelIndex &index, int role
     else if (role == ValueRole) {
         switch (column) {
         case 0:
-            return name; // Notice we sort by **None** rather than None, which makes it show up at the top for less than searches.
+            return name;
         case 1:
             return m_images[name].count;
         case 2:
@@ -124,6 +128,9 @@ QVariant Browser::AbstractCategoryModel::data(const QModelIndex &index, int role
             return range.end().toSecsSinceEpoch();
         }
         }
+    } else if (role == SortPriorityRole) {
+        // none is to be sorted first
+        return (name == DB::ImageDB::NONE());
     }
 
     return QVariant();

Browser/TreeFilter.cpp
@@ -5,6 +5,8 @@
 
 #include "TreeFilter.h"
 
+#include <Browser/enums.h>
+
 #include <QCollator>
 
 Browser::TreeFilter::TreeFilter(QObject *parent)
@@ -63,6 +65,12 @@ void Browser::TreeFilter::setNaturalSortOrder(bool naturalSortOrder)
 
 bool Browser::TreeFilter::lessThan(const QModelIndex &source_left, const QModelIndex &source_right) const
 {
+    const bool sortPriority_left = source_left.data(SortPriorityRole).toBool();
+    const bool sortPriority_right = source_right.data(SortPriorityRole).toBool();
+    // if only one item has priority, it gets prioritized above the other. Otherwise sort normally...
+    if (sortPriority_left xor sortPriority_right) {
+        return sortPriority_left;
+    }
     if (m_naturalSortOrder) {
         // numeric sort
         const QString &string_left = source_left.data(sortRole()).toString();

Browser/enums.h
@@ -1,7 +1,7 @@
-/* SPDX-FileCopyrightText: 2003-2010 Jesper K. Pedersen <blackie@kde.org>
-
-   SPDX-License-Identifier: GPL-2.0-or-later
-*/
+// SPDX-FileCopyrightText: 2003 - 2010 Jesper K. Pedersen <blackie@kde.org>
+// SPDX-FileCopyrightText: 2023 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef BROWSER_ENUMS_H
 #define BROWSER_ENUMS_H
@@ -11,8 +11,9 @@
 namespace Browser
 {
 
-const int ItemNameRole = Qt::UserRole + 1;
-const int ValueRole = Qt::UserRole + 2;
+constexpr int ItemNameRole = Qt::UserRole + 1;
+constexpr int ValueRole = Qt::UserRole + 2;
+constexpr int SortPriorityRole = Qt::UserRole + 3; ///< bool, \c true if item should be sorted before regular items, \c false otherwise.
 
 }
 
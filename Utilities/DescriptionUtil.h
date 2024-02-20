76d88ce99d3fa7c41c7c1c834562101dccea9296
Tobias Leupold <tl@stonemx.de>
2024-02-20 20:28:36 +0100

    Removed the old ago/age code

--- Utilities/DescriptionUtil.h
+++ Utilities/DescriptionUtil.h
@@ -1,7 +1,6 @@
-/* SPDX-FileCopyrightText: 2003-2019 The KPhotoAlbum Development Team
-
-   SPDX-License-Identifier: GPL-2.0-or-later
-*/
+// SPDX-FileCopyrightText: 2003-2024 The KPhotoAlbum Development Team
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef TEXTUTIL_H
 #define TEXTUTIL_H
@@ -11,17 +10,12 @@
 #include "DB/ImageInfoPtr.h"
 
 // Qt includes
-#include <QDate>
 #include <QMap>
-#include <QPair>
 #include <QString>
 
 namespace Utilities
 {
 QString createInfoText(DB::ImageInfoPtr info, QMap<int, QPair<QString, QString>> *);
-QString formatAge(DB::CategoryPtr category, const QString &item, DB::ImageInfoPtr info);
-QString timeAgo(const DB::ImageInfoPtr info);
-QString timeAgo(const QDate &date);
 }
 
 #endif /* TEXTUTIL_H */

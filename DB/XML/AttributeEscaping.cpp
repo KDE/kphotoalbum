045dacd302f6935811a102efff37ea251da11ce3
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2025-04-05 00:53:21 +0200

    Reformat for readability
    Have default case lead in the if statement.

--- DB/XML/AttributeEscaping.cpp
+++ DB/XML/AttributeEscaping.cpp
@@ -27,8 +27,22 @@ QString DB::unescapeAttributeName(const QString &str, int fileVersion)
     // for the "compressed" format. Also, there was some space-underscore substitution for the
     // "readable" format. We need this when reading a pre-v11 db, so we still provide this algorithm
     // here:
+    const bool usePercentEncoding = (fileVersion >= 11);
 
-    if (fileVersion <= 10) {
+    if (usePercentEncoding) {
+        // Beginning from db v11, we use a modified percent encoding provided by QByteArray that will
+        // work for all input strings and covers the whole Unicode range. We only do this for the
+        // "compressed" format. For the "readble" format, the string is used as-is.
+        if (!useCompressedFileFormat()) {
+            return str;
+        } else {
+            auto unEscaped = str.mid(1); // Here we strip the ":" we added in DB::FileWriter::escape
+            unEscaped = QString::fromUtf8(QByteArray::fromPercentEncoding(unEscaped.toUtf8(), '_'));
+            s_cache.insert(str, unEscaped);
+            return unEscaped;
+        }
+    } else {
+        // legacy escaping
         auto unEscaped = str;
 
         if (!useCompressedFileFormat()) {
@@ -66,18 +80,6 @@ QString DB::unescapeAttributeName(const QString &str, int fileVersion)
         s_cache.insert(str, unEscaped);
         return unEscaped;
     }
-
-    // Beginning from db v11, we use a modified percent encoding provided by QByteArray that will
-    // work for all input strings and covers the whole Unicode range. We only do this for the
-    // "compressed" format. For the "readble" format, the string is used as-is.
-    if (!useCompressedFileFormat()) {
-        return str;
-    } else {
-        auto unEscaped = str.mid(1); // Here we strip the ":" we added in DB::FileWriter::escape
-        unEscaped = QString::fromUtf8(QByteArray::fromPercentEncoding(unEscaped.toUtf8(), '_'));
-        s_cache.insert(str, unEscaped);
-        return unEscaped;
-    }
 }
 
 QString DB::escapeAttributeName(const QString &str, int fileVersion)
@@ -100,8 +102,39 @@ QString DB::escapeAttributeName(const QString &str, int fileVersion)
     // for the "compressed" format. Also, there was some space-underscore substitution for the
     // "readable" format. We need this when reading a pre-v11 db, so we still provide this algorithm
     // here:
+    const bool usePercentEncoding = (fileVersion >= 11);
 
-    if (fileVersion <= 10) {
+    if (usePercentEncoding) {
+        if (!useCompressedFileFormat()) {
+            // If we use the "readable" format, we don't need escaping:
+            return str;
+
+        } else {
+            // If we use the "compressed" file format, we have to do some escaping, because the category
+            // names are used as XML attributes and we can't use all characters for them. Beginning from
+            // db v11, we use a modified percent encoding provided by QByteArray that will work for all
+            // input strings and covers the whole Unicode range:
+
+            // The first character of an XML attribute must be a NameStartChar. That is a-z,
+            // A-Z, ":" or "_". From the second position on, also 0-9, "." and "-" are allowed
+            // (cf. https://www.w3.org/TR/xml/ for the full specification).
+            //
+            // To keep it simple, we escape everything that is not a small or capital letter, and use
+            // the underscore as our escaping char. This way, nothing can go wrong, no matter what the
+            // given string contains, and at which position.
+
+            // By default, QByteArray::toPercentEncoding encodes everything that is not a-z, A-Z, 0-9,
+            // "-", ".", "_" or "~". We thus have to add some more chars to include:
+            static const QByteArray s_escapeIncludes = QStringLiteral("-._~0123456789").toUtf8();
+
+            const auto escaped = QString::fromUtf8(
+                str.toUtf8().toPercentEncoding(QByteArray(), s_escapeIncludes, '_'));
+            s_cache.insert(str, escaped);
+
+            // We always start with a ":", so that we can't collide with our internal attribute names
+            return QStringLiteral(":%1").arg(escaped);
+        }
+    } else {
         QString escaped;
 
         if (!useCompressedFileFormat()) {
@@ -128,36 +161,6 @@ QString DB::escapeAttributeName(const QString &str, int fileVersion)
 
         s_cache.insert(str, escaped);
         return escaped;
-    }
-
-    if (!useCompressedFileFormat()) {
-        // If we use the "readable" format, we don't need escaping:
-        return str;
-
-    } else {
-        // If we use the "compressed" file format, we have to do some escaping, because the category
-        // names are used as XML attributes and we can't use all characters for them. Beginning from
-        // db v11, we use a modified percent encoding provided by QByteArray that will work for all
-        // input strings and covers the whole Unicode range:
-
-        // The first character of an XML attribute must be a NameStartChar. That is a-z,
-        // A-Z, ":" or "_". From the second position on, also 0-9, "." and "-" are allowed
-        // (cf. https://www.w3.org/TR/xml/ for the full specification).
-        //
-        // To keep it simple, we escape everything that is not a small or capital letter, and use
-        // the underscore as our escaping char. This way, nothing can go wrong, no matter what the
-        // given string contains, and at which position.
-
-        // By default, QByteArray::toPercentEncoding encodes everything that is not a-z, A-Z, 0-9,
-        // "-", ".", "_" or "~". We thus have to add some more chars to include:
-        static const QByteArray s_escapeIncludes = QStringLiteral("-._~0123456789").toUtf8();
-
-        const auto escaped = QString::fromUtf8(
-            str.toUtf8().toPercentEncoding(QByteArray(), s_escapeIncludes, '_'));
-        s_cache.insert(str, escaped);
-
-        // We always start with a ":", so that we can't collide with our internal attribute names
-        return QStringLiteral(":%1").arg(escaped);
     }
 }
 

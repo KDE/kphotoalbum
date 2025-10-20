893b764946438b2fa41e31aafa058ee569cbadc8
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2025-10-21 00:34:10 +0200

    Simplify file suffix related code
    The image suffix list was hardcoded with lower and upper case suffixes
    including the dot before the suffix, which is really not necessary.
    As a side-effect this change allows the image suffix code to be more in
    line with the video suffix code.

--- lib/kpabase/FileExtensions.cpp
+++ lib/kpabase/FileExtensions.cpp
@@ -14,13 +14,16 @@
 
 #include "FileExtensions.h"
 
+#include <kpabase/Logging.h>
 #include <kpabase/SettingsData.h>
 #include <kpabase/config-kpa-kdcraw.h>
+
 #ifdef HAVE_KDCRAW
 #include <KDCRAW/RawFiles>
 #include <libkdcraw_version.h>
 #endif
 
+#include <QDebug>
 #include <QFileInfo>
 #include <QSet>
 #include <QString>
@@ -39,48 +42,29 @@ void _initializeExtensionLists(QStringList &rawExtensions, QStringList &standard
             (*it).remove(QString::fromUtf8("*."));
 
         _standardExtensions << QString::fromLatin1("jpg")
-                            << QString::fromLatin1("JPG")
                             << QString::fromLatin1("jpeg")
-                            << QString::fromLatin1("JPEG")
-                            << QString::fromLatin1("tif")
-                            << QString::fromLatin1("TIF")
-                            << QString::fromLatin1("tiff")
-                            << QString::fromLatin1("TIFF")
                             << QString::fromLatin1("png")
-                            << QString::fromLatin1("PNG");
-        _ignoredExtensions << QString::fromLatin1("thm") // Thumbnails
-                           << QString::fromLatin1("THM")
+                            << QString::fromLatin1("tif")
+                            << QString::fromLatin1("tiff");
+        _ignoredExtensions << QString::fromLatin1("db")
+                           << QString::fromLatin1("thm") // thumbnail files
                            << QString::fromLatin1("thumb") // thumbnail files
                            // from dcraw
+                           << QString::fromLatin1("bz2")
                            << QString::fromLatin1("ctg") // Catalog files
                            << QString::fromLatin1("gz") // Compressed files
-                           << QString::fromLatin1("Z")
-                           << QString::fromLatin1("bz2")
-                           << QString::fromLatin1("zip")
-                           << QString::fromLatin1("xml")
-                           << QString::fromLatin1("XML")
-                           << QString::fromLatin1("html")
-                           << QString::fromLatin1("HTML")
                            << QString::fromLatin1("htm")
-                           << QString::fromLatin1("HTM")
+                           << QString::fromLatin1("html")
                            << QString::fromLatin1("pp3") // RawTherapee Sidecar files
-                           << QString::fromLatin1("PP3")
-                           << QString::fromLatin1("xmp") // Other sidecars
-                           << QString::fromLatin1("XMP")
                            << QString::fromLatin1("pto") // Hugin sidecars
-                           << QString::fromLatin1("PTO");
+                           << QString::fromLatin1("xml")
+                           << QString::fromLatin1("xmp") // Other sidecars
+                           << QString::fromLatin1("z")
+                           << QString::fromLatin1("zip");
 
-        QChar dot(QChar::fromLatin1('.'));
-        for (QStringList::iterator it = _rawExtensions.begin(); it != _rawExtensions.end(); ++it)
-            if (!(*it).startsWith(dot))
-                *it = dot + *it;
-        for (QStringList::iterator it = _standardExtensions.begin(); it != _standardExtensions.end(); ++it)
-            if (!(*it).startsWith(dot))
-                *it = dot + *it;
-        for (QStringList::iterator it = _ignoredExtensions.begin(); it != _ignoredExtensions.end(); ++it)
-            if (!(*it).startsWith(dot))
-                *it = dot + *it;
-
+        qCInfo(BaseLog) << "Recognized image file suffixes:" << _standardExtensions;
+        qCInfo(BaseLog) << "Recognized raw image file suffixes:" << _rawExtensions;
+        qCInfo(BaseLog) << "Ignored file suffixes:" << _ignoredExtensions;
         extensionListsInitialized = true;
     }
 
@@ -92,14 +76,16 @@ bool _fileExistsWithExtensions(const DB::FileName &fileName,
                                const QStringList &extensionList)
 {
     QString baseFileName = fileName.absolute();
-    int extStart = baseFileName.lastIndexOf(QChar::fromLatin1('.'));
+    // +1 -> include the dot
+    int extStart = baseFileName.lastIndexOf(QChar::fromLatin1('.')) + 1;
     // We're interested in xxx.yyy, not .yyy
-    if (extStart <= 1)
+    if (extStart <= 2)
         return false;
     baseFileName.remove(extStart, baseFileName.length() - extStart);
+
     for (QStringList::ConstIterator it = extensionList.begin();
          it != extensionList.end(); ++it) {
-        if (QFile::exists(baseFileName + *it))
+        if (QFile::exists(baseFileName + *it) || QFile::exists(baseFileName + (*it).toUpper()))
             return true;
     }
     return false;
@@ -110,13 +96,16 @@ bool _fileIsKnownWithExtensions(const DB::FileNameSet &files,
                                 const QStringList &extensionList)
 {
     QString baseFileName = fileName.absolute();
-    int extStart = baseFileName.lastIndexOf(QChar::fromLatin1('.'));
-    if (extStart <= 1)
+    // +1 -> include the dot
+    int extStart = baseFileName.lastIndexOf(QChar::fromLatin1('.')) + 1;
+    if (extStart <= 2)
         return false;
     baseFileName.remove(extStart, baseFileName.length() - extStart);
+
     for (QStringList::ConstIterator it = extensionList.begin();
          it != extensionList.end(); ++it) {
-        if (files.contains(DB::FileName::fromAbsolutePath(baseFileName + *it)))
+        if (files.contains(DB::FileName::fromAbsolutePath(baseFileName + *it))
+            || files.contains(DB::FileName::fromAbsolutePath(baseFileName + (*it).toUpper())))
             return true;
     }
     return false;
@@ -125,12 +114,9 @@ bool _fileIsKnownWithExtensions(const DB::FileNameSet &files,
 bool _fileEndsWithExtensions(const DB::FileName &fileName,
                              const QStringList &extensionList)
 {
-    for (QStringList::ConstIterator it = extensionList.begin();
-         it != extensionList.end(); ++it) {
-        if (fileName.relative().endsWith(*it, Qt::CaseInsensitive))
-            return true;
-    }
-    return false;
+    const QFileInfo fi(fileName.relative());
+    const QString ext = fi.suffix().toLower();
+    return extensionList.contains(ext);
 }
 } // namespace
 
@@ -195,34 +181,35 @@ const QSet<QString> &KPABase::videoExtensions()
 {
     static QSet<QString> videoExtensions;
     if (videoExtensions.empty()) {
-        videoExtensions.insert(QString::fromLatin1("3gp"));
-        videoExtensions.insert(QString::fromLatin1("avi"));
-        videoExtensions.insert(QString::fromLatin1("mp4"));
-        videoExtensions.insert(QString::fromLatin1("m4v"));
-        videoExtensions.insert(QString::fromLatin1("mpeg"));
-        videoExtensions.insert(QString::fromLatin1("mpg"));
-        videoExtensions.insert(QString::fromLatin1("qt"));
-        videoExtensions.insert(QString::fromLatin1("mov"));
-        videoExtensions.insert(QString::fromLatin1("moov"));
-        videoExtensions.insert(QString::fromLatin1("qtvr"));
-        videoExtensions.insert(QString::fromLatin1("rv"));
         videoExtensions.insert(QString::fromLatin1("3g2"));
-        videoExtensions.insert(QString::fromLatin1("fli"));
-        videoExtensions.insert(QString::fromLatin1("flc"));
-        videoExtensions.insert(QString::fromLatin1("mkv"));
-        videoExtensions.insert(QString::fromLatin1("mng"));
+        videoExtensions.insert(QString::fromLatin1("3gp"));
         videoExtensions.insert(QString::fromLatin1("asf"));
         videoExtensions.insert(QString::fromLatin1("asx"));
-        videoExtensions.insert(QString::fromLatin1("wmp"));
-        videoExtensions.insert(QString::fromLatin1("wmv"));
-        videoExtensions.insert(QString::fromLatin1("ogm"));
-        videoExtensions.insert(QString::fromLatin1("rm"));
+        videoExtensions.insert(QString::fromLatin1("avi"));
+        videoExtensions.insert(QString::fromLatin1("flc"));
+        videoExtensions.insert(QString::fromLatin1("fli"));
         videoExtensions.insert(QString::fromLatin1("flv"));
-        videoExtensions.insert(QString::fromLatin1("webm"));
+        videoExtensions.insert(QString::fromLatin1("m2ts"));
+        videoExtensions.insert(QString::fromLatin1("m4v"));
+        videoExtensions.insert(QString::fromLatin1("mkv"));
+        videoExtensions.insert(QString::fromLatin1("mng"));
+        videoExtensions.insert(QString::fromLatin1("moov"));
+        videoExtensions.insert(QString::fromLatin1("mov"));
+        videoExtensions.insert(QString::fromLatin1("mp4"));
+        videoExtensions.insert(QString::fromLatin1("mpeg"));
+        videoExtensions.insert(QString::fromLatin1("mpg"));
         videoExtensions.insert(QString::fromLatin1("mts"));
         videoExtensions.insert(QString::fromLatin1("ogg"));
+        videoExtensions.insert(QString::fromLatin1("ogm"));
         videoExtensions.insert(QString::fromLatin1("ogv"));
-        videoExtensions.insert(QString::fromLatin1("m2ts"));
+        videoExtensions.insert(QString::fromLatin1("qt"));
+        videoExtensions.insert(QString::fromLatin1("qtvr"));
+        videoExtensions.insert(QString::fromLatin1("rm"));
+        videoExtensions.insert(QString::fromLatin1("rv"));
+        videoExtensions.insert(QString::fromLatin1("webm"));
+        videoExtensions.insert(QString::fromLatin1("wmp"));
+        videoExtensions.insert(QString::fromLatin1("wmv"));
+        qCInfo(BaseLog) << "Recognized video file suffixes:" << videoExtensions;
     }
     return videoExtensions;
 }
